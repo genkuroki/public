701429f5932cf61d1012359b17a4bad9156028b0
Gen Kuroki <genkuroki@gmail.com>
2021-06-12 12:29:04 +0900

    Update v1.7 findmin and Plots plotly example.jl

--- 0004/v1.7 findmin and Plots plotly example.jl
+++ 0004/v1.7 findmin and Plots plotly example.jl
@@ -25,8 +25,40 @@ surface(x, y, f; xlim=extrema(x), ylim=extrema(y), camera=(30, 45), size=(500, 5
 # %%
 fmin, pt = findmin(p -> f(p...), Iterators.product(x, y))
 
-# %%
+# %% tags=[]
 surface(x, y, f; xlim=extrema(x), ylim=extrema(y), camera=(30, 45), size=(500, 500), colorbar=false)
 scatter!([pt[1]], [pt[2]], [f(pt...)]; label="", color=:cyan, ms=2)
+
+# %%
+f(p) = sin(p[1]) * sin(p[2]) + (p[1] - 2p[2])^2/50
+x = y = range(-π, π; length=401)
+findmin(f, Iterators.product(x, y))
+
+# %%
+f(p) = sin(p[1]) * sin(p[2]) + (p[1] - 2p[2])^2/50
+x = y = range(-π, π, 1000)
+findmin(f, Iterators.product(x, y))
+
+# %%
+using Plots
+plotly()
+x = y = range(-3, 3, length=100)
+f(x, y) = exp(-x^2-y^2)
+z = f.(x', y)
+surface(x, y, z; colorbar=false, size=(600, 600), color=:jet)
+
+# %%
+using Plots
+plotly()
+x = y = range(-3, 3, length=100)
+z = exp.(.-x'.^ 2 .- y.^ 2)
+surface(x, y, z; colorbar=false, size=(600, 600), color=:jet)
+
+# %%
+using Plots
+plotly()
+x = y = range(-3, 3, length=100)
+z = @. exp(-x'^2 .- y^2)
+surface(x, y, z; colorbar=false, size=(600, 600), color=:jet)
 
 # %%
