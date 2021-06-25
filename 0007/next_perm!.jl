e3299ccccd291a7254f886da906713d8655c5e3e
Gen Kuroki <genkuroki@gmail.com>
2021-06-25 18:27:42 +0900

    revised

--- 0007/next_perm!.jl
+++ 0007/next_perm!.jl
@@ -21,15 +21,54 @@
 # %%
 module O
 
-function next_perm!(v::AbstractVector)
-    length(v) ≤ 1 && return false
-    
+"""
+zero allocation version of AquaIndigo's code
+"""
+function next_perm_old!(v::AbstractVector)
+    length(v) ≤ 1 && return false    
     k = findlast(isless(v[i], v[i+1]) for i in firstindex(v):lastindex(v)-1)
     isnothing(k) && (reverse!(v); return false)
-    i = k + firstindex(v)-1
-    
+    i = k + firstindex(v) - 1    
     j = findlast(isless(v[i], v[j]) for j in i+1:lastindex(v)) + i
     v[i], v[j] = v[j], v[i]
+    reverse!(v, i + 1)
+    return true
+end
+
+"""
+    next_perm!(v::AbstractVector)
+
+changes `v` into the next permutation of `v` under the lexicographic order and returns `false` if it is the last permutation and `true` otherwise.
+
+This is a variant of `Combinatorics.nextpermutation(m, t, state)`.
+
+Examples
+
+```
+julia> v = [1, 2, 3]; println(v); while O.next_perm!(v) println(v) end
+[1, 2, 3]
+[1, 3, 2]
+[2, 1, 3]
+[2, 3, 1]
+[3, 1, 2]
+[3, 2, 1]
+```
+
+```
+julia> v = [1, 2, 2]; println(v); while O.next_perm!(v) println(v) end
+[1, 2, 2]
+[2, 1, 2]
+[2, 2, 1]
+```
+"""
+function next_perm!(v::AbstractVector)
+    length(v) ≤ 1 && return false
+    i = lastindex(v) - 1
+    @inbounds while i ≥ firstindex(v) && !isless(v[i], v[i+1]) i -= 1 end
+    i < firstindex(v) && (reverse!(v); return false)
+    j = lastindex(v)
+    @inbounds while j > i && !isless(v[i], v[j]) j -= 1 end
+    @inbounds v[i], v[j] = v[j], v[i]
     reverse!(v, i + 1)
     return true
 end
@@ -40,8 +79,34 @@ function collect_perm!(v::AbstractVector)
     a
 end
 
-struct Perm{T<:AbstractVector} v::T end
-perm(v::AbstractVector; kwargs...) = Perm(sort(v; kwargs...))
+"""
+    Perm(v::AbstractVector)
+
+constructs the lexicographic order iterator of the all permutations of `v`.
+
+Examples
+
+```
+julia> for s in O.Perm([1, 2, 3]) println(s) end
+[1, 2, 3]
+[1, 3, 2]
+[2, 1, 3]
+[2, 3, 1]
+[3, 1, 2]
+[3, 2, 1]
+```
+
+```
+julia> for s in O.Perm([1, 2, 2]) println(s) end
+[1, 2, 2]
+[2, 1, 2]
+[2, 2, 1]
+```
+"""
+struct Perm{T<:AbstractVector}
+    v::T
+    Perm(v::T) where T<: AbstractVector = new{T}(sort(v))
+end
 
 function Base.length(p::Perm)
     v = p.v
@@ -86,17 +151,48 @@ end
 end
 
 # %%
+@doc O.next_perm!
+
+# %%
+@doc O.Perm
+
+# %%
+v = [1, 2, 3]; println(v); while O.next_perm!(v) println(v) end
+
+# %%
+v = [1, 2, 2]; println(v); while O.next_perm!(v) println(v) end
+
+# %%
+for s in O.Perm([1, 2, 3]) println(s) end
+
+# %%
+for s in O.Perm([1, 2, 2]) println(s) end
+
+# %%
+v = sort([NaN, NaN, 2, 1])
+@show v
+while O.next_perm!(v)
+    @show v
+end
+
+# %%
+p = O.Perm([NaN, NaN, 2, 1])
+for s in p
+    @show s
+end
+
+# %%
 using BenchmarkTools
 
 # %%
-p = O.perm([NaN, NaN, 2, 1])
+p = O.Perm([NaN, NaN, 2, 1])
 _, state = iterate(p)
 
 # %%
 iterate(p, state)
 
 # %%
-p = O.perm([NaN, NaN, 2, 1])
+p = O.Perm([NaN, NaN, 2, 1])
 @btime collect($p)
 
 # %%
@@ -104,19 +200,17 @@ v = sort([NaN, NaN, 2, 1])
 @btime O.collect_perm!($v)
 
 # %%
-w = sort(v)
-@show w
-while O.next_perm!(w)
-    @show w
-end
-
-# %%
 test_next_perm!(v) = (c = 1; while O.next_perm!(v) c += 1 end; c)
 @show s = [1, 1, 2, 2, 2, 3, 4, 5, 6, 6]
 @btime test_next_perm!($s)
 
 # %%
-c = @btime collect($(O.perm(s)))
+test_next_perm_old!(v) = (c = 1; while O.next_perm_old!(v) c += 1 end; c)
+@show s = [1, 1, 2, 2, 2, 3, 4, 5, 6, 6]
+@btime test_next_perm_old!($s)
+
+# %%
+c = @btime collect($(O.Perm(s)))
 length(c)
 
 # %%
@@ -125,7 +219,7 @@ length(c)
 # %%
 using OffsetArrays
 @show t = OffsetArray([1, 1, 2, 2, 2, 3, 4, 5, 6, 6], -5:4)
-@btime collect($(O.perm(t)))
+@btime collect($(O.Perm(t)))
 
 # %%
 @btime O.collect_perm!($(sort(t)))
