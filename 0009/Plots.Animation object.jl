18801c5091f563675e8fd00b2c3714f9d66572a6
Gen Kuroki <genkuroki@gmail.com>
2021-07-09 09:24:26 +0900

    created

---
+++ 0009/Plots.Animation object.jl
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.8.0-DEV
+#     language: julia
+#     name: julia-1.8
+# ---
+
+# %%
+using Plots
+
+@userplot CirclePlot
+@recipe function f(cp::CirclePlot)
+    x, y, i = cp.args
+    n = length(x)
+    inds = circshift(1:n, 1 - i)
+    linewidth --> range(0, 10, length = n)
+    seriesalpha --> range(0, 1, length = n)
+    aspect_ratio --> 1
+    label --> false
+    x[inds], y[inds]
+end
+
+n = 150
+t = range(0, 2π, length = n)
+x = sin.(t)
+y = cos.(t)
+
+anim = @animate for i ∈ 1:n
+    circleplot(x, y, i)
+end
+gif(anim, "anim_fps15.gif", fps = 15)
+
+# %%
+dump(anim)
+
+# %%
+anim.dir
+
+# %%
+first(frame(anim), 5)
+
+# %%
+using Images
+
+A = [load(joinpath(anim.dir, fn)) for fn in frame(anim)]
+
+for img in A
+    IJulia.clear_output(true)
+    display(img)
+    sleep(1/15)
+end
+
+# %%
+A[77]
+
+# %%
