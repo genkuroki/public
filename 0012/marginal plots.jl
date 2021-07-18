43707c29981c714cc8425349f02413a66fbabca7
Gen Kuroki <genkuroki@gmail.com>
2021-07-18 15:15:18 +0900

    revised

--- 0012/marginal plots.jl
+++ 0012/marginal plots.jl
@@ -14,6 +14,11 @@
 #     name: julia-1.6
 # ---
 
+# %% [markdown]
+# # Examples of marginal plots
+#
+# Extended version of https://discourse.julialang.org/t/multiple-plots-some-refinements/64800/5
+
 # %%
 using Plots
 using Distributions
@@ -35,6 +40,9 @@ X, Y = sample[1, :], sample[2, :]
 k, kx, ky = kde.(((X, Y), X, Y))
 ik, ikx, iky = InterpKDE.((k, kx, ky));
 
+# %% [markdown]
+# ## Sample marginal plots
+
 # %%
 layout = @layout [
     a             _
@@ -46,6 +54,20 @@ legend = colorbar = false
 a = plot(kx.x, kx.density; xlim, legend)
 c = plot(ky.density, ky.x; ylim, legend, xrotation=90)
 b = contour(k.x, k.y, k.density; xlim, ylim, legend)
+
+plot(a, b, c; layout, link=:both, size=(500, 500))
+
+# %%
+layout = @layout [
+    a             _
+    b{0.8w, 0.8h} c
+]
+
+xlim, ylim = extrema.((k.x, k.y))
+legend = colorbar = false
+a = plot(kx.x, kx.density; xlim, legend)
+c = plot(ky.density, ky.x; ylim, legend, xrotation=90)
+b = contourf(k.x, k.y, k.density; xlim, ylim, legend)
 
 plot(a, b, c; layout, link=:both, size=(500, 500))
 
@@ -91,6 +113,54 @@ b = scatter(X, Y; xlim, ylim, legend, marker_z=pdf.(Ref(ik), X, Y), alpha=0.7, m
 
 plot(a, b, c; layout, link=:both, size=(500, 500))
 
+# %% [markdown]
+# ## True distribution marginal plots
+
+# %%
+layout = @layout [
+    a             _
+    b{0.8w, 0.8h} c
+]
+
+xlim, ylim = extrema.((k.x, k.y))
+legend = colorbar = false
+a = plot(kx.x, pdf.(distx_true, kx.x); xlim, label="true")
+c = plot(pdf.(disty_true, ky.x), ky.x; ylim, xrotation=90, label="true")
+b = contour(k.x, k.y, (x, y)->pdf(dist_true, [x, y]); xlim, ylim, legend)
+
+plot(a, b, c; layout, link=:both, size=(500, 500))
+
+# %%
+layout = @layout [
+    a             _
+    b{0.8w, 0.8h} c
+]
+
+xlim, ylim = extrema.((k.x, k.y))
+legend = colorbar = false
+a = plot(kx.x, pdf.(distx_true, kx.x); xlim, label="true")
+c = plot(pdf.(disty_true, ky.x), ky.x; ylim, xrotation=90, label="true")
+b = contourf(k.x, k.y, (x, y)->pdf(dist_true, [x, y]); xlim, ylim, legend)
+
+plot(a, b, c; layout, link=:both, size=(500, 500))
+
+# %%
+layout = @layout [
+    a             _
+    b{0.8w, 0.8h} c
+]
+
+xlim, ylim = extrema.((k.x, k.y))
+legend = colorbar = false
+a = plot(kx.x, pdf.(distx_true, kx.x); xlim, label="true")
+c = plot(pdf.(disty_true, ky.x), ky.x; ylim, xrotation=90, label="true")
+b = heatmap(k.x, k.y, (x, y)->pdf(dist_true, [x, y]); xlim, ylim, legend)
+
+plot(a, b, c; layout, link=:both, size=(500, 500))
+
+# %% [markdown]
+# ## Other examples
+
 # %%
 layout = @layout [
     a             _
@@ -104,6 +174,22 @@ plot!(kx.x, pdf.(distx_true, kx.x); label="true", ls=:dash)
 c = plot(ky.density, ky.x; ylim, xrotation=90, label="Y")
 plot!(pdf.(disty_true, ky.x), ky.x; label="true", ls=:dash)
 b = contour(k.x, k.y, k.density; xlim, ylim, legend)
+
+plot(a, b, c; layout, link=:both, size=(500, 500))
+
+# %%
+layout = @layout [
+    a             _
+    b{0.8w, 0.8h} c
+]
+
+xlim, ylim = extrema.((k.x, k.y))
+legend = colorbar = false
+a = plot(kx.x, kx.density; xlim, label="X")
+plot!(kx.x, pdf.(distx_true, kx.x); label="true", ls=:dash)
+c = plot(ky.density, ky.x; ylim, xrotation=90, label="Y")
+plot!(pdf.(disty_true, ky.x), ky.x; label="true", ls=:dash)
+b = contourf(k.x, k.y, k.density; xlim, ylim, legend)
 
 plot(a, b, c; layout, link=:both, size=(500, 500))
 
@@ -136,6 +222,7 @@ plot!(kx.x, pdf.(distx_true, kx.x); label="true", ls=:dash)
 c = plot(ky.density, ky.x; ylim, xrotation=90, label="Y")
 plot!(pdf.(disty_true, ky.x), ky.x; label="true", ls=:dash)
 b = scatter(X, Y; xlim, ylim, colorbar, marker_z=pdf.(Ref(ik), X, Y), alpha=0.7, msw=0, label="(X, Y)")
+contour!(k.x, k.y, (x, y)->pdf(dist_true, [x, y]); xlim, ylim, legend, ls=:dash, color=2)
 
 plot(a, b, c; layout, link=:both, size=(500, 500))
 
@@ -152,6 +239,7 @@ plot!(kx.x, pdf.(distx_true, kx.x); label="true", ls=:dash)
 c = plot(ky.density, ky.x; ylim, xrotation=90, label="Y")
 plot!(pdf.(disty_true, ky.x), ky.x; label="true", ls=:dash)
 b = scatter(X, Y; xlim, ylim, colorbar, marker_z=pdf.(Ref(ik), X, Y), alpha=0.7, msw=0, color=:rainbow, label="(X, Y)")
+contour!(k.x, k.y, (x, y)->pdf(dist_true, [x, y]); xlim, ylim, legend, ls=:dash, color=2)
 
 plot(a, b, c; layout, link=:both, size=(500, 500))
 
