3022f586687b9145b3d1e18d466645041b692f77
Gen Kuroki <genkuroki@gmail.com>
2021-07-26 12:58:54 +0900

    created

---
+++ 0013/rand of uniform dist on simplex.jl
@@ -0,0 +1,66 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.7.0-beta3
+#     language: julia
+#     name: julia-1.7
+# ---
+
+# %%
+using Random, Statistics, Plots
+
+function diff!(X)
+    @inbounds for i in reverse(keys(X)[begin:end-1])
+        X[i+1] -= X[i]
+    end
+    X
+end
+
+function randsimplex!(rng::AbstractRNG, X)
+    X[end] = one(eltype(X))
+    Y = @view X[begin:end-1]
+    rand!(rng, Y)
+    sort!(Y)
+    X .*= length(X)
+    diff!(X)
+end
+randsimplex(rng::AbstractRNG, N, T=Float64) = randsimplex!(rng, Vector{T}(undef, N))
+randsimplex!(X) = randsimplex!(Random.default_rng(), X)
+randsimplex(N, T=Float64) = randsimplex!(Vector{T}(undef, N))
+
+n = 10^4
+A = randsimplex(n)
+@show mean(A), var(A)
+histogram(A; norm=true, alpha=0.3, label="randsimplex")
+plot!(x->exp(-x), 0, 6; xlim=(-0.1, 6), lw=2, label="exp dist")
+
+# %%
+struct Simplex{T} N::Int end
+Simplex(N) = Simplex{Float64}(N)
+
+function Random.rand!(rng::AbstractRNG,
+        X::AbstractVector{T}, d::Random.SamplerTrivial{Simplex{T}}) where T
+    @assert length(X) == d[].N
+    randsimplex!(rng, X)
+end
+Base.rand(rng::AbstractRNG, d::Random.SamplerTrivial{Simplex{T}}) where T =
+    rand!(rng, Vector{T}(undef, d[].N), d)
+
+# %%
+using BenchmarkTools
+
+X = zeros(10)
+d = Simplex(10)
+@btime rand!($X, $d)
+
+# %%
+rand(Simplex(10))
+
+# %%
