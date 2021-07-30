12cb92828dc7252fa35cb65bc635e7c6c09ff120
Gen Kuroki <genkuroki@gmail.com>
2021-07-31 07:17:14 +0900

    updated

--- 0013/Monte Carlo calculation of pi.jl
+++ 0013/Monte Carlo calculation of pi.jl
@@ -17,36 +17,61 @@
 # %%
 using Statistics
 using Plots
-using Distributed
-
-rmprocs(procs()[2:end])
-@show addprocs()
 
 function mcpi1(n)
-    c = @distributed (+) for _ in 1:n
-        rand()^2 + rand()^2 ≤ 1
+    c = 0
+    for _ in 1:n
+        c += rand()^2 + rand()^2 ≤ 1
     end
     4c/n
 end
 
 function mcpi2(n)
-    c = @distributed (+) for _ in 1:n
+    c = 0
+    for _ in 1:n
         x, y = rand(), rand()
-        if x + y < 1
-            x, y = 1 - y, 1 - x
-        end
-        x^2 + y^2 ≤ 1
+        c += (x^2 + y^2 ≤ 1 && (1 - x)^2 + (1 - y)^2 ≤ 1)
     end
     2c/n + 2
 end
 
-@time pi1 = [mcpi1(10^6) for _ in 1:10^4]
-@time pi2 = [mcpi2(10^6) for _ in 1:10^4];
+function mcpi3(n)
+    c = 0
+    for _ in 1:n
+        x, y = rand(), (1 - √3/2)*rand() + √3/2
+        c += (x^2 + y^2 > 1 && (1 - x)^2 + y^2 > 1)
+    end
+    6(1 - √3/4 - (1 - √3/2)*c/n)
+end
+
+function sim(mcpi, n, L)
+    X = Vector{Float64}(undef, L)
+    Threads.@threads for i in 1:L
+        X[i] = mcpi(n)
+    end
+    X
+end
+
+@show Threads.nthreads()
+@time pi1 = sim(mcpi1, 10^6, 10^4)
+@time pi2 = sim(mcpi2, 10^6, 10^4)
+@time pi3 = sim(mcpi3, 10^6, 10^4)
 
 @show mean(pi1), std(pi1)
 @show mean(pi2), std(pi2)
+@show mean(pi3), std(pi3)
 
-stephist(pi1; norm=true, label="mcpi1")
-stephist!(pi2; norm=true, label="mcpi2")
+plot()
+stephist!(pi1; norm=true, label="mcpi1", lw=1.5, ls=:solid)
+stephist!(pi2; norm=true, label="mcpi2", lw=1.5, ls=:dash)
+stephist!(pi3; norm=true, label="mcpi3", lw=1.5, ls=:dashdot)
+
+# %%
+using Base64
+showimg(mime, fn; tag="img") = open(fn) do f
+    base64 = base64encode(f)
+    display("text/html", """<$tag src="data:$mime;base64,$base64" />""")
+end
+showimg("image/jpeg", "1BD08057-D77D-44FF-9D3A-7620DA4CF740.jpeg"; tag="img width=80%")
 
 # %%
