5b11efebf7cbaaf68d7a1bb23ece271f45777f27
Gen Kuroki <genkuroki@gmail.com>
2021-08-12 05:04:10 +0900

    created

---
+++ 0015/Arpack/LinearMaps vs. SparseArrays.jl
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.8.0-DEV
+#     language: julia
+#     name: julia-1.8
+# ---
+
+# %%
+using LinearAlgebra
+using LoopVectorization
+using LinearMaps
+using ArnoldiMethod
+
+function prepareDiag(L)
+    diag = zeros(2^L)
+    for state = 1:2^L
+        for i = 1:L
+            j = i==L ? 1 : i+1
+            @inbounds diag[state] -= (((state >> (i-1))&1) == ((state >> (j-1))&1)) ? 1 : -1
+        end
+    end
+    diag
+end
+    
+function Hfunc!(C, B, diag, L)
+    N = length(diag)
+    @tturbo for state = 1:N
+        C[state] = diag[state] * B[state]
+    end
+    for i = 1:L
+        @tturbo for state = 1:N
+            newstate = (state&(~(2^L))) ⊻ (1<<(i-1))
+            c = newstate == 0
+            newstate = !c*newstate + c*N # remove if statement
+            C[newstate] -= B[state]
+        end
+    end
+end
+prepareHfunc!(L) = (C, B) -> Hfunc!(C, B, prepareDiag(L), L)
+
+function f_LinearMap(L)
+    H! = prepareHfunc!(L)
+    H_LinearMap = LinearMap(H!, 2^L, ismutating=true, issymmetric=true, isposdef=false)
+    d_LM, h_LM = partialschur(H_LinearMap, nev=1, which=SR())
+    d_LM.eigenvalues[1]
+end
+
+f_exact(L) = -2sum(abs(sin((n-1/2) * pi/L)) for n in 1:L)
+
+# %%
+# https://juliaphysics.github.io/PhysicsTutorials.jl/tutorials/general/quantum_ising/quantum_ising.html
+
+using SparseArrays
+using ArnoldiMethod
+⊗(x,y) = kron(x,y)
+
+function TransverseFieldIsing_sparse(;N,h)
+    id = [1 0; 0 1] |> sparse
+    σˣ = [0 1; 1 0] |> sparse
+    σᶻ = [1 0; 0 -1] |> sparse
+    
+    first_term_ops = fill(id, N)
+    first_term_ops[1] = σᶻ
+    first_term_ops[2] = σᶻ
+    
+    second_term_ops = fill(id, N)
+    second_term_ops[1] = σˣ
+    
+    H = spzeros(Int, 2^N, 2^N) # note the spzeros instead of zeros here
+    #for i in 1:N-1
+    for i in 1:N # periodic boundary condition
+        H -= foldl(⊗, first_term_ops)
+        first_term_ops = circshift(first_term_ops,1)
+    end
+    
+    for i in 1:N
+        H -= h*foldl(⊗, second_term_ops)
+        second_term_ops = circshift(second_term_ops,1)
+    end
+    H
+end
+
+function f_sparse(L)
+    H = TransverseFieldIsing_sparse(N=L, h=1)
+    d, h = partialschur(H; nev=1, which=SR())
+    d.eigenvalues[1]
+end
+
+# %%
+f_exact(20)
+
+# %%
+@time f_LinearMap(20)
+@time f_LinearMap(20)
+
+# %%
+@time f_sparse(20)
+@time f_sparse(20)
+
+# %%
+@time H = TransverseFieldIsing_sparse(N=20, h=1)
+@time d, h = partialschur(H; nev=1, which=SR())
+
+# %%
