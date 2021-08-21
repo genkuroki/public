801d9fdd007dce27db11fde477655febcf664365
Gen Kuroki <genkuroki@gmail.com>
2021-08-22 02:00:58 +0900

    add [83207-1557 1557; 317-1 1] example

--- 0014/2×2の分割表の独立性検定の信頼区間.jl
+++ 0014/2×2の分割表の独立性検定の信頼区間.jl
@@ -228,4 +228,109 @@ plot!(Q; ylim=(0, 0.15), ytick=0:0.05:1)
 
 plot(P, Q; size=(800, 300))
 
+# %% [markdown]
+# https://twitter.com/myuuuuun/status/1428579692219490304?s=21
+
+# %%
+println("="^30 * " Test data:\n")
+
+a, b, c, d = 83207-1557, 1557, 317-1, 1
+A = [a b; c d]
+@show A
+println()
+
+println("="^30 * " Calculated by R:\n")
+@show rcopy(fisher_test(A))[:p_value]
+@show rcopy(fisher_test(A))[:conf_int]
+println()
+@show rcopy(chisq_test(A, correct=false))[:p_value]
+@show rcopy(chisq_test(A))[:p_value]
+
+sleep(0.1)
+println()
+
+println("="^30 * " Calculated by Julia:\n")
+@show pvalue(A..., Val(:fisher))
+@show confidence_interval(A..., Val(:fisher))
+@show pvalue(A..., Val(:fisher_dos))
+@show confidence_interval(A..., Val(:fisher_dos))
+println()
+@show pvalue(A..., Val(:chisq))
+@show confidence_interval(A..., Val(:chisq))
+@show pvalue(A..., Val(:chisq_yates))
+@show confidence_interval(A..., Val(:chisq_yates))
+;
+
+# %%
+a, b, c, d = 83207-1557, 1557, 317-1, 1
+A = [a b; c d]
+
+null = multinomial_null(A...)
+L = 10^5
+X = rand(null, L)
+@time pvalue_fisher = vec(mapslices(A -> pvalue(A..., Val(:fisher)), X; dims=1))
+@time pvalue_fisher_dos = vec(mapslices(A -> pvalue(A..., Val(:fisher_dos)), X; dims=1))
+@time pvalue_chisq = vec(mapslices(A -> pvalue(A..., Val(:chisq)), X; dims=1))
+@time pvalue_chisq_yates = vec(mapslices(A -> pvalue(A..., Val(:chisq_yates)), X; dims=1));
+
+
+# %%
+p = range(0, 1; length=1001)
+P = plot()
+plot!(p, p -> ecdf(pvalue_fisher, p); label="fisher")
+plot!(p, p -> ecdf(pvalue_fisher_dos, p); label="fisher_dos", ls=:dash)
+plot!(p, p -> ecdf(pvalue_chisq, p); label="chisq")
+plot!(p, p -> ecdf(pvalue_chisq_yates, p); label="chisq_yates", ls=:dashdot)
+plot!([0, maximum(p)], [0, maximum(p)]; label="", color=:black, ls=:dot)
+plot!(; xlim=extrema(p), ylim=extrema(p))
+plot!(; size=(400, 400), legend=:topleft)
+plot!(; xlabel="significance level α", ylabel="probability of the first class errors")
+plot!(; xtick=range(extrema(p)...; length=11), ytick=range(extrema(p)...; length=11), tickfontsize=7, guidefontsize=9)
+plot!(; title="null = Multinomial(n=$(ntrials(null)), p=$(round.(probs(null); digits=4)))", titlefontsize=7)
+
+p = range(0, 0.1; length=101)
+Q = plot()
+plot!(p, p -> ecdf(pvalue_fisher, p); label="fisher")
+plot!(p, p -> ecdf(pvalue_fisher_dos, p); label="fisher_dos", ls=:dash)
+plot!(p, p -> ecdf(pvalue_chisq, p); label="chisq")
+plot!(p, p -> ecdf(pvalue_chisq_yates, p); label="chisq_yates", ls=:dashdot)
+plot!([0, maximum(p)], [0, maximum(p)]; label="", color=:black, ls=:dot)
+plot!(; xlim=extrema(p), ylim=extrema(p))
+plot!(; size=(400, 400), legend=:topleft)
+plot!(; xlabel="significance level α", ylabel="probability of the first class errors")
+plot!(; xtick=range(extrema(p)...; length=11), ytick=range(extrema(p)...; length=11), tickfontsize=7, guidefontsize=9)
+plot!(; title="null = Multinomial(n=$(ntrials(null)), p=$(round.(probs(null); digits=4)))", titlefontsize=7)
+
+plot(P, Q; size=(800, 400), left_margin=5Plots.mm, bottom_margin=5Plots.mm)
+
+# %%
+logω = range(-2.0, 0.5; length=500)
+P = plot()
+plot!(logω, t -> pvalue(A..., Val(:fisher); ω=10^t); label="fisher")
+plot!(logω, t -> pvalue(A..., Val(:fisher_dos); ω=10^t); label="fisher_dos", ls=:dash)
+plot!(logω, t -> pvalue(A..., Val(:chisq); ω=10^t); label="chisq")
+plot!(logω, t -> pvalue(A..., Val(:chisq_yates); ω=10^t); label="chisq_yates", ls=:dashdot)
+plot!(logω, 0.05ones(length(logω)); label="α=0.05", color=:red)
+#plot!(; legend=:topleft)
+x = [0.01, 0.03, 0.1, 0.3, 1.0, 3.0]
+plot!(; xtick = (log10.(x), string.(x)), ytick=0:0.1:1)
+plot!(; title="p-value functions for A = $A", titlefontsize=9)
+
+#Q = deepcopy(P)
+#plot!(Q; ylim=(0, 0.15), ytick=0:0.05:1)
+
+logω = range(-0.5, 0.5; length=500)
+Q = plot()
+plot!(logω, t -> pvalue(A..., Val(:fisher); ω=10^t); label="fisher")
+plot!(logω, t -> pvalue(A..., Val(:fisher_dos); ω=10^t); label="fisher_dos", ls=:dash)
+plot!(logω, t -> pvalue(A..., Val(:chisq); ω=10^t); label="chisq")
+plot!(logω, t -> pvalue(A..., Val(:chisq_yates); ω=10^t); label="chisq_yates", ls=:dashdot)
+plot!(logω, 0.05ones(length(logω)); label="α=0.05", color=:red)
+#plot!(; legend=:topleft)
+x = [0.3, 1.0, 3.0]
+plot!(; xtick = (log10.(x), string.(x)), ylim=(0, 0.1), ytick=0:0.01:1)
+plot!(; title="p-value functions for A = $A", titlefontsize=9)
+
+plot(P, Q; size=(800, 300))
+
 # %%
