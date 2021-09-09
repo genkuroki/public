2871dd5f1ed2423358611daf34d0318d6842f45e
Gen Kuroki <genkuroki@gmail.com>
2021-09-09 21:30:42 +0900

    created

---
+++ 0018/HMC leapfrog StaticArrays.jl
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.6.2
+#     language: julia
+#     name: julia-1.6
+# ---
+
+# %%
+module My
+
+using LinearAlgebra
+using ConcreteStructs: @concrete
+using ForwardDiff: gradient
+using Parameters: @unpack
+using Random: default_rng, randn!
+using StaticArrays
+
+@concrete struct LFProblem{dim} ϕ; H; F; dt; nsteps end
+
+"""ϕ should be a potential function."""
+function LFProblem(dim, ϕ; dt = 1.0, nsteps = 40)
+    H(x, v, param) = dot(v, v)/2 + ϕ(x, param)
+    F(x, param) = -gradient(x -> ϕ(x, param), x)
+    LFProblem{dim}(ϕ, H, F, dt, nsteps)
+end
+
+"""Numerically solve Hamilton's equation of motion with leapfrog method"""
+function solve(lf::LFProblem, x, v, param)
+    @unpack F, dt, nsteps = lf
+    v = v + F(x, param)*dt/2
+    x = x + v*dt
+    for _ in 2:nsteps
+        v = v + F(x, param)*dt
+        x = x + v*dt
+    end
+    v = v + F(x, param)*dt/2
+    x, v
+end
+
+"""Hamiltonian Monte Carlo"""
+function HMC(lf::LFProblem{dim}, param=nothing; niters = 10^5, burnin = 0, rng = default_rng(),
+        x0 = SVector{dim}(randn(rng, dim))) where dim
+    @unpack H = lf
+    vtmp = MVector{dim}(zeros(eltype(x0), dim))
+    x = x0
+    for _ in 1:burnin
+        v = SVector{dim}(randn!(rng, vtmp))
+        xnew, vnew = solve(lf, x, v, param)
+        dH = H(xnew, vnew, param) - H(x, v, param)
+        alpha = min(1, exp(-dH))
+        rand(rng) ≤ alpha && (x = xnew)
+    end
+    sample = Vector{typeof(x0)}(undef, niters)
+    for i in 1:niters
+        v = SVector{dim}(randn!(rng, vtmp))
+        xnew, vnew = solve(lf, x, v, param)
+        dH = H(xnew, vnew, param) - H(x, v, param)
+        alpha = min(1, exp(-dH))
+        rand(rng) ≤ alpha && (x = xnew)
+        sample[i] = x
+    end
+    sample
+end
+
+end
+
+# %%
+using Plots
+using BenchmarkTools
+using StaticArrays
+using LinearAlgebra
+using KernelDensity
+using Statistics
+
+# %%
+A = @SMatrix [
+     1  1/2
+    1/2  1
+]
+param = (; A = A)
+ϕ(x, param) = dot(x, param.A, x)/2
+lf = My.LFProblem(2, ϕ)
+
+# %%
+@time sample = My.HMC(lf, param)
+@time sample = My.HMC(lf, param)
+@time sample = My.HMC(lf, param);
+
+# %%
+X, Y = first.(sample), last.(sample)
+d = InterpKDE(kde((X, Y)))
+x, y = range(extrema(X)...; length=201), range(extrema(Y)...; length=201)
+heatmap(x, y, (x, y) -> pdf(d, x, y); size=(450, 400))
+
+# %%
+f(n) = mean(x -> x*x', @view sample[1:n])
+
+n = 1:2000
+S = f.(n)
+S11 = (S -> S[1,1]).(S)
+S22 = (S -> S[2,2]).(S)
+S12 = (S -> S[1,2]).(S)
+
+plot()
+plot!(S11; label="s11", c=1)
+hline!([inv(A)[1,1]]; label="", c=1, ls=:dash)
+plot!(S22; label="s22", c=2)
+hline!([inv(A)[2,2]]; label="", c=2, ls=:dash)
+plot!(S12; label="s12", c=3)
+hline!([inv(A)[1,2]]; label="", c=3, ls=:dash)
+
+# %%
