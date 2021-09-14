431b50f5af76f7de870e7966c489bf4eda5511f3
Gen Kuroki <genkuroki@gmail.com>
2021-09-14 11:36:22 +0900

    updated

--- 0020/バックスラッシュ一発で最小二乗法.jl
+++ 0020/バックスラッシュ一発で最小二乗法.jl
@@ -30,6 +30,43 @@ scatter(x, y; label="data", color=1)
 plot!(xs, f; label="true line", color=1, ls=:dash)
 plot!(xs, g; label="regression line", color=2, lw=2)
 
+# %% [markdown]
+# https://discourse.julialang.org/t/differences-in-a-b-for-sparse-and-nonsparse-rank-deficient-a/66917
+
 # %%
+using LinearAlgebra, SparseArrays
+
+Is = [9, 8, 9, 12, 2, 5, 6, 11, 8, 12]
+Js = [1, 3, 4, 4, 5, 5, 7, 7, 10, 10]
+Vs = [0.046668312772398135, 0.0732172552606527, 0.07228019735547542, 0.27500506619596665, 0.8821260519923395, 0.188438924730004, 0.22883463110234215, 0.17705262933291666, 0.297278962545636, 0.07770904672896628]
+A = sparse(Is, Js, Vs)
+b = [0.0
+ 0.26743848063303416
+ 0.0
+ 0.0
+ 0.057129952809003536
+ 0.20824640373847988
+ 0.0
+ 0.1580262484804872
+ 0.09667497046894227
+ 0.0
+ 0.16112322314768995
+ 0.27723584012581337]
+
+@show qr(A) \ b ≈ A \ b # True
+@show A \ b ≈ pinv(Matrix(A))*b # False
+@show qr(Matrix(A), Val(true)) \ b ≈ pinv(Matrix(A))*b # True;
+
+# %%
+typeof(A), typeof(b)
+
+# %%
+@show A \ b;
+
+# %%
+@show Matrix(A) \ b;
+
+# %%
+@show pinv(Matrix(A)) * b;
 
 # %%
