2d469f47d9fb24e0a306a5495c8da44ff38237c3
Gen Kuroki <genkuroki@gmail.com>
2021-09-17 07:45:16 +0900

    created

---
+++ 0021/plot tstat and chisqatat.jl
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.6.2
+#     language: julia
+#     name: julia-1.6
+# ---
+
+# %%
+using Distributions
+using StatsPlots
+
+function plot_tstat(;
+        n = 10,
+        L = 10^5,
+        dist = Normal(1, 2),
+        xlim = (-5, 5),
+        xtick = -5:5,
+        bin = range(extrema(xtick)...; length=51),
+    )
+    tstat(sample) = (mean(sample) - mean(dist))/√(var(sample)/n)
+    T = [tstat(rand(dist, n)) for _ in 1:L]
+    histogram(T; norm=true, alpha=0.3, label="(x - μ)/√(u²/n)", xlim, xtick, bin)
+    plot!(TDist(n-1); lw=1.5, label="Tdist(n - 1)")
+    diststr = replace("$dist", r"\{.*\}"=>"")
+    title!("$diststr,  n = $n"; titlefontsize=10)
+end
+
+function plot_chisqstat(;
+        n = 10,
+        L = 10^5,
+        dist = Normal(1, 2),
+        xlim = (-1, 3n),
+        xtick = 0:(n÷2):3n,
+        bin = range(extrema(xtick)...; length=51),
+    )
+    X = (n-1)*[var(rand(dist, n)) for _ in 1:L]/var(dist)
+    histogram(X; norm=true, alpha=0.3, label="(n - 1)u²/σ²", xlim, xtick, bin)
+    plot!(Chisq(n-1); lw=1.5, label="Chisq(n - 1)")
+    diststr = replace("$dist", r"\{.*\}"=>"")
+    title!("$diststr,  n = $n"; titlefontsize=10)
+end
+
+function plot_both(;
+        n = 10,
+        L = 10^5,
+        dist = Normal(1, 2),
+        xlim1 = (-5, 5),
+        xtick1 = -5:5,
+        xlim2 = (-1, 3n),
+        xtick2 = 0:(n÷2):3n,
+        bin1 = range(extrema(xtick1)...; length=51),
+        bin2 = range(extrema(xtick2)...; length=51),
+    )
+    P = plot_tstat(; n, L, dist, xlim=xlim1, xtick=xtick1, bin=bin1)
+    Q = plot_chisqstat(; n, L, dist, xlim=xlim2, xtick=xtick2, bin=bin2)
+    plot(P, Q; size=(800, 300))
+end
+
+# %%
+plot_both()
+
+# %%
+plot_both(dist = Uniform(0, 1))
+
+# %%
+plot_both(dist = Gamma(2, 1))
+
+# %%
+plot_both(dist = Gamma(4, 1))
+
+# %%
+plot_both(dist = TDist(4); xlim2=(-1, 50))
+
+# %%
+plot_both(dist = Exponential())
+
+# %%
+plot_both(dist = Exponential(), n = 20)
+
+# %%
+plot_both(dist = Exponential(), n = 50)
+
+# %%
+plot_both(dist = Exponential(), n = 100)
+
+# %%
+plot_both(dist = Exponential(), n = 200)
+
+# %%
+plot_both(dist = Exponential(), n = 400)
+
+# %%
