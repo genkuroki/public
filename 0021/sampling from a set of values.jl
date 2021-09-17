159271306d83d39e4c277c7afe12aba96666d982
Gen Kuroki <genkuroki@gmail.com>
2021-09-17 11:59:28 +0900

    created

---
+++ 0021/sampling from a set of values.jl
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Julia 1.6.2
+#     language: julia
+#     name: julia-1.6
+# ---
+
+# %%
+using StatsBase
+
+a = sample([2, 3, 5], ProbabilityWeights([0.2, 0.3, 0.5]), 10^6)
+[(x, count(==(x), a)/length(a)) for x in sort(unique(a))]
+
+# %%
+?sample
+
+# %%
+using Distributions
+
+a = rand(DiscreteNonParametric([2, 3, 5], [0.2, 0.3, 0.5]), 10^6)
+[(x, count(==(x), a)/length(a)) for x in sort(unique(a))]
+
+# %%
+?DiscreteNonParametric
+
+# %%
