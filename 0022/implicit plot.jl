3420529d7cd5241ac0750b68ed2823c242c5bf59
Gen Kuroki <genkuroki@gmail.com>
2021-10-06 02:09:20 +0900

    updated

--- 0022/implicit plot.jl
+++ 0022/implicit plot.jl
@@ -15,11 +15,30 @@
 
 # %%
 using Plots
-h(x, y) = iszero(y) ? x : x^(x^y) - y
+h(x, y) = x^(x^y) - y
 
 x = range(0, 0.11; length=1000)
 y = range(0, 1.1; length=1000)
 contour(x, y, h; levels=zeros(1), color=:red, colorbar=false)
+plot!(; xlim=extrema(x), ylim=extrema(y), size=(400, 400))
+
+# %%
+using Plots
+
+function f(x, y, n)
+    z = x^y
+    for i in 1:n
+        z = x^z
+    end
+    z
+end
+
+x = range(0, 2.1, length=1000)
+y = range(0, 10.1; length=1000)
+plot(; colorbar=false)
+for n in 1:4
+    contour!(x, y, (x, y) -> f(x, y, n) - y; levels=zeros(1), c=n, ls=:auto)
+end
 plot!(; xlim=extrema(x), ylim=extrema(y), size=(400, 400))
 
 # %%
