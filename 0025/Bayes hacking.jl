a065d79c17090494d3efef2c5a5226e1bd2db351
Gen Kuroki <genkuroki@gmail.com>
2021-12-02 08:07:18 +0900

    updated

--- 0025/Bayes hacking.jl
+++ 0025/Bayes hacking.jl
@@ -20,6 +20,9 @@ using Plots
 using StatsFuns: logit, logistic
 using Roots
 using StatsBase: ecdf
+using Memoization
+
+x ⪅ y = x < y || x ≈ y
 
 # %%
 """
@@ -31,7 +34,7 @@ posterior_dist(n, k; a=1, b=a) = Beta(k + a, n - k + b)
 """
 pvalue_bayes(n, p, k; a, b) < α ⇔ p ∉ (ベイズ版100(1-α)%信用区間)
 """
-function pvalue_bayes(n, p, k; a = 1, b = a)
+@memoize function pvalue_bayes(n, p, k; a = 1, b = a)
     posterior = posterior_dist(n, k; a, b)
     v0 = logpdf(posterior, p)
     f(t) = logpdf(posterior, logistic(t)) - v0
@@ -70,11 +73,61 @@ function bayeshacking(N, p; α = 0.05, L = 10^4, a = 1, b = a)
 end
 
 # %%
-numtrials = bayeshacking(10^3, 0.5)
-[(n, ecdf(numtrials)(n)) for n in (10, 100, 1000)]
+numtrials_bhack = bayeshacking(10^3, 0.5)
+[(n, ecdf(numtrials_bhack)(n)) for n in (10, 100, 1000)]
 
 # %%
-plot(n -> ecdf(numtrials)(n), 0, 10^3; label="", xtick=0:100:1000)
+plot(n -> ecdf(numtrials_bhack)(n), 0, 10^3; label="", xtick=0:100:1000)
 title!("ecdf(number of trials for Bayes hacking)"; titlefontsize=12)
+
+# %%
+"""通常のP値函数(exact版)"""
+@memoize function pvalue_exact(n, p, k)
+    bin = Binomial(n, p)
+    p0 = pdf(bin, k)
+    s = sum(pdf(bin, j) for j in support(bin) if pdf(bin, j) ⪅ p0)
+    min(1, s)
+end
+
+"""通常のP値函数(正規分布近似)"""
+@memoize function pvalue_normal(n, p, k)
+    bin = Binomial(n, p)
+    normal = Normal(mean(bin), std(bin))
+    min(1, 2cdf(normal, k), 2ccdf(normal, k))
+end
+
+"""
+Bernoulli(p)分布の独立試行をNまで行うとき、
+P値がα未満になるまでの試行回数をL個計算
+"""
+function phacking(pvalue_func, N, p; α = 0.05, L = 10^4)
+    numtrials = fill(N + 1, L)
+    for i in 1:L
+        k = 0
+        for n in 1:N
+            k += rand(Bernoulli(p))
+            if pvalue_func(n, p, k) < α
+                numtrials[i] = n
+                break
+            end
+        end
+    end
+    numtrials
+end
+
+# %%
+numtrials_phack_exact = phacking(pvalue_exact, 10^3, 0.5)
+[(n, ecdf(numtrials_phack_exact)(n)) for n in (10, 100, 1000)]
+
+# %%
+numtrials_phack_normal = phacking(pvalue_normal, 10^3, 0.5)
+[(n, ecdf(numtrials_phack_normal)(n)) for n in (10, 100, 1000)]
+
+# %%
+plot(; legend=:bottomright)
+plot!(n -> ecdf(numtrials_bhack)(n), 0, 10^3; label="Bayes hacking", xtick=0:100:1000)
+plot!(n -> ecdf(numtrials_phack_exact)(n), 0, 10^3; label="p-hacking (exact p-value)", xtick=0:100:1000, ls=:dash)
+plot!(n -> ecdf(numtrials_phack_normal)(n), 0, 10^3; label="p-hacking (normal dist. approx.)", xtick=0:100:1000, ls=:dashdot)
+title!("ecdf(number of trials)"; titlefontsize=12)
 
 # %%
