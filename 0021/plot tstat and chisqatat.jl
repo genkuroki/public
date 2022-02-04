63044162e8218a8d28cd3106cef810ee14a62e1c
Gen Kuroki <genkuroki@gmail.com>
2022-02-05 01:27:32 +0900

    add examples, with multithread

--- 0021/plot tstat and chisqatat.jl
+++ 0021/plot tstat and chisqatat.jl
@@ -7,16 +7,18 @@
 #       extension: .jl
 #       format_name: hydrogen
 #       format_version: '1.3'
-#       jupytext_version: 1.11.2
+#       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Julia 1.6.3
+#     display_name: Julia 1.7.1
 #     language: julia
-#     name: julia-1.6
+#     name: julia-1.7
 # ---
 
 # %%
 using Distributions
 using StatsPlots
+using Random
+using QuadGK
 
 function plot_tstat(;
         n = 10,
@@ -25,12 +27,17 @@ function plot_tstat(;
         xlim = (-5, 5),
         xtick = -5:5,
         bin = range(extrema(xtick)...; length=51),
+        skewnessfunc = skewness
     )
     tstat(sample) = (mean(sample) - mean(dist))/√(var(sample)/n)
-    T = [tstat(rand(dist, n)) for _ in 1:L]
+    T = Vector{Float64}(undef, L)
+    tmp = [Vector{Float64}(undef, n) for i in 1:Threads.nthreads()]
+    Threads.@threads for i in 1:L
+        T[i] = tstat(rand!(dist, tmp[Threads.threadid()]))
+    end
     histogram(T; norm=true, alpha=0.3, label="(x - μ)/√(u²/n)", xlim, xtick, bin)
     plot!(TDist(n-1); lw=1.5, label="TDist(n - 1)")
-    sk = skewness(dist)
+    sk = skewnessfunc(dist)
     diststr = replace("$dist", r"\{.*\}"=>"")
     title!("$diststr,  n = $n\nskewness = $(round(sk; digits=3))"; titlefontsize=10)
 end
@@ -39,6 +46,7 @@ function plot_chisqstat(;
         n = 10,
         L = 10^5,
         dist = Normal(1, 2),
+        kurtosisfunc = kurtosis
     )
     X = (n-1)*[var(rand(dist, n)) for _ in 1:L]/var(dist)
     m, s = mean(X), std(X)
@@ -46,7 +54,7 @@ function plot_chisqstat(;
     bin = range(0, xlim[2]; length=round(Int, 51/(1 - xlim[1]/xlim[2])))
     histogram(X; norm=true, alpha=0.3, label="(n - 1)u²/σ²", xlim, bin)
     plot!(Chisq(n-1); lw=1.5, label="Chisq(n - 1)")
-    kurt = kurtosis(dist)
+    kurt = kurtosisfunc(dist)
     if isfinite(kurt)
         M, S = n - 1, (n - 1)*√((kurt + 3)/n - (n - 3)/(n*(n - 1)))
         plot!(Normal(M, S); lw=1.5, ls=:dash, label="normal dist.")
@@ -62,10 +70,25 @@ function plot_both(;
         xlim1 = (-5, 5),
         xtick1 = -5:5,
         bin1 = range(extrema(xtick1)...; length=51),
+        skewnessfunc = skewness,
+        kurtosisfunc = kurtosis,
+        kwargs...
     )
-    P = plot_tstat(; n, L, dist, xlim=xlim1, xtick=xtick1, bin=bin1)
-    Q = plot_chisqstat(; n, L, dist)
-    plot(P, Q; size=(800, 300))
+    P = plot_tstat(; n, L, dist, xlim=xlim1, xtick=xtick1, bin=bin1, skewnessfunc)
+    Q = plot_chisqstat(; n, L, dist, kurtosisfunc)
+    plot(P, Q; size=(800, 300), kwargs...)
+end
+
+function myskewness(dist)
+    μ, σ = mean(dist), std(dist)
+    f(x) = ((x - μ)/σ)^3 * pdf(dist, x)
+    quadgk(f, extrema(dist)...)[1]
+end
+
+function mykurtosis(dist)
+    μ, σ = mean(dist), std(dist)
+    f(x) = ((x - μ)/σ)^4 * pdf(dist, x)
+    quadgk(f, extrema(dist)...)[1] - 3
 end
 
 # %%
@@ -80,7 +103,7 @@ end
 
 # %%
 for n in (10, 20, 50, 100, 200, 400)
-    plot_both(; dist = TDist(4), n) |> display
+    plot_both(; dist = TDist(4), n, kurtosisfunc = kurtosis) |> display
 end
 
 # %%
@@ -106,6 +129,59 @@ end
 # %%
 for n in (10, 20, 50, 100, 200, 400)
     plot_both(; dist = Exponential(), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = Beta(0.5, 0.5), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = Beta(0.2, 0.2), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = Beta(0.15, 0.25), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = Beta(0.1, 0.3), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = TDist(4.01), n) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = MixtureModel([Normal(), Normal(10, 1)], [0.95, 0.05]), n,
+        skewnessfunc=myskewness, kurtosisfunc=mykurtosis,
+        titlefontsize=8, topmargin=1Plots.cm) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = MixtureModel([Normal(-4, 1), Normal(4, 1)], [0.5, 0.5]), n,
+        skewnessfunc=myskewness, kurtosisfunc=mykurtosis,
+        titlefontsize=8, topmargin=1Plots.cm) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = MixtureModel([Normal(-4, 1), Normal(4, 1)], [0.6, 0.4]), n,
+        skewnessfunc=myskewness, kurtosisfunc=mykurtosis,
+        titlefontsize=8, topmargin=1Plots.cm) |> display
+end
+
+# %%
+for n in (10, 20, 50, 100, 200, 400)
+    plot_both(; dist = MixtureModel([Normal(-4, 1), Normal(4, 1)], [0.8, 0.2]), n,
+        skewnessfunc=myskewness, kurtosisfunc=mykurtosis,
+        titlefontsize=8, topmargin=1Plots.cm) |> display
 end
 
 # %%
