e25545531759a0f98e41e6f560f73008c94896ef
Gen Kuroki <genkuroki@gmail.com>
2022-06-29 10:19:22 +0900

    add posterior

--- 0032/likelihood and P-value functions of binomial distribution model for data n=1000, k=867.jl
+++ 0032/likelihood and P-value functions of binomial distribution model for data n=1000, k=867.jl
@@ -17,18 +17,19 @@
 # %%
 using Distributions
 using StatsPlots
-default(fmt=:png, size=(400, 250), titlefontsize=10)
+default(fmt=:png, size=(400, 250), titlefontsize=10, tickfontsize=7)
 
 n, k = 1000, 867
 
 P1 = plot(p -> pdf(Binomial(n, p), k), 0.82, 0.92; label="",
-    xguide="x", yguide="likelihood", title="data: n=$n, k=$k",
-    xtick=0:0.01:1, )
+    xguide="p", yguide="likelihood",
+    title="likelihood function for data n=$n, k=$k",
+    xtick=0:0.01:1)
 
 # %%
 using Distributions
 using StatsPlots
-default(fmt=:png, size=(400, 250), titlefontsize=10)
+default(fmt=:png, size=(400, 250), titlefontsize=10, tickfontsize=7)
 
 function pvalue_clopper_pearson(n, k, p)
     bin = Binomial(n, p)
@@ -43,18 +44,61 @@ end
 
 n, k = 1000, 867
 α = 0.05
+@show n, k
 ci = @eval @show confint_clopper_pearson(n, k; α=$α)
 
 P2 = plot(p -> pvalue_clopper_pearson(n, k, p), 0.82, 0.92; label="",
-    xguide="x", yguide="P-value", title="data: n=$n, k=$k",
+    xguide="p", yguide="P-value", title="P-value function for data n=$n, k=$k",
     xtick=0:0.01:1, ytick=0:0.1:1)
-plot!(ci, fill(α, 2); label="$(100(1-α))% conf.int.")
+plot!(ci, fill(α, 2); label="$(100(1-α))% conf.int.", lw=2, c=:red)
+
+# %%
+pvalue_clopper_pearson.(n, k, ci)
 
 # %%
 plot(P1, P2; size=(800, 250),
     leftmargin=4Plots.mm, bottommargin=4Plots.mm)
 
 # %%
-pvalue_clopper_pearson.(n, k, ci)
+using Distributions
+using Roots
+using StatsPlots
+default(fmt=:png, size=(400, 250), titlefontsize=10, tickfontsize=7)
+
+posterior_conjugate(n, k; a=1, b=a) = Beta(a+k, n-k+b)
+
+function credint(posterior; α=0.05)
+    p_L, p_U = quantile.(posterior, (α/2, 1-α/2))
+    [p_L, p_U]
+end
+
+n, k = 1000, 867
+α = 0.05
+a, b = 1, 1
+
+@show n, k
+@eval @show posterior = posterior_conjugate(n, k; a=$a, b=$b)
+bci = @eval @show credint(posterior; α=$α)
+
+P3 = plot(p -> pdf(posterior, p), 0.82, 0.92; label="",
+    xguide="p", yguide="density",
+    title="posterior for data n=$n, k=$k, prior Beta($a,$b)",
+    xtick=0:0.01:1, titlefontsize=9)
+plot!(bci, zeros(2); label="$(100(1-α))% cred.int.", lw=2, c=:red)
+plot!(p -> pdf(posterior, p), 0.82, first(bci); label="",
+    fillrange=0, c=1, fc=:red, fa=0.3)
+plot!(p -> pdf(posterior, p), last(bci), 0.92; label="",
+    fillrange=0, c=1, fc=:red, fa=0.3)
+
+# %%
+bci - ci
+
+# %%
+plot(P2, P3; size=(800, 250),
+    leftmargin=4Plots.mm, bottommargin=4Plots.mm)
+
+# %%
+plot(P1, P2, P3, P3; size=(800, 500), layout=(2, 2),
+    leftmargin=4Plots.mm, bottommargin=4Plots.mm)
 
 # %%
