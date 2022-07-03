81fed37f8b1c62e5e7723da6c2306f213a90e62d
Gen Kuroki <genkuroki@gmail.com>
2022-07-03 09:34:42 +0900

    revised

--- 0033/likelihood function for binomial model.jl
+++ 0033/likelihood function for binomial model.jl
@@ -23,13 +23,25 @@ plot(p -> p*(1-p), 0, 1; label="p -> p*(1-p)")
 # %%
 n, k = 100, 90
 plot(legend=:topleft)
-plot!(p -> pdf(Binomial(n, p), k), 0.7, 1; label="p -> pdf(Binomial(n, p), k)")
-plot!(p -> pdf(Normal(n*p, √(n*p*(1-p))), k); label="p -> pdf(Normal(n*p, √(n*p*(1-p))), k)", ls=:dash)
+plot!(p -> pdf(Binomial(n, p), k), 0.7, 1;
+    label="p -> pdf(Binomial(n, p), k)")
+plot!(p -> pdf(Normal(n*p, √(n*p*(1-p))), k); 
+    label="p -> pdf(Normal(n*p, √(n*p*(1-p))), k)", ls=:dash)
 
 # %%
 n, k = 1000, 990
 plot(legend=:topleft)
-plot!(p -> pdf(Binomial(n, p), k), 0.97, 1; label="p -> pdf(Binomial(n, p), k)")
-plot!(p -> pdf(Normal(n*p, √(n*p*(1-p))), k); label="p -> pdf(Normal(n*p, √(n*p*(1-p))), k)", ls=:dash)
+plot!(p -> pdf(Binomial(n, p), k), 0.97, 1;
+    label="p -> pdf(Binomial(n, p), k)")
+plot!(p -> pdf(Normal(n*p, √(n*p*(1-p))), k);
+    label="p -> pdf(Normal(n*p, √(n*p*(1-p))), k)", ls=:dash)
+
+# %%
+n, p = 100, 0.9
+plot(legend=:topleft)
+plot!(k -> pdf(Binomial(n, p), round(Int, k)), 70, 100;
+    label="k -> pdf(Binomial(n, p), round(Int, k))")
+plot!(k -> pdf(Normal(n*p, √(n*p*(1-p))), k);
+    label="k -> pdf(Normal(n*p, √(n*p*(1-p))))", ls=:dash)
 
 # %%
