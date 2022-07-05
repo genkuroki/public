c2d2d88a5c17d470ae1e60587c2b1fef60a79df5
Gen Kuroki <genkuroki@gmail.com>
2022-07-05 19:24:20 +0900

    add baysian P-value functions

--- 0033/Reimplementation of binom.test, fisher.test, poisson.test.jl
+++ 0033/Reimplementation of binom.test, fisher.test, poisson.test.jl
@@ -103,3 +103,72 @@ plot(datas, pvals; label, ls, xtick=datas, ytick, xguide="data", yguide)
 title!("Poisson test for λ=$λ")
 
 # %%
+using Distributions
+using StatsPlots
+default(fmt=:png, titlefontsize=10)
+
+safediv(x, y) = x == 0 ? float(x) : y == Inf ? zero(float(y)) : x/y
+
+function pvalue_wilson(model::Binomial, data)
+    n = ntrials(model)
+    p = succprob(model)
+    mu = n*p
+    se = √(n*p*(1-p))
+    z = safediv(data - mu, se)
+    2ccdf(Normal(), abs(z))
+end
+
+function posterior(model::Binomial, data; conjprior=Beta(0.5, 0.5))
+    n = ntrials(model)
+    a, b = params(conjprior)
+    Beta(a + data, b + n - data)
+end
+
+function pvalue_bayes(model::Binomial, data; conjprior=Beta(0.5, 0.5))
+    posterior_dist = posterior(model, data; conjprior)
+    p = succprob(model)
+    min(1, 2cdf(posterior_dist, p), 2ccdf(posterior_dist, p))
+end
+
+n, data = 20, 6
+conjprior = Beta(0.5, 0.5)
+plot(p -> pvalue_wilson(Binomial(n, p), data); label="Wilson")
+plot!(p -> pvalue_bayes(Binomial(n, p), data; conjprior);
+    label="Bayes (prior = Beta$(params(conjprior)))", ls=:dash)
+plot!(xtick=0:0.1:1, ytick=0:0.05:1)
+plot!(xguide="parameter p", yguide="P-value")
+title!("P-value functions for binomial data n=$n, x=$data")
+
+# %%
+using StatsBase: ecdf
+
+function pvalue_oddsratio_wald(a, b, c, d, ω=1.0)
+    logORhat = log(safediv(a*d, b*c))
+    SEhat_logORhat = √(1/a + 1/b + 1/c + 1/d)
+    2ccdf(Normal(), safediv(abs(logORhat - log(ω)), SEhat_logORhat))
+end
+
+function make_pvalue_oddsratio_bayes(a, b, c, d, ω=1.0; L=10^6)
+    (a+b == 0 || c+d == 0) && return ω -> 1.0
+    bin1, bin2 = Binomial(a+b, safediv(a, a+b)), Binomial(c+d, safediv(c, c+d))
+    post1, post2 = posterior(bin1, a), posterior(bin2, c)
+    p, q = rand(post1, L), rand(post2, L)
+    or = @. safediv(p*(1-q), (1-p)*q)
+    ecdf_or = ecdf(or)
+    pvalue_oddsratio_bayes(ω) = min(1, 2ecdf_or(ω), 2(1-ecdf_or(ω)))
+end
+
+a, b, c, d = 15, 5, 9, 11
+pvalue_oddsratio_bayes = make_pvalue_oddsratio_bayes(a, b, c, d)
+
+xtick = Any[0.05, 0.1, 0.2, 0.3, 0.5, 1, 2, 3, 5, 10, 20, 30, 50, 100]
+plot(ω -> pvalue_oddsratio_wald(a, b, c, d, ω), 0.3, 30; label="Wald")
+plot!(ω -> pvalue_oddsratio_bayes(ω), 0.3, 30;
+    label="Bayes (prior = Beta$(params(conjprior)))", ls=:dash)
+vline!([1]; label="", c=:black, ls=:dot)
+plot!(xscale=:log10, xtick=(xtick, string.(xtick)), ytick=0:0.05:1)
+plot!(xguide="parameter OR", yguide="P-value")
+title!("P-value functions for data [$a $b; $c $d]")
+plot!(legend=:topleft)
+
+# %%
