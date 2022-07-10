3c57496b02011f13faf9a0c4e1a89e27db7f7c46
Gen Kuroki <genkuroki@gmail.com>
2022-07-10 16:18:53 +0900

    updated

--- 0033/hypergeometric distributions.jl
+++ 0033/hypergeometric distributions.jl
@@ -21,6 +21,7 @@
 using Distributions
 using StatsPlots
 default(fmt=:png, titlefontsize=10, tickfontsize=6)
+using Memoization
 
 function _likelihood(w, r; a=43, b=7, c=24, d=26, W=5000, R=1000)
     a+b ≤ w+r || return 0.0
@@ -32,7 +33,7 @@ function _likelihood(w, r; a=43, b=7, c=24, d=26, W=5000, R=1000)
     likA * likB
 end
 
-function calculate_likelihoods(; a=43, b=7, c=24, d=26, W=5000, R=1000)
+@memoize function calculate_likelihoods(; a=43, b=7, c=24, d=26, W=5000, R=1000)
     lik = Matrix{Float64}(undef, W+1, R+1)
     Ws, Rs = 0:W, 0:R 
     Threads.@threads for j in eachindex(Rs)
@@ -57,21 +58,22 @@ function plot_likelihoods(f = identity;
     end
     plot_line && plot!(x -> (d/c)*(x - (W-c)) + (R-d); label="", c=:cyan)
     plot!(xlim=extrema(Ws[xidx]), ylim=extrema(Rs[yidx]))
+    plot!(xguide="w", yguide="r")
     title!(title)
     plot!(; kwargs...)
 end
 
 # %%
-plot_likelihoods()
+plot_likelihoods(; title="likelihoods")
 
 # %%
-plot_likelihoods(ws=4700:5000, rs=700:1000)
+plot_likelihoods(ws=4700:5000, rs=700:1000, title="likelihoods")
 
 # %%
-plot_likelihoods(ws=4900:5000, rs=900:1000)
+plot_likelihoods(ws=4900:5000, rs=900:1000, title="likelihoods")
 
 # %%
-plot_likelihoods(ws=4960:4977, rs=960:975)
+plot_likelihoods(ws=4960:4977, rs=960:975, title="likelihoods")
 
 # %%
 plot_likelihoods(ws=4700:4950, rs=700:950, title="likelihoods")
@@ -81,18 +83,170 @@ plot_likelihoods(ws=4700:4900, rs=700:900, title="likelihoods")
 
 # %%
 plot_likelihoods(p -> log(p + 1e-4); colorbar=false,
-    plot_line=true, title="line: x-(5000-24) : y-(1000-26) = 24 : 26")
+    plot_line=true, title="likelihoods\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
 
 # %%
-plot_likelihoods(p -> log(p + 1e-4); ws=3500:5000, colorbar=false,
-    plot_line=true, title="line: x-(5000-24) : y-(1000-26) = 24 : 26")
+plot_likelihoods(p -> log(p + 1e-4); ws=3000:5000, colorbar=false,
+    plot_line=true, title="likelihoods\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
 
 # %%
 plot_likelihoods(p -> log(p + 1e-4); ws=4700:5000, rs=700:1000, colorbar=false,
-    plot_line=true, title="line: x-(5000-24) : y-(1000-26) = 24 : 26")
+    plot_line=true, title="likelihoods\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
 
 # %%
 plot_likelihoods(p -> log(p + 1e-4); ws=4900:5000, rs=900:1000, colorbar=false,
-    plot_line=true, title="line: x-(5000-24) : y-(1000-26) = 24 : 26")
+    plot_line=true, title="likelihoods\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+f(x) = (26/24)*(x - (5000-24)) + (1000-26)
+plot_likelihoods(p -> log(p + 1e-4); ws=3000:5000, colorbar=false)
+plot!(x -> f(x); label="likelihoods", c=:white)
+
+# %%
+(; lik, Ws, Rs) = calculate_likelihoods()
+g(x) = @views f(x) ≥ 0 ? maximum(lik[1 .+ (0:round(Int, x)), 1 .+ (0:round(Int, f(x)))]) : 0.0
+
+# %%
+plot(g, 4000, 5000; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(g, 4000, 5000; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(g, 4700, 5000; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(g, 4800, 5000; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(g, 4800, 5000-50; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(g, 4950, 5000-20; label="", xguide="x")
+title!("maximum of likelihoods for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+function _pvalue(w, r; a=43, b=7, c=24, d=26, W=5000, R=1000)
+    a+b ≤ w+r || return 0.0
+    c+d ≤ W+R-(w+r) || return 0.0
+    hgA = Hypergeometric(w, r, a+b)
+    hgB = Hypergeometric(W-w, R-r, c+d)
+    pvalA = min(2cdf(hgA, a), 2ccdf(hgA, a-1))
+    pvalB = min(2cdf(hgB, c), 2ccdf(hgB, c-1))
+    pvalA * pvalB
+end
+
+@memoize function calculate_pvalues(; a=43, b=7, c=24, d=26, W=5000, R=1000)
+    pval = Matrix{Float64}(undef, W+1, R+1)
+    Ws, Rs = 0:W, 0:R 
+    Threads.@threads for j in eachindex(Rs)
+        for i in eachindex(Ws)
+            pval[i, j] = _pvalue(Ws[i], Rs[j]; a, b, c, d, W, R)
+        end
+    end
+    (; pval, Ws, Rs)
+end
+
+function plot_pvalues(f = identity;
+        a=43, b=7, c=24, d=26, W=5000, R=1000,
+        ws=0:W, rs=0:R, color=:CMRmap, colorbar=true,
+        plot_line=false, plot_contourf=false, 
+        title="", kwargs...)
+    (; pval, Ws, Rs) = calculate_pvalues(; a, b, c, d, W, R)
+    xidx, yidx = 1 .+ ws, 1 .+ rs
+    if plot_contourf
+        contourf(Ws[xidx], Rs[yidx], f.(pval[xidx, yidx])'; color, colorbar, kwargs...)
+    else
+        heatmap(Ws[xidx], Rs[yidx], f.(pval[xidx, yidx])'; color, colorbar, kwargs...)
+    end
+    plot_line && plot!(x -> (d/c)*(x - (W-c)) + (R-d); label="", c=:cyan)
+    plot!(xlim=extrema(Ws[xidx]), ylim=extrema(Rs[yidx]))
+    plot!(xguide="w", yguide="r")
+    title!(title)
+end
+
+# %%
+plot_pvalues(; title="pvalues")
+
+# %%
+plot_pvalues(ws=4700:5000, rs=700:1000, title="pvalues")
+
+# %%
+plot_pvalues(ws=4900:5000, rs=900:1000, title="pvalues")
+
+# %%
+plot_pvalues(ws=4960:4977, rs=960:975, title="pvalues")
+
+# %%
+plot_pvalues(ws=4700:4950, rs=700:950, title="pvalues")
+
+# %%
+plot_pvalues(ws=4700:4900, rs=700:900, title="pvalues")
+
+# %%
+plot_pvalues(p -> log(p + 1e-4); colorbar=false,
+    plot_line=true, title="pvalues\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot_pvalues(p -> log(p + 1e-4); ws=3000:5000, colorbar=false,
+    plot_line=true, title="pvalues\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot_pvalues(p -> log(p + 1e-4); ws=4700:5000, rs=700:1000, colorbar=false,
+    plot_line=true, title="pvalues\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot_pvalues(p -> log(p + 1e-4); ws=4900:5000, rs=900:1000, colorbar=false,
+    plot_line=true, title="pvalues\nline: x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+f(x) = (26/24)*(x - (5000-24)) + (1000-26)
+plot_pvalues(p -> log(p + 1e-4); ws=3000:5000, colorbar=false)
+plot!(x -> f(x); label="pvalues", c=:white)
+
+# %%
+α = 0.1
+plot_pvalues(p -> p ≥ α ? p : 0.0; ws=3000:5000, clims=(-0.1, 1.8), title="max($α, pvalues)")
+
+# %%
+α = 0.05
+plot_pvalues(p -> p ≥ α ? p : 0.0; ws=3000:5000, clims=(-0.1, 1.8), title="max($α, pvalues)")
+
+# %%
+α = 0.01
+plot_pvalues(p -> p ≥ α ? p : 0.0; ws=3000:5000, clims=(-0.1, 1.8), title="max($α, pvalues)")
+
+# %%
+(; pval, Ws, Rs) = calculate_pvalues()
+h(x) = @views f(x) ≥ 0 ? maximum(pval[1 .+ (0:round(Int, x)), 1 .+ (0:round(Int, f(x)))]) : 0.0
+
+# %%
+h(5000-24)
+
+# %%
+plot(h, 4000, 5000; label="", xguide="x")
+title!("maximum of pvalues for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(h, 4700, 5000; label="", xguide="x")
+title!("maximum of pvalues for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+plot(h, 4800, 5000-20; label="", xguide="x")
+title!("maximum of pvalues for w=0,1,…,round(x), r=0,1,…,round(y),\nwhere x-(5000-24) : y-(1000-26) = 24 : 26")
+
+# %%
+h.(4814:4816)
+
+# %%
+h.(5000-26:5000-24)
+
+# %%
+all(h.(4815:5000-25) .== h(4815))
 
 # %%
