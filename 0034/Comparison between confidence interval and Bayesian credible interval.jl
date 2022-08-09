b038a707daf7effffc2e6a0a0f20878cd50f9930
Gen Kuroki <genkuroki@gmail.com>
2022-08-09 23:33:21 +0900

    created

---
+++ 0034/Comparison between confidence interval and Bayesian credible interval.jl
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Julia 1.7.3
+#     language: julia
+#     name: julia-1.7
+# ---
+
+# %%
+using Distributions
+using StatsPlots
+default(fmt=:png, titlefontsize=10, size=(400, 250))
+
+# %%
+function pvalue_clopper_pearson(n, k, p)
+    bin = Binomial(n, p)
+    min(1, 2cdf(bin, k), 2ccdf(bin, k-1))
+end
+
+function confint_clopper_pearson(n, k; α=0.05)
+    p_L = k > 0 ? quantile(Beta(k, n-k+1), α/2) : zero(α)
+    p_U = k < n ? quantile(Beta(k+1, n-k), 1-α/2) : one(α)
+    [p_L, p_U]
+end
+
+function pvalue_wilson(n, k, p)
+    p̂ = k/n
+    SE = √(p*(1-p)/n)
+    2ccdf(Normal(), abs(p̂ - p)/SE)
+end
+
+function confint_wilson(n, k; α=0.05)
+    p̂ = k/n
+    z = quantile(Normal(), 1-α/2)
+    a, b, c = 1+z^2/n, p̂+z^2/(2n), p̂^2
+    # ap² - 2bp + c = 0 を解く.
+    sqrtD = √(b^2 - a*c)
+    p_L = (b - sqrtD)/a
+    p_U = (b + sqrtD)/a
+    [p_L, p_U]
+end
+
+function pvalue_bayesian(n, k, p; prior=(1/2, 1/2))
+    beta = Beta((prior .+ (k, n-k))...)
+    min(1, 2cdf(beta, p), 2ccdf(beta, p))
+end
+
+function credint_bayesian(n, k; α=0.05, prior=(1/2, 1/2))
+    beta = Beta((prior .+ (k, n-k))...)
+    quantile.(beta, [α/2, 1-α/2])
+end
+
+function show_cis(n, k; α=0.05, prior=(1/2, 1/2),
+        s=Bool[1,1,0], legend=:topleft)
+    ci_ba = credint_bayesian(n, k; α, prior)
+    ci_wi = confint_wilson(n, k; α)
+    ci_cp = confint_clopper_pearson(n, k; α)
+    beta0 = Beta(prior...)
+    beta = Beta((prior .+ (k, n-k))...)
+    prob_ci_ba = cdf(beta, ci_ba[end]) - cdf(beta, ci_ba[begin])
+    prob_ci_wi = cdf(beta, ci_wi[end]) - cdf(beta, ci_wi[begin])
+    prob_ci_cp = cdf(beta, ci_cp[end]) - cdf(beta, ci_cp[begin])
+    if s[1]
+        @show ci_ba
+        @show prob_ci_ba
+    end
+    if s[2]
+        @show ci_wi
+        @show prob_ci_wi
+    end
+    if s[3]
+        @show ci_cp
+        @show prob_ci_cp
+    end
+    
+    P1 = plot(; legend)
+    plot!(beta; label="posterior", c=1)
+    plot!(beta0; label="prior", ls=:dot, c=:black)
+    title!("prior and posterior")
+    
+    P2 = plot()
+    s[1] && plot!(p -> pvalue_bayesian(n, k, p; prior); label="Bayes", c=1)
+    s[2] && plot!(p -> pvalue_wilson(n, k, p); label="Wilson", c=2, ls=:dash)
+    s[3] && plot(p -> pvalue_clopper_pearson(n, k, p), 0, 1; label="CP", c=3, ls=:dashdot)
+    title!("P-value functions")
+    plot(P1, P2; size=(800, 250))
+end
+
+# %%
+prior = (7, 3)
+beta0 = Beta(prior...)
+plot(beta0; label="", title="prior: $beta0", ls=:dot, c=:black)
+
+# %%
+show_cis(2, 1; prior)
+
+# %%
+show_cis(4, 2; prior)
+
+# %%
+show_cis(10, 5; prior)
+
+# %%
+show_cis(20, 10; prior)
+
+# %%
+show_cis(40, 20; prior)
+
+# %%
+show_cis(80, 40; prior)
+
+# %%
+show_cis(160, 80; prior)
+
+# %%
+show_cis(320, 160; prior)
+
+# %%
+function prob_ci_wilson(n, k; α=0.05, prior=(1/2, 1/2))
+    beta = Beta((prior .+ (k, n-k))...)
+    ci = confint_wilson(n, k; α)
+    cdf(beta, ci[end]) - cdf(beta, ci[begin])
+end
+plot(2:2:640, n -> prob_ci_wilson(n, n÷2; prior); label="")
+title!("probability of Wilson's CI w.r.t. posterior")
+plot!(xguide="n")
+
+# %%
