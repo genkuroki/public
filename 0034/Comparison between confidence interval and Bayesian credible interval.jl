4627c7c6a236432016367c9eddc1865b5bc0397d
Gen Kuroki <genkuroki@gmail.com>
2022-08-10 12:12:48 +0900

    updated

--- 0034/Comparison between confidence interval and Bayesian credible interval.jl
+++ 0034/Comparison between confidence interval and Bayesian credible interval.jl
@@ -84,13 +84,25 @@ function show_cis(n, k; α=0.05, prior=(1/2, 1/2),
     P1 = plot(; legend)
     plot!(beta; label="posterior", c=1)
     plot!(beta0; label="prior", ls=:dot, c=:black)
-    title!("prior and posterior")
+    title!("prior and posterior of data (n=$n, k=$k)")
+    plot!(tickfontsize=6, xtick=0:0.1:1)
     
-    P2 = plot()
-    s[1] && plot!(p -> pvalue_bayesian(n, k, p; prior); label="Bayes", c=1)
-    s[2] && plot!(p -> pvalue_wilson(n, k, p); label="Wilson", c=2, ls=:dash)
-    s[3] && plot(p -> pvalue_clopper_pearson(n, k, p), 0, 1; label="CP", c=3, ls=:dashdot)
-    title!("P-value functions")
+    P2 = plot(; legend)
+    if s[1]
+        plot!(p -> pvalue_bayesian(n, k, p; prior); label="Bayes", c=1)
+        plot!(ci_ba, fill(α+0.005, 2); label="", c=1)
+    end
+    if s[2]
+        plot!(p -> pvalue_wilson(n, k, p); label="Wilson", c=2, ls=:dash)
+        plot!(ci_wi, fill(α, 2); label="", c=2, ls=:dash)
+    end
+    if s[3]
+        plot(p -> pvalue_clopper_pearson(n, k, p), 0, 1; label="CP", c=3, ls=:dashdot)
+        plot!(ci_cp, fill(α-0.005, 2); label="", c=3, ls=:dashdot)
+    end
+    title!("P-value functions of data (n=$n, k=$k)")
+    plot!(tickfontsize=6, xtick=0:0.1:1, ytick=0:0.05:1)
+
     plot(P1, P2; size=(800, 250))
 end
 
@@ -100,28 +112,28 @@ beta0 = Beta(prior...)
 plot(beta0; label="", title="prior: $beta0", ls=:dot, c=:black)
 
 # %%
-show_cis(2, 1; prior)
+show_cis(2, 1; prior=(7, 3))
 
 # %%
-show_cis(4, 2; prior)
+show_cis(4, 2; prior=(7, 3))
 
 # %%
-show_cis(10, 5; prior)
+show_cis(10, 5; prior=(7, 3))
 
 # %%
-show_cis(20, 10; prior)
+show_cis(20, 10; prior=(7, 3))
 
 # %%
-show_cis(40, 20; prior)
+show_cis(40, 20; prior=(7, 3))
 
 # %%
-show_cis(80, 40; prior)
+show_cis(80, 40; prior=(7, 3))
 
 # %%
-show_cis(160, 80; prior)
+show_cis(160, 80; prior=(7, 3))
 
 # %%
-show_cis(320, 160; prior)
+show_cis(320, 160; prior=(7, 3))
 
 # %%
 function prob_ci_wilson(n, k; α=0.05, prior=(1/2, 1/2))
@@ -129,8 +141,52 @@ function prob_ci_wilson(n, k; α=0.05, prior=(1/2, 1/2))
     ci = confint_wilson(n, k; α)
     cdf(beta, ci[end]) - cdf(beta, ci[begin])
 end
-plot(2:2:640, n -> prob_ci_wilson(n, n÷2; prior); label="")
-title!("probability of Wilson's CI w.r.t. posterior")
-plot!(xguide="n")
+
+function plot_prob_ci(; α=0.05, p_true=0.5, prior=(1/2, 1/2),
+        nmin=2, nmax=1000, kwargs...)
+    plot(nmin:2:nmax, n -> prob_ci_wilson(n, n*p_true; α, prior); label="")
+    title!("prior = $prior,  data = (n, $(p_true)n)\n\
+        probability of p ∈ Wilson's CI with p ∼ posterior")
+    hline!([1-α]; label="", ls=:dot)
+    plot!(xguide="n")
+    plot!(topmargin=4Plots.mm)
+    plot!(; kwargs...)
+end
+
+# %%
+plot_prob_ci(prior=(7, 3), nmax=1000, ylim=(0.93, 0.97))
+
+# %%
+plot_prob_ci(prior=(1, 1), nmax=1000, ylim=(0.93, 0.97))
+
+# %%
+show_cis(10, 5; prior=(7, 3))
+
+# %%
+show_cis(10, 5; prior=(1, 1))
+
+# %%
+show_cis(10, 7; prior=(7, 3))
+
+# %%
+show_cis(10, 7; prior=(1, 1))
+
+# %%
+plot_prob_ci(prior=(7, 3), nmax=1000, ylim=(0.93, 0.97), p_true=0.7)
+
+# %%
+plot_prob_ci(prior=(1, 1), nmax=1000, ylim=(0.93, 0.97), p_true=0.7)
+
+# %%
+show_cis(10, 3; prior=(7, 3), legend=:topright)
+
+# %%
+show_cis(10, 3; prior=(1, 1), legend=:topright)
+
+# %%
+plot_prob_ci(prior=(7, 3), nmax=1000, ylim=(0.8, 0.97), p_true=0.3)
+
+# %%
+plot_prob_ci(prior=(1, 1), nmax=1000, ylim=(0.93, 0.97), p_true=0.3)
 
 # %%
