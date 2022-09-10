d678adbfa679936b5f316aa374bf559928aa02f8
Gen Kuroki <genkuroki@gmail.com>
2022-09-10 15:15:39 +0900

    revised

--- 0032/Mixture Gaussian Example of Lindley 1972.jl
+++ 0032/Mixture Gaussian Example of Lindley 1972.jl
@@ -9,9 +9,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Julia 1.7.3
+#     display_name: Julia 1.8.0
 #     language: julia
-#     name: julia-1.7
+#     name: julia-1.8
 # ---
 
 # %% [markdown]
@@ -28,52 +28,65 @@ using Optim
 mixnormal(μ, σ) = MixtureModel([Normal(), Normal(μ, σ)], [0.5, 0.5])
 
 # %%
-Random.seed!(45105963)
 n = 10
-X = round.(rand(mixnormal(4, 1.5), n); digits=1)
+dist_true = mixnormal(4, 1.5)
 
 # %%
-f(μ, t) = loglikelihood(mixnormal(μ, exp(t)), X)
+plot(x -> pdf(dist_true, x), -5, 11; label="dist_true")
+
+# %%
+#Random.seed!(45105963)
+#@show X = sort(round.(rand(dist_true, n); digits=1))
+X = [-0.8, -0.1, 0.3, 0.8, 1.1, 1.9, 2.8, 3.8, 5.3, 7.4]
+@show X;
+
+# %%
+loglik(μ, t) = loglikelihood(mixnormal(μ, exp(t)), X)
+lik(μ, t) = exp(loglik(μ, t))
 
 # %%
 μ = range(-1, 8, 361)
 t = range(-20, 2, 200)
-z = f.(μ', t)
-heatmap(μ, t, z)
+heatmap(μ, t, loglik)
 title!("log likelihood")
 plot!(; xguide="μ", yguide="log σ")
 
 # %%
 μ = range(-1, 8, 361)
 t = range(-100, 2, 200)
-z = f.(μ', t)
-heatmap(μ, t, z)
+heatmap(μ, t, loglik)
 title!("log likelihood")
 plot!(; xguide="μ", yguide="log σ")
 
 # %%
 μ = range(-1, 9, 361)
 t = range(-1, 2, 200)
-z = exp.(f.(μ', t))
-heatmap(μ, t, z)
+heatmap(μ, t, lik; xlim=extrema(μ), ylim=extrema(t), colorbar=false)
 title!("likelihood")
 plot!(; xguide="μ", yguide="log σ")
+scatter!([4], [log(1.5)]; c=:cyan, label="true param", legend=:bottomleft)
 
 # %%
-@show o = optimize(x -> -f(x...), [4.0, 1.0])
+@show o = optimize(x -> -loglik(x...), [4.0, 1.0])
 o.minimizer[1], exp(o.minimizer[2])
 
 # %%
 for k in eachindex(X)
-    o = optimize(x -> -f(x...), [X[k], -50])
+    o = optimize(x -> -loglik(x...), [X[k], -50])
     @show k, X[k], o.minimizer[1], exp(o.minimizer[2])
 end
 
 # %%
 P = plot(; legend=:topleft)
 for k in eachindex(X)
-    plot!(t -> f(X[k], -t), -2, 70; label="$k", ls=:auto)
+    plot!(t -> loglik(X[k], -t), -2, 70; label="X[$k]", ls=:auto)
 end
-P
+plot!(xguide="-t = -log σ", yguide="log likelihood")
+
+# %%
+@show X[1]
+@eval @show lik($(X[1]), log(7.5e-28))
+@show likelihood_of_true_param = lik(4, log(1.5))
+@show lik(X[1], log(7.5e-28)) / likelihood_of_true_param;
 
 # %%
