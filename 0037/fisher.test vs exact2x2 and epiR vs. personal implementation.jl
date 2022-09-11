619fe64a6bccb480605a835ef36da29dfe7f9248
Gen Kuroki <genkuroki@gmail.com>
2022-09-11 16:47:16 +0900

    created

---
+++ 0037/fisher.test vs exact2x2 and epiR vs. personal implementation.jl
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Julia 1.8.1
+#     language: julia
+#     name: julia-1.8
+# ---
+
+# %%
+using Distributions
+using RCall
+using Roots
+using StatsFuns
+
+safemul(x, y) = x == 0 ? x : isinf(x) ? typeof(x)(Inf) : x*y
+safediv(x, y) = x == 0 ? x : isinf(y) ? zero(y) : x/y
+
+oddsratiohat(a, b, c, d) = safediv(a*d, b*c)
+stderr_logoddsratiohat(a, b, c, d) = √(1/a + 1/b + 1/c + 1/d)
+
+function pvalue_or_wald(a, b, c, d; ω=1)
+    logORhat = log(oddsratiohat(a, b, c, d))
+    SEhat_logORhat = stderr_logoddsratiohat(a, b, c, d)
+    2ccdf(Normal(0, 1), safediv(abs(logORhat - log(ω)), SEhat_logORhat))
+end
+
+function confint_or_wald(a, b, c, d; α=0.05)
+    z = quantile(Normal(), 1-α/2)
+    ORhat = oddsratiohat(a, b, c, d)
+    SEhat_logORhat = stderr_logoddsratiohat(a, b, c, d)
+    [safemul(exp(-z*SEhat_logORhat), ORhat), safemul(exp(z*SEhat_logORhat), ORhat)]
+end
+
+riskratiohat(a, b, c, d) = safediv(a*(c+d), (a+b)*c)
+stderr_logriskratiohat(a, b, c, d) = √(1/a - 1/(a+b) + 1/c - 1/(c+d))
+
+function pvalue_rr_wald(a, b, c, d; ρ=1)
+    (a+b==0 || c+d==0) && return 1.0
+    logRRhat = log(riskratiohat(a, b, c, d))
+    SEhat_logRRhat = stderr_logriskratiohat(a, b, c, d)
+    2ccdf(Normal(0, 1), safediv(abs(logRRhat - log(ρ)), SEhat_logRRhat))
+end
+
+function confint_rr_wald(a, b, c, d; α=0.05)
+    (a+b==0 || c+d==0) && return [0, Inf]
+    z = quantile(Normal(), 1-α/2)
+    RRhat = riskratiohat(a, b, c, d)
+    SEhat_logRRhat = stderr_logriskratiohat(a, b, c, d)
+    [safemul(exp(-z*SEhat_logRRhat), RRhat), safemul(exp(z*SEhat_logRRhat), RRhat)]
+end
+
+function delta(a, b, c, d; ω=1)
+    A, B, C = 1-ω, a+d+ω*(b+c), a*d-ω*b*c
+    isinf(ω) ? typeof(ω)(-min(b, c)) : safediv(2C, B + √(B^2 - 4A*C))
+end
+
+# correction = 0.5 は連続性補正を与える.
+function _chisqstat_or(a, b, c, d, δ; correction=0.0)
+    ã, b̃, c̃, d̃ = a-δ, b+δ, c+δ, d-δ
+    safemul(max(0, abs(δ)-correction)^2, 1/ã + 1/b̃ + 1/c̃ + 1/d̃)
+end
+
+function chisqstat_or(a, b, c, d; ω=1, correction=0.0)
+    δ = delta(a, b, c, d; ω)
+    _chisqstat_or(a, b, c, d, δ; correction)
+end
+
+function pvalue_or_pearson_chisq(a, b, c, d; ω=1, correction=0.0)
+    χ² = chisqstat_or(a, b, c, d; ω, correction)
+    ccdf(Chisq(1), χ²)
+end
+
+function confint_or_pearson_chisq(a, b, c, d; α=0.05, correction=0.0)
+    (a+b==0 || c+d==0 || a+c==0 || b+d==0) && return [0, Inf]
+    f(logω) = logit(pvalue_or_pearson_chisq(a, b, c, d; ω=exp(logω), correction)) - logit(α)
+    ps = if a == 0 || d == 0
+        [0, exp(find_zero(f, 0.0))]
+    elseif b == 0 || c == 0
+        [exp(find_zero(f, 0.0)), Inf]
+    else
+        ORhat = oddsratiohat(a, b, c, d)
+        ω_L, ω_U = ORhat/2, 2ORhat
+        [exp(find_zero(f, log(ω_L))), exp(find_zero(f, log(ω_U)))]
+    end
+end
+
+function Delta(a, b, c, d; ρ=1)
+    m, n = a+b, c+d
+    A, B, C = ρ-1, n-a+ρ*(m-c), a*n-ρ*m*c
+    isinf(ρ) ? typeof(ω)(-c) : safediv(2C, B + √(B^2 - 4A*C))
+end
+
+function _chisqstat_rr(a, b, c, d, Δ)
+    m, n = a+b, c+d
+    safemul(Δ^2, safediv(b, m*(a-Δ)) + safediv(d, n*(c+Δ)))
+end
+
+function chisqstat_rr(a, b, c, d; ρ=1)
+    Δ = Delta(a, b, c, d; ρ)
+    _chisqstat_rr(a, b, c, d, Δ)
+end
+
+function pvalue_rr_pearson_chisq(a, b, c, d; ρ=1)
+    χ² = chisqstat_rr(a, b, c, d; ρ)
+    ccdf(Chisq(1), χ²)
+end
+
+function confint_rr_pearson_chisq(a, b, c, d; α=0.05)
+    (a+b==0 || c+d==0 || a+c==0 || b+d==0) && return [0, Inf]
+    f(logρ) = logit(pvalue_rr_pearson_chisq(a, b, c, d; ρ=exp(logρ))) - logit(α)
+    RRhat = riskratiohat(a, b, c, d)
+    if a == 0
+        [0.0, exp(find_zero(f, 0.0))]
+    elseif c == 0
+        [exp(find_zero(f, 0.0)), Inf]
+    elseif b == 0
+        [0.0, exp(find_zero(f, log(2RRhat)))]
+    elseif d == 0
+        [exp(find_zero(f, log(RRhat/2))), Inf]
+    else
+        ρ_L, ρ_U = RRhat/2, 2RRhat
+        [exp(find_zero(f, log(ρ_L))), exp(find_zero(f, log(ρ_U)))]
+    end
+end
+
+# %%
+R"""fisher.test(matrix(c(16, 4, 4, 6), 2, 2, byrow=T))"""
+
+# %%
+R"""exact2x2::exact2x2(matrix(c(16, 4, 4, 6), 2, 2, byrow=T), plot=T)"""
+
+# %%
+R"""exact2x2::exact2x2(matrix(c(16, 4, 4, 6), 2, 2, byrow=T), tsmethod="central", plot=T)"""
+
+# %%
+R"""epiR::epi.2by2(matrix(c(16, 4, 4, 6), 2, 2, byrow=T), digits=4)"""
+
+# %%
+@show confint_rr_wald(16, 4, 4, 6);
+@show confint_or_wald(16, 4, 4, 6);
+@show confint_rr_pearson_chisq(16, 4, 4, 6);
+@show confint_or_pearson_chisq(16, 4, 4, 6);
+
+# %%
+@show pvalue_rr_wald(16, 4, 4, 6);
+@show pvalue_or_wald(16, 4, 4, 6);
+@show pvalue_rr_pearson_chisq(16, 4, 4, 6);
+@show pvalue_or_pearson_chisq(16, 4, 4, 6);
+@show pvalue_or_pearson_chisq(16, 4, 4, 6; correction=0.5);
+
+# %%
