4c524394b83819159948c8d0d0b8022da52cc7f8
Gen Kuroki <genkuroki@gmail.com>
2022-09-16 01:49:43 +0900

    updated

--- 0037/plot mean and var.jl
+++ 0037/plot mean and var.jl
@@ -23,6 +23,15 @@ default(fmt=:png, size=(500, 300),
 
 distname(dist) = replace(string(dist), r"{[^}]*}"=>"")
 
+function distname(dist::LocationScale)
+    μ, σ, ρ = params(dist)
+    if μ == 0
+        "$σ * $(distname(ρ))"
+    else
+        "$μ + $σ * $(distname(ρ))"
+    end
+end
+
 function plot_mean_and_var(; dist = Normal(10, 2), n = 20, L = 10^6, M = 10^4,
         binX̄=:auto, binS²=:auto, xlimX̄=:auto, xlimS²=:auto, kwargs...)
     X̄ = Vector{Float64}(undef, L)
@@ -34,10 +43,17 @@ function plot_mean_and_var(; dist = Normal(10, 2), n = 20, L = 10^6, M = 10^4,
         S²[i] = var(X)
     end
     
-    @show mean(dist)
-    @show var(dist)
-    @show skewness(dist)
-    @show kurtosis(dist)
+    @show μ = mean(dist)
+    @show σ² = var(dist)
+    @show κ₃ = skewness(dist)
+    @show κ₄ = kurtosis(dist)
+    @show varS² = σ²^2*(κ₄/n + 2/(n-1))
+    @show a = varS²/(2σ²)
+    @show ν = 2σ²^2/varS²
+    @show normal = Normal(μ, √(σ²/n))
+    achisqν = a*Chisq(ν)
+    println("achisqν = a * Chisq(ν) = ", distname(achisqν))
+    @show normalS² = Normal(σ², √varS²)
     
     P1 = scatter(X̄[1:M], S²[1:M]; label="", ms=1, msw=0, alpha=0.5)
     plot!(xguide="\$\\overline{X}\$", yguide="\$S^2\$", yscale=:log10)
@@ -45,8 +61,19 @@ function plot_mean_and_var(; dist = Normal(10, 2), n = 20, L = 10^6, M = 10^4,
     plot!(; kwargs...)
     P2 = stephist(X̄; norm=true, bin=binX̄, xlim=xlimX̄, 
         label="", title="\$\\overline{X}\$")
+    plot!(normal, quantile.(Ref(X̄), (0.000005, 0.999995))...; label="", ls=:dash)
     P3 = stephist(S²; norm=true, bin=binS², xlim=xlimS², 
         label="", title="\$S^2\$")
+    xmax = min(quantile(S², ν ≤ 2 ? 0.999 : 0.99995))
+    if binS² != :auto
+        xmax = min(last(binS²), xmax)
+    end
+    if xlimS² != :auto
+        xmax = min(last(xlimS²), xmax)
+    end
+    xmin = max(quantile(S², 0.000005), xmax/200)
+    plot!(normalS², xmin, xmax; label="", ls=:dash)
+    plot!(achisqν, xmin, xmax; label="", ls=:dashdot)
     
     layout = @layout [a [b; c]]
     plot(P1, P2, P3; layout, size=(800, 400))
@@ -60,7 +87,7 @@ plot_mean_and_var(dist = Normal(10, 2), n = 20)
 plot_mean_and_var(dist = Uniform(), n = 20)
 
 # %%
-plot_mean_and_var(dist = Gamma(4, 2), n = 20)
+plot_mean_and_var(dist = Gamma(4, 2), n = 20, xlimS² = (0, 60))
 
 # %%
 plot_mean_and_var(dist = Poisson(1), n = 20, binX̄=0:0.05:2, binS²=0:0.15:3)
@@ -75,6 +102,15 @@ plot_mean_and_var(dist = LogNormal(), n = 20, xlimX̄=(0.5, 4), binS²=0:0.05:20
 plot_mean_and_var(dist = LogNormal(), n = 100, xlimX̄=(0.9, 2.4), binS²=0:0.05:20)
 
 # %%
-plot_mean_and_var(dist = LogNormal(), n = 500, xlimX̄=(1.3, 2.0), binS²=0:0.05:12)
+plot_mean_and_var(dist = LogNormal(), n = 500, xlimX̄=(1.2, 2.2), xlimS²=(1, 12))
+
+# %%
+@time plot_mean_and_var(dist = LogNormal(), n = 2500, xlimS²=(2.5, 10))
+
+# %%
+@time plot_mean_and_var(dist = LogNormal(), n = 12500, xlimS²=(3, 7))
+
+# %%
+@time plot_mean_and_var(dist = LogNormal(), n = 62500, xlimS²=(4, 5.7))
 
 # %%
