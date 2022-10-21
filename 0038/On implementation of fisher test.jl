ccf5346805880e53b3ef4f7130ef8c683b597857
Gen Kuroki <genkuroki@gmail.com>
2022-10-21 17:05:17 +0900

    updated

--- 0038/On implementation of fisher test.jl
+++ 0038/On implementation of fisher test.jl
@@ -37,7 +37,60 @@ function fisher_test_le(a, b, c, d)
     sum(pdf(hg, i) for i in support(hg) if pdf(hg, i) ≤ pa)
 end
 
+# https://qiita.com/WolfMoon/items/530413ce7c8439d18c18
+using SpecialFunctions
+# 以下も不適切な実装
+function fisher_test_wm(a, b, c, d)
+    lchoose(n, k) = logfactorial(n) - logfactorial(k) - logfactorial(n - k)
+    Stats(i, e, f, g, n) = exp(lchoose(e, i) + lchoose(f, g - i) - lchoose(n, g))
+    e, f, g, h, n = a + b, c + d, a + c, b + d, a + b + c + d
+    mi = max(0, e + g - n)
+    length = min(e, g) - mi
+    prob = [Stats(mi + i, e, f, g, n) for i in 0:length]
+    #println("Fisher's Exact Test for Count Data (two tailed)")
+    #println("p value =", sum(prob[prob .<= Stats(a, e, f, g, n)]))
+    sum(prob[prob .<= Stats(a, e, f, g, n)])
+end
+
 # %%
+s, t, n = 17, 7, 12
+hg = Hypergeometric(s, t, n)
+supp = support(hg)
+Any[
+    [(a, s-a, n-a, t-n+a) for a in supp];;
+    [fisher_test_wm(a, s-a, n-a, t-n+a) for a in supp];;
+    [pvalue(FisherExactTest(a, s-a, n-a, t-n+a), method=:minlike) for a in supp];;
+    [fisher_test(a, s-a, n-a, t-n+a) for a in supp];;
+    [fisher_test_le(a, s-a, n-a, t-n+a) for a in supp];;
+    [fisher_test_wm(a, s-a, n-a, t-n+a) ≈ pvalue(FisherExactTest(a, s-a, n-a, t-n+a), method=:minlike) for a in supp];;
+    [fisher_test_wm(a, s-a, n-a, t-n+a) ≈ fisher_test(a, s-a, n-a, t-n+a) for a in supp];;
+    [fisher_test(a, s-a, n-a, t-n+a) ≈ fisher_test_le(a, s-a, n-a, t-n+a) for a in supp];;
+]
+
+# %%
+a, b, c, d = 11, 6, 1, 6
+@show fisher(a, b, c, d)
+@show pvalue(FisherExactTest(a, b, c, d))
+@show fisher_test(a, b, c, d)
+@show fisher_test_le(a, b, c, d);
+
+# %%
+a, b, c, d = 9, 8, 3, 4
+@show fisher(a, b, c, d)
+@show pvalue(FisherExactTest(a, b, c, d))
+@show fisher_test(a, b, c, d)
+@show fisher_test_le(a, b, c, d);
+
+# %%
+s, t, n = 17, 7, 12
+hg = Hypergeometric(s, t, n)
+supp = support(hg)
+[(a, pdf(Hypergeometric(s, t, n), a)) for a in supp]
+
+# %%
+using HypothesisTests
+using RCall
+
 # https://qiita.com/WolfMoon/items/530413ce7c8439d18c18
 
 using SpecialFunctions
@@ -50,37 +103,26 @@ function fisher(a, b, c, d)
     mi = max(0, e + g - n)
     length = min(e, g) - mi
     prob = [Stats(mi + i, e, f, g, n) for i in 0:length]
-    #println("Fisher's Exact Test for Count Data (two tailed)")
-    #println("p value =", sum(prob[prob .<= Stats(a, e, f, g, n)]))
-    sum(prob[prob .<= Stats(a, e, f, g, n)])
+    println("Fisher's Exact Test for Count Data (two tailed)")
+    println("p value =", sum(prob[prob .<= Stats(a, e, f, g, n)]))
 end;
 
 # %%
-s, t, n = 11, 9, 10
-hg = Hypergeometric(s, t, n)
-supp = support(hg)
-Any[
-    [(a, s-a, n-a, t-n+a) for a in supp];;
-    [fisher(a, s-a, n-a, t-n+a) for a in supp];;
-    [pvalue(FisherExactTest(a, s-a, n-a, t-n+a)) for a in supp];;
-    [fisher_test(a, s-a, n-a, t-n+a) for a in supp];;
-    [fisher_test_le(a, s-a, n-a, t-n+a) for a in supp];;
-    [fisher(a, s-a, n-a, t-n+a) ≈ pvalue(FisherExactTest(a, s-a, n-a, t-n+a)) for a in supp];;
-    [fisher(a, s-a, n-a, t-n+a) ≈ fisher_test(a, s-a, n-a, t-n+a) for a in supp];;
-    [fisher_test(a, s-a, n-a, t-n+a) ≈ fisher_test_le(a, s-a, n-a, t-n+a) for a in supp];;
-]
+fisher(11, 6, 1, 6)
 
 # %%
-(a, b, c, d) = (6, 5, 4, 5)
-@show fisher(a, b, c, d)
-@show pvalue(FisherExactTest(a, b, c, d))
-@show fisher_test(a, b, c, d)
-@show fisher_test_le(a, b, c, d);
+pvalue(FisherExactTest(11, 6, 1, 6), method=:minlike)
 
 # %%
-s, t, n = 11, 9, 10
-hg = Hypergeometric(s, t, n)
-supp = support(hg)
-[(a, pdf(Hypergeometric(s, t, n), a)) for a in supp]
+R"fisher.test(matrix(c(11, 6, 1, 6), nrow=2))"
+
+# %%
+fisher(9, 8, 3, 4)
+
+# %%
+pvalue(FisherExactTest(9, 8, 3, 4), method=:minlike)
+
+# %%
+R"fisher.test(matrix(c(9, 8, 3, 4), nrow=2))"
 
 # %%
