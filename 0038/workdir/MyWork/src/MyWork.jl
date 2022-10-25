23d8eeee58f2f2707ef13092acc32d871d86ff85
Gen Kuroki <genkuroki@gmail.com>
2022-10-25 14:48:19 +0900

    created

---
+++ 0038/workdir/MyWork/src/MyWork.jl
@@ -0,0 +1,12 @@
+module MyWork
+
+greet() = print("Hello World!")
+
+"""
+    mrump(a, b)
+
+returns 333.75b^6 + a^2*(11a^2*b^2 - b^6 - 121b^4 - 2) + 5.5b^8.
+"""
+mrump(a, b) = 333.75b^6 + a^2*(11a^2*b^2 - b^6 - 121b^4 - 2) + 5.5b^8
+
+end # module MyWork
