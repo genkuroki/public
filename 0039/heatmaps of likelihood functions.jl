564489fd81cc70dcd1f6394c506312f0f058fe4a
Gen Kuroki <genkuroki@gmail.com>
2022-11-10 12:22:01 +0900

    modeltanh3

--- 0039/heatmaps of likelihood functions.jl
+++ 0039/heatmaps of likelihood functions.jl
@@ -23,7 +23,15 @@ default(fmt=:png,
     titlefontsize=10, plot_titlefontsize=10,
     guidefontsize=9, legendfontsize=8, tickfontsize=6)
 
+# %% tags=[]
+?log1p
+
+# %% tags=[]
+?expm1
+
 # %% [markdown]
+# モデル:
+#
 # $$
 # \begin{aligned}
 # p(x|a,b)
@@ -35,11 +43,11 @@ default(fmt=:png,
 # \\ &= \frac{e^{-x^2/2}}{\sqrt{2\pi}}
 # \left(
 # 1 + a\left(e^{b(x-b/2)} - 1\right)
-# \right)
+# \right).
 # \end{aligned}
 # $$
 #
-# のとき
+# このとき
 #
 # $$
 # \log p(x|a,b) 
@@ -54,12 +62,6 @@ default(fmt=:png,
 # $$
 # L(a,b|X) = \sum_{i=1}^n \log p(x_i|a,b).
 # $$
-
-# %%
-?log1p
-
-# %%
-?expm1
 
 # %%
 mixnormal(a, b) = MixtureModel([Normal(), Normal(b, 1)], [1-a, a])
@@ -144,6 +146,7 @@ plot(X, Y; label="data")
 plot!(; xguide="X", yguide="Y")
 plot!(x -> b*tanh(a*x), -1, 1; label="$(b==1 ? "" : "$b ")tanh($(a)x)")
 plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
 
 # %%
 a, b, n = 1, 1, 100
@@ -153,6 +156,7 @@ plot(X, Y; label="data")
 plot!(; xguide="X", yguide="Y")
 plot!(x -> b*tanh(a*x), -1, 1; label="$(b==1 ? "" : "$b ")tanh($(a)x)")
 plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
 
 # %%
 a, b, n = 0.5, 1, 100
@@ -162,6 +166,7 @@ plot(X, Y; label="data")
 plot!(; xguide="X", yguide="Y")
 plot!(x -> b*tanh(a*x), -1, 1; label="$(b==1 ? "" : "$b ")tanh($(a)x)")
 plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
 
 # %%
 a, b, n = 0.2, 1, 100
@@ -180,6 +185,7 @@ plot(X, Y; label="data")
 plot!(; xguide="X", yguide="Y")
 plot!(x -> b*tanh(a*x), -1, 1; label="$(b==1 ? "" : "$b ")tanh($(a)x)")
 plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
 
 # %%
 a, b, n = 0, 1, 100
@@ -189,6 +195,7 @@ plot(X, Y; label="data")
 plot!(; xguide="X", yguide="Y")
 plot!(x -> b*tanh(a*x), -1, 1; label="$(b==1 ? "" : "$b ")tanh($(a)x)")
 plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
 
 # %%
 function plog_lik_regtanh(; a=1, b=1, n=100)
@@ -241,5 +248,115 @@ end
 
 # %%
 @time plog_lik_regtanh(; a=0, b=1, n=1000)
+
+# %% [markdown]
+# モデル:
+#
+# $$
+# p(y|a,b,x) = \frac{1}{\sqrt{2\pi}}\exp\left(\frac{1}{2}\left(y - \mu(a,b,x)\right)^2\right),
+# \quad
+# \mu(a,b,x) = \tanh(ax)\tanh((a-2b)x)\tanh((a+2b)x).
+# $$
+
+# %%
+modeltanh3(a, b, x) = Normal(tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), 1)
+loglik_modeltanh3(a, b, X, Y) = sum(logpdf(modeltanh3(a, b, x), y) for (x, y) in zip(X, Y))
+
+# %%
+a, b, n = 2, 2, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+a, b, n = 1, 1, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+a, b, n = 0.5, 0.5, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+a, b, n = 0.2, 0.2, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+a, b, n = 0.1, 0.1, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+a, b, n = 0, 0, 100
+X = range(-1, 1, n)
+Y = [rand(modeltanh3(a, b, x)) for x in X]
+plot(X, Y; label="data")
+plot!(; xguide="X", yguide="Y")
+plot!(x -> tanh(a*x)*tanh((a-2b)*x)*tanh((a+2b)*x), -1, 1; label="")
+plot!(legend=:topleft)
+title!("a = $a,  b = $b,  n = $n")
+
+# %%
+function plog_lik_modeltanh3(; a=1, b=1, n=100)
+    X = range(-1, 1, n)
+    as = range(-4, 4, 400)
+    bs = range(-4, 4, 400)
+    ws = Vector{Matrix{Float64}}(undef, 20)
+    Threads.@threads for i in 1:20
+        Y = [rand(regtanh(a, b, x)) for x in X]
+        logz = loglik_modeltanh3.(as, bs', Ref(X), Ref(Y))
+        maxlogz = maximum(logz)
+        ws[i] = @. exp(logz - maxlogz)
+    end
+    PP = Vector{Any}(undef, 20)
+    for i in 1:20
+        P = heatmap(bs, as, ws[i]; colorbar=false, tickfontsize=5)
+        plot!(xtick=-10:10, ytick=-10:10)
+        PP[i] = P
+    end
+    plot(PP...; layout=(4, 5), size=(1000, 800))
+    plot!(plot_title="true a = $a,  true b = $b,  n = $n")
+end
+
+# %%
+@time plog_lik_modeltanh3(; a=1, b=1, n=100)
+
+# %%
+@time plog_lik_modeltanh3(; a=0.5, b=0.5, n=100)
+
+# %%
+@time plog_lik_modeltanh3(; a=0.2, b=0.2, n=100)
+
+# %%
+@time plog_lik_modeltanh3(; a=0.1, b=0.1, n=100)
+
+# %%
+@time plog_lik_modeltanh3(; a=0, b=0, n=100)
 
 # %%
