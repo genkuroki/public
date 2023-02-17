f6de286a1eeed07f800f438197a59cc57671e2d8
Gen Kuroki <genkuroki@gmail.com>
2023-02-18 00:37:43 +0900

    revised

--- 0041/scatter plots for linear regression.jl
+++ 0041/scatter plots for linear regression.jl
@@ -22,6 +22,7 @@ using StatsPlots
 default(fmt=:png, titlefontsize=10, tickfontsize=6, guidefontsize=7)
 using DataFrames
 using RCall
+using Random
 
 # %% [markdown]
 # データを生成するモデル:
@@ -29,7 +30,7 @@ using RCall
 # \begin{aligned}
 # & X_1 = U_1, \quad & & U_1 \sim \operatorname{Uniform}(-\sqrt{3}, \sqrt{3}) \\
 # & X_2 = X_1 + U_2, \quad & & U_2 \sim 10\operatorname{Uniform}(-\sqrt{3}, \sqrt{3}), \\
-# & Y = 1 + X_1 + X_2^2 + X_2 + U_3, \quad & & U_3 \sim \operatorname{Normal}(0, 1).
+# & Y = 1 + X_1 + X_1^2 + X_2 + U_3, \quad & & U_3 \sim \operatorname{Normal}(0, 1).
 # \end{aligned}
 # $$
 #
@@ -44,13 +45,15 @@ using RCall
 # $$
 
 # %%
+Random.seed!(4649373)
+
 n = 200
 x1 = rand(Uniform(-√3, √3), n)
 x2 = x1 + 10rand(Uniform(-√3, √3), n)
 y = @. 1 + x1 + x1^2 + x2 + randn()
 
 anim = @animate for t in 0:3:359
-    scatter(x1, x2, y; label="", title="data", msc=:auto, alpha=0.5, ms=4, camera=(t, 30))
+    scatter(x1, x2, y; label="", title="data", msc=:auto, alpha=0.5, ms=4, camera=(t+30, 20))
     plot!(xguide="x1", yguide="x2", zguide="y")
     plot!(size=(500, 500))
 end
@@ -89,7 +92,15 @@ R2 = scatter(x2, y - ŷ; label="(x2, y - ŷ)", msc=:auto, alpha=0.5, ms=3)
 hline!([0]; label="", ls=:dot, c=:gray)
 
 plot(P1, P2, Q1, Q2, R1, R2; size=(600, 800), layout=(3, 2))
-plot!(legend=:outertop)
+plot!(legend=:outertop) |> display
+
+anim = @animate for t in 0:3:359
+    scatter(x1, x2, y - ŷ; label="", msc=:auto, alpha=0.5, ms=4, camera=(t+30, 20))
+    plot!(xguide="x1", yguide="x2", zguide="y - ŷ")
+    title!("residual error of model1")
+    plot!(size=(500, 500))
+end
+gif(anim, "reserr1.gif")
 
 # %%
 println("*** Y = β₀ + β₁X₁ + β₂X₁² + β₃X₂ + σU で推定 ***\n")
@@ -125,11 +136,20 @@ R2 = scatter(x2, y - ŷ; label="(x2, y - ŷ)", msc=:auto, alpha=0.5, ms=3)
 hline!([0]; label="", ls=:dot, c=:gray)
 
 plot(P1, P2, Q1, Q2, R1, R2; size=(600, 800), layout=(3, 2))
-plot!(legend=:outertop)
+plot!(legend=:outertop) |> display
+
+anim = @animate for t in 0:3:359
+    scatter(x1, x2, y - ŷ; label="", msc=:auto, alpha=0.5, ms=4, camera=(t+30, 20))
+    plot!(xguide="x1", yguide="x2", zguide="y - ŷ")
+    title!("residual error of model2")
+    plot!(size=(500, 500))
+end
+gif(anim, "reserr2.gif")
 
 # %%
 R"""
-library(car)
+library(dplyr)
+library(ggplot2)
 """
 
 # %%
@@ -155,5 +175,71 @@ model2 = lm(y ~ x1 + x12 + x2, data = df)
 R"""
 car::crPlots(model2)
 """;
+
+# %% [markdown]
+# https://twitter.com/NorimitsuNishi1/status/1626514396112650240
+
+# %%
+R"""
+set.seed(2349)
+n <- 40
+d <- tibble(x1 = runif(n, 3, 8), x2 = runif(n, 1, 5),
+    epsilon = rnorm(n, 0, 0.3)) |>
+    mutate(y = 10 + (4*x1) + (1 * x2^2) + epsilon)
+"""
+@rget d
+y = @. 10 + (4*d.x1) + (1 * d.x2^2) + d.epsilon
+@show y == d.y
+d.x22 = @. d.x2^2
+@rput d
+
+# %%
+R"""
+ggplot(d) + geom_point(aes(x = x1, y = y)) +
+geom_smooth(aes(x=x1, y=y), se=FALSE)
+"""
+
+# %%
+R"""
+ggplot(d) + geom_point(aes(x = x2, y = y)) +
+geom_smooth(aes(x=x2, y=y), se=FALSE)
+"""
+
+# %%
+R"""
+ggplot(d) + geom_point(aes(x = x22, y = y)) +
+geom_smooth(aes(x=x22, y=y), se=FALSE)
+"""
+
+# %%
+
+R"""
+model = lm(y ~ x1, data = d)
+car::crPlots(model)
+"""
+
+# %%
+R"""
+model = lm(y ~ x2, data = d)
+car::crPlots(model)
+"""
+
+# %%
+R"""
+model = lm(y ~ x22, data = d)
+car::crPlots(model)
+"""
+
+# %%
+R"""
+model = lm(y ~ x1 + x2, data = d)
+car::crPlots(model)
+"""
+
+# %%
+R"""
+model = lm(y ~ x1 + x22, data = d)
+car::crPlots(model)
+"""
 
 # %%
