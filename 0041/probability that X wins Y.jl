8283bd5d9532b0391422514836d9bcf65e886cbf
Gen Kuroki <genkuroki@gmail.com>
2023-03-24 06:29:19 +0900

    revised simple example

--- 0041/probability that X wins Y.jl
+++ 0041/probability that X wins Y.jl
@@ -8,7 +8,7 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Julia 1.9.0-beta4
+#     display_name: Julia 1.9.0
 #     language: julia
 #     name: julia-1.9
 # ---
@@ -24,13 +24,8 @@ probability_that_x_wins_y(x, y) = (x > y) + (x == y)/2
 function probability_that_distX_wins_distY(
         distX::DiscreteUnivariateDistribution,
         distY::DiscreteUnivariateDistribution)
-    p = 0.0
-    for x in support(distX)
-        for y in support(distY)
-            p += probability_that_x_wins_y(x, y) * pdf(distX, x) * pdf(distY, y)
-        end
-    end
-    p
+    sum(probability_that_x_wins_y(x, y) * pdf(distX, x) * pdf(distY, y)
+        for x in support(distX), y in support(distY))
 end
 
 # %%
@@ -49,6 +44,21 @@ PC = bar(distC; label="", title="distribution of C", c=3)
 plot(PA, PB, PC; layout=(3, 1))
 
 # %%
+distX = Categorical(0, 2/3, 0, 0, 1/3)
+distY = Categorical(1/3, 0, 0, 2/3, 0)
+distZ = Categorical(0, 1/3, 1/3, 1/3, 0)
+
+@show probability_that_distX_wins_distY(distX, distY)
+@show probability_that_distX_wins_distY(distY, distZ)
+@show probability_that_distX_wins_distY(distZ, distX)
+println()
+
+PX = bar(distX; label="", title="distribution of X", c=1)
+PY = bar(distY; label="", title="distribution of Y", c=2)
+PZ = bar(distZ; label="", title="distribution of Z", c=3)
+plot(PX, PY, PZ; layout=(3, 1), ylim=(-0.01, 0.7))
+
+# %%
 distX = Categorical(0, 1/2, 1/6, 0, 1/3)
 distY = Categorical(1/3, 0, 1/6, 1/2, 0)
 distZ = Categorical(0, 1/6, 2/3, 1/6, 0)
@@ -61,6 +71,6 @@ println()
 PX = bar(distX; label="", title="distribution of X", c=1)
 PY = bar(distY; label="", title="distribution of Y", c=2)
 PZ = bar(distZ; label="", title="distribution of Z", c=3)
-plot(PX, PY, PZ; layout=(3, 1))
+plot(PX, PY, PZ; layout=(3, 1), ylim=(-0.01, 0.7))
 
 # %%
