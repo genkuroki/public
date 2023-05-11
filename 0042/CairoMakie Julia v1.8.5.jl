c00852047b32ed578354366c71af93dbed1924e0
Gen Kuroki <genkuroki@gmail.com>
2023-05-12 03:04:01 +0900

    created

---
+++ 0042/CairoMakie Julia v1.8.5.jl
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Julia 1.8.5
+#     language: julia
+#     name: julia-1.8
+# ---
+
+# %%
+versioninfo()
+
+# %%
+@time using CairoMakie
+
+# %%
+@time plot(range(-π, π, 1000), sin)
+
+# %%
