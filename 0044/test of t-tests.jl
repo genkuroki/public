0db8bc9162160f8f38b9f2fc6d04b882db3a62ab
Gen Kuroki <genkuroki@gmail.com>
2023-09-21 11:06:28 +0900

    updated

--- 0044/test of t-tests.jl
+++ 0044/test of t-tests.jl
@@ -17,7 +17,7 @@
 # %%
 using Distributions
 using StatsPlots
-default(fmt=:png, tickfontsize=6)
+default(fmt=:png, tickfontsize=6, titlefontsize=12)
 
 ECDF(A, x) = count(≤(x), A)/length(A)
 
@@ -34,17 +34,21 @@ function plot_t_tests(;
         L = 10^6,
         ytick = 0:0.01:1,
         ylim = :auto,
-        legend = :bottom
+        legend = :bottom,
+        bin = :auto
     )
-    @show distx 
-    @show disty
-    @show m
-    @show n
-    @show s
+    println("(distribution of X) = distx = ", distx) 
+    println("(distribution of Y) = disty = ", disty)
+    println("(size of sample from X) = m = ", m)
+    println("(size of sample from Y) = n = ", n)
+    if s != 1 println("multiple of disty = ", s) end
+    println()
+    
     @show mean(distx)
     if s == 1 @show(mean(disty)) else @show(s*mean(disty)) end
     @show std(distx)
     if s == 1 @show(std(disty)) else @show(abs(s)*std(disty)) end
+    println()
     
     X = rand(distx, m, L)
     Y = rand(disty, n, L)
@@ -58,15 +62,21 @@ function plot_t_tests(;
     T_Student = @. (X̄ - Ȳ)/(S*√(1/m + 1/n))
     T_Welch = @. (X̄ - Ȳ)/√(SX^2/m + SY^2/n)
     @show T_Student ≈ T_Welch
+    println()
+    
+    df_Student = m + n - 2
     DF_Welch = @. degree_of_freedom_Welchelch(m, SX^2, n, SY^2)
-
-    @show m+n-2 mean(DF_Welch) std(DF_Welch)
+    @show df_Student
+    @show mean(DF_Welch) std(DF_Welch)
+    println()
 
     pval_Student = @. 2ccdf(TDist(m+n-2), abs(T_Student))
     pval_Welch = @. 2ccdf(TDist(DF_Welch), abs(T_Welch))
 
     @show ECDF(pval_Student, 0.05) ECDF(pval_Welch, 0.05)
+    println()
     @show ECDF(pval_Student, 0.01) ECDF(pval_Welch, 0.01)
+    println()
     
     P = plot(α -> ECDF(pval_Student, α), 0, 0.1; label="Student t-test")
     plot!(α -> ECDF(pval_Welch, α), 0, 0.1; label="Welch t-test", ls=:dash)
@@ -76,8 +86,8 @@ function plot_t_tests(;
         yguide="probability of p-value ≤ α")
     plot!(; legend=:outertop, legendfontsize=10)
 
-    Q = stephist(pval_Student; norm=true, label="Student t-test")
-    stephist!(pval_Welch; norm=true, ls=:dash, label="Welch t-test")
+    Q = stephist(pval_Student; norm=true, bin, label="Student t-test")
+    stephist!(pval_Welch; norm=true, bin, ls=:dash, label="Welch t-test")
     plot!(; xguide="P-value", yguide="probability density")
     plot!(; xtick=0:0.05:1, ytick=0:0.1:5, xrotation=90)
     plot!(; legend=:outertop, legendfontsize=10, ylim)
@@ -86,32 +96,34 @@ function plot_t_tests(;
     plot!(; bottommargin=4Plots.mm, leftmargin=4Plots.mm)
 end
 
-function plot_df_and_SEratio(;
+function plot_df_and_tstatratio(;
         m = 9,
         n = 11,
-        tick = Any[0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+        tick = Any[0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100]
     )
     log10tick = (tick, string.(tick))
     as = 10.0 .^ range(log10(minimum(tick)), log10(maximum(tick)), 1000)
 
-    P = plot(as, a -> degree_of_freedom_Welchelch(m, 1, n, a); label="Welch t-test")
+    P = plot(as, a -> degree_of_freedom_Welchelch(m, 1, n, a^2); label="Welch t-test")
     hline!([(m-1)+(n-1)]; label="Student t-test", ls=:dash)
     hline!([min(m-1, n-1)]; label="min(m-1, n-1)", ls=:dashdot)
     hline!([max(m-1, n-1)]; label="max(m-1, n-1)", ls=:dashdotdot)
-    vline!([(n*(n-1))/(m*(m-1))]; label="(n*(n-1))/(m*(m-1))", ls=:dot)
+    vline!([√((n*(n-1))/(m*(m-1)))]; label="√((n*(n-1))/(m*(m-1)))", ls=:dot)
+    vline!([1]; label="", ls=:dot, c=:gray)
     plot!(xscale=:log10, xtick=log10tick)
-    plot!(xguide="var(sample of X) / var(sample of Y)")
+    plot!(xguide="std(sample of Y) / std(sample of X)")
     title!("degree of freedom for m = $m, n = $n")
 
-    Q = plot(as, a -> √(1/m + a/n) / √((((m-1)+(n-1)*a)/(m-1+n-1))*(1/m+1/n));
-        label="(Welch SE)/(Student SE)")
+    Q = plot(as, a -> √(1/m + a^2/n) / √((((m-1)+(n-1)*a^2)/(m-1+n-1))*(1/m+1/n));
+        label="t_Student / t_Welch")
     hline!([√(m/n), √(n/m)]; label="√(m/n), √(n/m)", ls=:dash)
+    vline!([1]; label="", ls=:dot, c=:gray)
     plot!(xscale=:log10, xtick=log10tick)
-    plot!(xguide="var(sample of X) / var(sample of Y)")
+    plot!(xguide="std(sample of Y) / std(sample of X)")
     plot!(ylim=(0.9, 1.1) .* minmax(√(m/n), √(n/m)))
-    title!("(Welch's SE) / (Student's SE) for m = $m,  n = $n")
+    title!("t_Student / t_Welch for m = $m,  n = $n")
 
-    plot(P, Q; size=(1200, 400))
+    plot(P, Q; size=(1000, 400))
     plot!(bottommargin=4Plots.mm)
 end
 
@@ -122,26 +134,35 @@ plot_t_tests(; distx = Normal(), disty = Normal(), m = 10, n = 10)
 plot_t_tests(; distx = Normal(), disty = Normal(), m = 9, n = 11)
 
 # %%
-plot_t_tests(; distx = Normal(1.3, 1), disty = Normal(0, 1), m = 10, n = 10,
-    ytick=0:0.1:1, ylim=(-0.06, 3))
+plot_t_tests(; distx = Normal(), disty = Normal(), m = 8, n = 12)
 
 # %%
-plot_t_tests(; distx = Normal(), disty = Normal(), m = 8, n = 12)
+plot_t_tests(; distx = Normal(1.3, 1), disty = Normal(0, 1), m = 10, n = 10,
+    ytick=0:0.1:1, ylim=(-0.06, 3))
 
 # %%
 plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 10, n = 10)
 
 # %%
+plot_df_and_tstatratio(; m = 10, n = 10)
+
+# %%
 plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 9, n = 11)
+
+# %%
+plot_df_and_tstatratio(; m = 9, n = 11)
 
 # %%
 plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 8, n = 12)
 
 # %%
+plot_df_and_tstatratio(; m = 8, n = 12)
+
+# %%
 plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 80, n = 120)
 
 # %%
-plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 800, n = 1200, L = 10^5)
+plot_t_tests(; distx = Normal(0, 2), disty = Normal(0, 1), m = 800, n = 1200, L = 10^5, bin=0:0.01:1)
 
 # %%
 plot_t_tests(; distx = Normal(0, 3), disty = Normal(0, 1), m = 9, n = 11)
@@ -153,28 +174,27 @@ plot_t_tests(; distx = Normal(0, 3), disty = Normal(0, 1), m = 90, n = 110)
 plot_t_tests(; distx = Normal(0, 1.1), disty = Normal(0, 1), m = 10, n = 50)
 
 # %%
-plot_df_and_SEratio(; m = 10, n = 50,
-    tick = Any[0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
+plot_df_and_tstatratio(; m = 10, n = 50)
 
 # %%
 plot_t_tests(; distx = Normal(0, 1.1), disty = Normal(0, 1), m = 20, n = 100)
 
 # %%
-plot_df_and_SEratio(; m = 20, n = 100,
-    tick = Any[0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
+plot_df_and_tstatratio(; m = 20, n = 100,
+    tick = Any[0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500])
 
 # %%
 plot_t_tests(; distx = Normal(0, 1.2), disty = Normal(0, 1), m = 10, n = 30)
 
 # %%
-plot_df_and_SEratio(; m = 10, n = 30,
-    tick = Any[0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
+plot_df_and_tstatratio(; m = 10, n = 30,
+    tick = Any[0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500])
 
 # %%
 plot_t_tests(; distx = Normal(0, 1.2), disty = Normal(0, 1), m = 20, n = 60)
 
 # %%
-plot_df_and_SEratio(; m = 20, n = 60,
+plot_df_and_tstatratio(; m = 20, n = 60,
     tick = Any[0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000])
 
 # %%
@@ -195,33 +215,62 @@ disty = LogNormal(0.2, 0.7)
 plot_t_tests(; distx, disty, m = 10^3, n = 10^3, L = 10^5, ytick=0:0.1:1, ylim=(-0.06, 3))
 
 # %%
-plot_df_and_SEratio(; m=10, n=10)
+plot_df_and_tstatratio(; m=10, n=10)
 
 # %%
-plot_df_and_SEratio(; m=10, n=11)
+plot_df_and_tstatratio(; m=10, n=11)
 
 # %%
-plot_df_and_SEratio(; m=10, n=12)
+plot_df_and_tstatratio(; m=10, n=12)
 
 # %%
-plot_df_and_SEratio(; m=10, n=14)
+plot_df_and_tstatratio(; m=10, n=14)
 
 # %%
-plot_df_and_SEratio(; m=10, n=20)
+plot_df_and_tstatratio(; m=10, n=20)
 
 # %%
-plot_df_and_SEratio(; m=100, n=100)
+plot_df_and_tstatratio(; m=100, n=100)
 
 # %%
-plot_df_and_SEratio(; m=100, n=110)
+plot_df_and_tstatratio(; m=100, n=110)
 
 # %%
-plot_df_and_SEratio(; m=100, n=120)
+plot_df_and_tstatratio(; m=100, n=120)
 
 # %%
-plot_df_and_SEratio(; m=100, n=140)
+plot_df_and_tstatratio(; m=100, n=140)
 
 # %%
-plot_df_and_SEratio(; m=100, n=200)
+plot_df_and_tstatratio(; m=100, n=200)
+
+# %%
+z = 1.96
+plot(n -> 2ccdf(TDist(n-1), z), 5, 100; label="2ccdf(TDist(n-1), $z)")
+hline!([2ccdf(Normal(), z)]; label="2ccdf(Normal(), $z)", ls=:dash)
+plot!(xguide="n")
+plot!(xtick=0:5:100, ytick=0:0.01:1)
+
+# %%
+z = 2.576
+plot(n -> 2ccdf(TDist(n-1), z), 5, 100; label="2ccdf(TDist(n-1), $z)")
+hline!([2ccdf(Normal(), z)]; label="2ccdf(Normal(), $z)", ls=:dash)
+plot!(xguide="n")
+plot!(xtick=0:5:100, ytick=0:0.01:1)
+
+# %%
+z = 1.96
+plot(x -> 2ccdf(Normal(0,1), x), 0.8z, 1.2z; label="Normal(0,1) P-value")
+plot!(xtick=0:0.1:4, ytick=0:0.01:1)
+vline!([z]; label="", ls=:dot)
+hline!([2ccdf(Normal(), z)]; label="", ls=:dot)
+
+# %%
+dist = TDist(10)
+z = cquantile(dist, 0.05/2)
+plot(x -> 2ccdf(dist, x), 0.8z, 1.2z; label="TDist(10) P-value")
+plot!(xtick=0:0.1:4, ytick=0:0.01:1)
+vline!([z]; label="", ls=:dot)
+hline!([2ccdf(dist, z)]; label="", ls=:dot)
 
 # %%
