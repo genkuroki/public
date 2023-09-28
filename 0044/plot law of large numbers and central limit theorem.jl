856488635bfd4e0db106ce21761d6d57c26c5f9d
Gen Kuroki <genkuroki@gmail.com>
2023-09-28 23:09:15 +0900

    updated

--- 0044/plot law of large numbers and central limit theorem.jl
+++ 0044/plot law of large numbers and central limit theorem.jl
@@ -152,3 +152,130 @@ plot_lln_clt(; dist = Poisson(1))
 plot_lln_clt(; dist = Bernoulli(0.1))
 
 # %%
+using Distributions
+
+function meanvarstdskku(dist)
+    m = mean(dist)
+    s2 = var(dist)
+    s = √s2
+    sk = skewness(dist)
+    ku = kurtosis(dist)
+    m, s2, s, sk, ku
+end
+
+function meanvarstdskku(dist::MixtureModel)
+    M1, M2, M3, M4 = moment1234(dist)
+    m = mean(dist)
+    s2 = var(dist)
+    s = √s2
+    s3, s4 = s*s2, s2^2
+    sk = 1/s3 * (M3 - 3m*s2 - m^3)
+    ku = 1/s4 * (M4 - 4m*s3*sk - 6m^2*s2 - m^4) - 3
+    m, s2, s, sk, ku
+end
+
+function moment1234(dist)
+    m, s2, s, sk, ku = meanvarstdskku(dist)
+    s3, s4 = s*s2, s2^2
+    m1 = m
+    m2 = s2 + m^2
+    m3 = s3*sk + 3m*s2 + m^3
+    m4 = s4*(ku + 3) + 4m*s3*sk + 6m^2*s2 + m^4
+    m1, m2, m3, m4
+end
+
+function moment1234(dist::MixtureModel)
+    T = float(eltype(dist))
+    M1, M2, M3, M4 = zero(T), zero(T), zero(T), zero(T)
+    for (d, q) in zip(components(dist), probs(dist))
+        m1, m2, m3, m4 = moment1234(d)
+        M1 += q * m1
+        M2 += q * m2
+        M3 += q * m3
+        M4 += q * m4
+    end
+    M1, M2, M3, M4
+end
+
+# Warning: type piracy
+Distributions.skewness(dist::MixtureModel) = meanvarstdskku(dist)[4]
+Distributions.kurtosis(dist::MixtureModel) = meanvarstdskku(dist)[5]
+
+# %%
+using Distributions
+using Random
+using StatsPlots
+default(fmt=:png)
+
+function plot_lln_clt_meanvar2x2(; dist=Gamma(2, 3), m=5, nbin=200, nns=200, L=10^6)
+    μ, σ = mean(dist), std(dist)
+    sk, ku = skewness(dist), kurtosis(dist)
+    ns = round.(Int, exp.(range(log(1), log(1000), nns)))
+    nmax = maximum(ns)
+
+    X̄ = zeros(L, length(ns))
+    S² = zeros(L, length(ns))
+    Xtmp = [zeros(eltype(dist), nmax) for _ in 1:Threads.nthreads()]
+    @time Threads.@threads for i in 1:L
+        X = rand!(dist, Xtmp[Threads.threadid()])
+        for (j, n) in enumerate(ns)
+            XX = @view X[1:n]
+            X̄[i, j] = mean(XX)
+            S²[i, j] = n == 1 ? (XX[1] - μ)^2 : var(XX)
+        end
+    end
+
+    js = collect(eachindex(ns))
+    js_gif = [fill(js[1], 60); js; fill(js[end], 60)]
+    ns_gif = ns[js_gif]
+    
+    @time @gif for (j, n) in zip(js_gif, ns_gif)
+        bin = if dist isa DiscreteUnivariateDistribution
+            binmin, binmax = floor(n*(μ-m*σ/√n))/n, ceil(n*(μ+m*σ/√n))/n
+            binstep = max(1, round(n*(binmax - binmin)/nbin))/n
+            binmin-binstep/2:binstep:binmax+binstep/2
+        else
+            :auto
+        end
+        
+        P1 = stephist(@view(X̄[:, j]); norm=true, bin, label="")
+        plot!(xlim=(μ-m*σ, μ+m*σ))
+        vline!([μ]; label="μ", ls=:dot, c=3)
+        title!("law of large numbers for sample mean (n=$n)"; titlefontsize=10)
+        plot!(ytick=[0])
+
+        Q1 = stephist(@view(X̄[:, j]); norm=true, bin, label="")
+        plot!(xlim=(μ-m*σ/√n, μ+m*σ/√n))
+        plot!(Normal(μ, σ/√n), μ-m*σ/√n, μ+m*σ/√n; label="Normal(μ, σ/√n)", ls=:dash)
+        vline!([μ]; label="μ", ls=:dot)
+        title!("central limit theorem for sample mean (n=$n)"; titlefontsize=10)
+        plot!(ytick=[0])
+        
+        P2 = stephist(@view(S²[:, j]); norm=true, label="")
+        plot!(xlim=(σ^2-m*σ^2*√(ku+2), σ^2+m*σ^2*√(ku+2)))
+        vline!([σ^2]; label="σ²", ls=:dot, c=3)
+        title!("law of large numbers for unbiased variance (n = $n)"; titlefontsize=10)
+        plot!(ytick=[0])
+
+        std_S² = σ^2 * √(ku/n + 2/max(1, n-1))
+        #alpha = σ^4/std_S²^2
+        #theta = std_S²^2/σ^2
+        Q2 = stephist(@view(S²[:, j]); norm=true, label="")
+        plot!(xlim=(σ^2-m*std_S², σ^2+m*std_S²))
+        plot!(Normal(σ^2, std_S²), σ^2-m*std_S², σ^2+m*std_S²; label="normal approx.", ls=:dash)
+        vline!([σ^2]; label="σ²", ls=:dot)
+        #plot!(Gamma(alpha, theta), σ^2-m*std_S², σ^2+m*std_S²; label="gamma aprrox.", ls=:dashdot)
+        title!("central limit theorem for unbiased variance (n = $n)"; titlefontsize=10)
+        plot!(ytick=[0])
+        
+        plot(P1, P2, Q1, Q2; size=(1000, 500), layout=(2, 2))
+    end
+end
+
+# %%
+plot_lln_clt_meanvar2x2(; dist=Gamma(2, 3))
+
+# %%
+plot_lln_clt_meanvar2x2(; dist=MixtureModel([Normal(), Normal(20)], [0.95, 0.05]))
+
+# %%
--- 0044/skewness and kurtosis of MixtureModel.jl
+++ 0044/skewness and kurtosis of MixtureModel.jl
@@ -16,19 +16,6 @@
 
 # %%
 using Distributions
-using QuadGK
-
-function skewness_quadgk(dist)
-    μ = mean(dist)
-    σ = std(dist)
-    quadgk(x -> ((x-μ)/σ)^3 * pdf(dist, x), extrema(dist)...)[1]
-end
-
-function kurtosis_quadgk(dist)
-    μ = mean(dist)
-    σ = std(dist)
-    quadgk(x -> ((x-μ)/σ)^4 * pdf(dist, x), extrema(dist)...)[1] - 3
-end
 
 function meanvarstdskku(dist)
     m = mean(dist)
@@ -39,17 +26,8 @@ function meanvarstdskku(dist)
     m, s2, s, sk, ku
 end
 
-function meanvarstdskku_quadgk(dist)
-    m = mean(dist)
-    s2 = quadgk(x -> (x - m)^2 * pdf(dist, x), extrema(dist)...)[1]
-    s = √s2
-    sk = quadgk(x -> ((x - m)/s)^3 * pdf(dist, x), extrema(dist)...)[1]
-    ku = quadgk(x -> ((x - m)/s)^4 * pdf(dist, x), extrema(dist)...)[1] - 3
-    m, s2, s, sk, ku
-end
-
 function meanvarstdskku(dist::MixtureModel)
-    M1, M2, M3, M4 = moment1234_quadgk(dist)
+    M1, M2, M3, M4 = moment1234(dist)
     m = mean(dist)
     s2 = var(dist)
     s = √s2
@@ -69,14 +47,6 @@ function moment1234(dist)
     m1, m2, m3, m4
 end
 
-function moment1234_quadgk(dist)
-    m1 = quadgk(x -> x * pdf(dist, x), extrema(dist)...)[1]
-    m2 = quadgk(x -> x^2 * pdf(dist, x), extrema(dist)...)[1]
-    m3 = quadgk(x -> x^3 * pdf(dist, x), extrema(dist)...)[1]
-    m4 = quadgk(x -> x^4 * pdf(dist, x), extrema(dist)...)[1]
-    m1, m2, m3, m4
-end
-
 function moment1234(dist::MixtureModel)
     T = float(eltype(dist))
     M1, M2, M3, M4 = zero(T), zero(T), zero(T), zero(T)
@@ -90,20 +60,109 @@ function moment1234(dist::MixtureModel)
     M1, M2, M3, M4
 end
 
+# Warning: type piracy
+Distributions.skewness(dist::MixtureModel) = meanvarstdskku(dist)[4]
+Distributions.kurtosis(dist::MixtureModel) = meanvarstdskku(dist)[5]
+
 # %%
-T = float(eltype(Bernoulli(0.5)))
-zero(T)
+mix = MixtureModel([Normal(), Normal(20)], [0.95, 0.05])
+@show skewness(mix) kurtosis(mix);
+
+# %%
+mixdisc = MixtureModel([Poisson(), Poisson(10)], [0.9, 0.1])
+@show skewness(mixdisc) kurtosis(mixdisc);
+
+# %%
+mixmix = MixtureModel([mix, Normal(2, 3)], [0.7, 0.3])
+@show skewness(mixmix) kurtosis(mixmix);
+
+# %%
+using Distributions
+using QuadGK
+
+function skewness_approx(dist::ContinuousUnivariateDistribution)
+    μ = mean(dist)
+    σ = std(dist)
+    quadgk(x -> ((x-μ)/σ)^3 * pdf(dist, x), extrema(dist)...)[1]
+end
+
+function skewness_approx(dist::DiscreteUnivariateDistribution)
+    μ = mean(dist)
+    σ = std(dist)
+    xmin = max(round(Int, μ-100σ), minimum(dist))
+    xmax = min(round(Int, μ+100σ), maximum(dist))
+    sum(x -> ((x-μ)/σ)^3 * pdf(dist, x), xmin:xmax)
+end
+
+function kurtosis_approx(dist::ContinuousUnivariateDistribution)
+    μ = mean(dist)
+    σ = std(dist)
+    quadgk(x -> ((x-μ)/σ)^4 * pdf(dist, x), extrema(dist)...)[1] - 3
+end
+
+function kurtosis_approx(dist::DiscreteUnivariateDistribution)
+    μ = mean(dist)
+    σ = std(dist)
+    xmin = max(round(Int, μ-100σ), minimum(dist))
+    xmax = min(round(Int, μ+100σ), maximum(dist))
+    sum(x -> ((x-μ)/σ)^4 * pdf(dist, x), xmin:xmax) - 3
+end
+
+function meanvarstdskku_approx(dist)
+    m = mean(dist)
+    s2 = var(dist)
+    s = √s2
+    sk = skewness_approx(dist)
+    ku = kurtosis_approx(dist)
+    m, s2, s, sk, ku
+end
+
+function _moment_approx(dist::ContinuousUnivariateDistribution, k)
+    quadgk(x -> x^k * pdf(dist, x), extrema(dist)...)[1]
+end
+
+function _moment_approx(dist::DiscreteUnivariateDistribution, k)
+    μ = mean(dist)
+    σ = std(dist)
+    xmin = max(round(Int, μ-100σ), minimum(dist))
+    xmax = min(round(Int, μ+100σ), maximum(dist))
+    sum(x -> x^k * pdf(dist, x), xmin:xmax)
+end
+
+function moment1234_approx(dist)
+    m1, m2, m3, m4 = (_moment_approx(dist, k) for k in 1:4)
+    m1, m2, m3, m4
+end
 
 # %%
 dist = Gamma(2, 3)
-@show meanvarstdskku(dist) meanvarstdskku_quadgk(dist)
-@show moment1234(dist) moment1234_quadgk(dist);
+@show meanvarstdskku(dist) meanvarstdskku_approx(dist)
+@show moment1234(dist) moment1234_approx(dist);
+
+# %%
+dist = Poisson(30)
+@show meanvarstdskku(dist) meanvarstdskku_approx(dist)
+@show moment1234(dist) moment1234_approx(dist);
 
 # %%
 mix = MixtureModel([Normal(), Normal(20)], [0.95, 0.05])
 @show moment1234(mix)
-@show moment1234_quadgk(mix)
+@show moment1234_approx(mix)
 @show meanvarstdskku(mix)
-@show meanvarstdskku_quadgk(mix);
+@show meanvarstdskku_approx(mix);
+
+# %%
+mixdisc = MixtureModel([Poisson(1), Poisson(10)], [0.9, 0.1])
+@show moment1234(mixdisc)
+@show moment1234_approx(mixdisc)
+@show meanvarstdskku(mixdisc)
+@show meanvarstdskku_approx(mixdisc);
+
+# %%
+mixmix = MixtureModel([mix, Normal(2, 3)], [0.7, 0.3])
+@show moment1234(mixmix)
+@show moment1234_approx(mixmix)
+@show meanvarstdskku(mixmix)
+@show meanvarstdskku_approx(mixmix);
 
 # %%
