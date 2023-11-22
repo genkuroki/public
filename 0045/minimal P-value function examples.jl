243d38e5392f08b22c3a9b66df86a71a0c9d3cd5
Gen Kuroki <genkuroki@gmail.com>
2023-11-22 18:28:43 +0900

    updated

--- 0045/minimal P-value function examples.jl
+++ 0045/minimal P-value function examples.jl
@@ -21,13 +21,25 @@ default(fmt=:png, titlefontsize=10)
 safediv(x, y) = x == 0 ? zero(x/y) : x/y
 
 function pvalue_binomial_wilson(k, n, p)
-    phat, sehat = k/n, √(p * (1 - p) / n)
-    z = safediv(phat - p, sehat)
+    phat, se = k/n, √(p * (1 - p) / n)
+    z = safediv(phat - p, se)
     2ccdf(Normal(), abs(z))
 end
 
-k, n = 20, 50
+function confint_binomia_wilson(k, n, α=0.05)
+    phat, z = k/n, quantile(Normal(), 1-α/2)
+    a, b, c = 1+z^2/n, phat+z^2/(2n), phat^2
+    sqrtD = √(b^2 - a*c)
+    [(b - sqrtD)/a, (b + sqrtD)/a]
+end
+
+@show k, n, α = 20, 50, 0.05
+@show confint_binomia_wilson(k, n, α)
+@show pvalue_binomial_wilson(k, n, 0.2)
+@show pvalue_binomial_wilson(k, n, 0.5)
+@show pvalue_binomial_wilson(k, n, 0.6)
 plot(p -> pvalue_binomial_wilson(k, n, p), 0, 1; label="")
+plot!(confint_binomia_wilson(k, n, α), fill(α, 2); label="$(100(1-α))% CI")
 plot!(xtick=0:0.05:1, ytick=0:0.05:1, xrotation=45)
 plot!(xguide="p", yguide="P-value")
 title!("P-value function for data (k=$k, n=$n)
@@ -44,9 +56,20 @@ function pvalue_onesample_t_test(xbar, s2, n, mu)
     2ccdf(TDist(n - 1), abs(t))
 end
 
-xbar, s2, n = 170, 10^2, 128
+function confint_onesample_t_test(xbar, s2, n, α=0.05)
+    sehat, c = √(s2/n), quantile(TDist(n-1), 1-α/2)
+    [xbar - c*sehat, xbar + c*sehat]
+end
+
+@show xbar, s2, n, α = 170, 10^2, 128, 0.05
+@show confint_onesample_t_test(xbar, s2, n, α)
+@show pvalue_onesample_t_test(xbar, s2, n, 165)
+@show pvalue_onesample_t_test(xbar, s2, n, 168)
+@show pvalue_onesample_t_test(xbar, s2, n, 172)
+@show pvalue_onesample_t_test(xbar, s2, n, 175)
 plot(mu -> pvalue_onesample_t_test(xbar, s2, n, mu), 165, 175; label="")
 plot!(xtick=0:0.5:200, ytick=0:0.05:1, xrotation=45)
+plot!(confint_onesample_t_test(xbar, s2, n, α), fill(α, 2); label="$(100(1-α))% CI")
 plot!(xguide="mu", yguide="P-value")
 title!("P-value function for data (x̄=$xbar, s²=$s2, n=$n)
     (model: normal distribution)")
--- 0045/permutation test of mean difference.jl
+++ 0045/permutation test of mean difference.jl
@@ -14,12 +14,14 @@
 #     name: julia-1.9
 # ---
 
-# %%
+# %% tags=[]
 using Printf
 using Random
 Random.seed!(4649373)
 
 using Distributions
+using QuadGK
+using Roots
 using StatsPlots
 default(fmt=:png, titlefontsize=10, size=(500, 350))
 
@@ -131,6 +133,158 @@ Y = randn(100)
 pvalue_student_t_test(X, Y), pvalue(EqualVarianceTTest(X, Y))
 
 # %%
+function winningrate(
+        distx::ContinuousUnivariateDistribution,
+        disty::ContinuousUnivariateDistribution
+    )
+    quadgk(y -> cdf(distx, y)*pdf(disty, y), extrema(disty)...)[1]
+end
+
+function tieshift(
+        distx::ContinuousUnivariateDistribution,
+        disty::ContinuousUnivariateDistribution;
+        p = 1/2
+    )
+    find_zero(0.0) do a
+        winningrate(distx + a, disty) - p
+    end
+end
+
+@show distx = Exponential(1)
+@show disty = Exponential(1.5)
+@show winningrate(distx, disty)
+println()
+@show tiesh = tieshift(distx, disty)
+@show mediansh = median(disty) - median(distx)
+@show meansh = mean(disty) - mean(distx)
+println()
+@show winningrate(distx + tiesh, disty)
+@show winningrate(distx + mediansh, disty)
+@show winningrate(distx + meansh, disty)
+println()
+@show median(distx + tiesh)
+@show median(distx + mediansh)
+@show median(distx + meansh)
+@show median(disty)
+println()
+@show mean(distx + tiesh)
+@show mean(distx + mediansh)
+@show mean(distx + meansh)
+@show mean(disty)
+;
+
+# %%
+using Distributions
+
+@doc raw"""
+    brunner_munzel_test(X, Y; p = 1/2)
+
+この函数は数値のベクトルのデータ `X`, `Y` について, 
+Brunner-Munzel検定関係の統計量達を計算する. 詳細は以下の通り.
+
+函数 $H(x, y)$ と $\hat{p}$, $H^x_i$, $H^y_j$, $\bar{H}^x$, $\bar{H}^y$ を次のように定める:
+
+```math
+\begin{aligned}
+&
+m = \mathrm{length}(X), \quad
+n = \mathrm{length}(Y), \quad
+x_i = X[i], \quad
+y_j = Y[j],
+\\ &
+\hat{p} = \frac{1}{mn}\sum_{i=1}^m \sum_{j=1}^n H(x_i, y_j),
+\\ &
+H(x, y) = \begin{cases} 1 & (x < y) \\ 1/2 & (x = y) \\ 0 & (x > y), \end{cases}
+\\ &
+H^x_i = \sum_{j=1}^n H(y_j, x_i), \quad
+H^y_j = \sum_{i=1}^m H(x_i, y_j),
+\\ &
+\bar{H}^x = \frac{1}{m} \sum_{i=1}^m H^x_i = n - n\hat{p}, \quad
+\bar{H}^y = \frac{1}{n} \sum_{j=1}^n H^y_j = m\hat{p}.
+\end{aligned}
+```
+
+この函数は以下達の named tuple で返す:
+
+```math
+\begin{aligned}
+&
+\mathrm{phat} = 
+\hat{p} = \frac{\bar{H}^y - \bar{H}^x + n}{m + n},
+\\ &
+\mathrm{sx2} =
+\hat{\sigma}_x^2 = \frac{1}{n^2}\frac{1}{m-1}\sum_{i=1}^m (H^x_i - \bar{H}^x)^2,
+\\ &
+\mathrm{sy2} =
+\hat{\sigma}_y^2 = \frac{1}{m^2}\frac{1}{n-1}\sum_{j=1}^n (H^y_j - \bar{H}^y)^2,
+\\ &
+\mathrm{sehat} = 
+\widehat{\mathrm{se}} = \sqrt{\frac{\hat{\sigma}_x^2}{m} + \frac{\hat{\sigma}_y^2}{n}}, 
+\\ &
+\mathrm{tvalue} = t = \frac{\hat{p} - p}{\widehat{\mathrm{se}}},
+\\ &
+\mathrm{df} =
+\nu = 
+\frac
+{\left(\hat{\sigma}_x^2/m + \hat{\sigma}_y^2/n\right)^2}
+{
+\dfrac{\left(\hat{\sigma}_x^2/m\right)^2}{m-1} +
+\dfrac{\left(\hat{\sigma}_y^2/n\right)^2}{n-1}
+},
+\\ &
+\mathrm{pvalue} =
+2\mathrm{ccdf}(\mathrm{TDist}(\nu), |t|),
+\\ &
+\mathrm{p} = p.
+\end{aligned}
+```
+"""
+function brunner_munzel_test(X, Y; p=1/2)
+    m, n = length(X), length(Y)
+    phat = mean((x < y) + (x == y)/2 for x in X, y in Y)
+    Hbarx = n*(1 - phat)
+    Hbary = m*phat
+    sx2 = 1/n^2 * 1/(m-1) * sum(x -> (sum((y < x) + (y == x)/2 for y in Y) - Hbarx)^2, X)
+    sy2 = 1/m^2 * 1/(n-1) * sum(y -> (sum((x < y) + (x == y)/2 for x in X) - Hbary)^2, Y)
+    sehat = √(sx2/m + sy2/n)
+    tvalue = (phat - p)/sehat
+    df = (sx2/m + sy2/n)^2 / ((sx2/m)^2/(m-1) + (sy2/n)^2/(n-1))
+    pvalue = sehat > 0 ? 2ccdf(TDist(df), abs(tvalue)) : phat ≈ p ? 1.0 : 0.0
+    (; phat, sehat, tvalue, df, pvalue, p)
+end
+
+@doc raw"""
+    pvalue_brunner_munzel_test(X, Y; p = 1/2)
+
+この函数はBrunner-Munzel検定のP値 `pvalue` を返す.
+"""
+pvalue_brunner_munzel_test(X, Y; p=1/2) = brunner_munzel_test(X, Y; p).pvalue
+
+using RCall
+@rimport lawstat
+X = rand(100)
+Y = rand(100)
+pvalue_brunner_munzel_test(X, Y), rcopy(lawstat.brunner_munzel_test(X, Y))[:p_value]
+
+# %%
+function mann_whitney_u_test(X, Y; correct=true)
+    m, n = length(X), length(Y)
+    phat = mean((x < y) + (x == y)/2 for x in X, y in Y)
+    sehat = √((m+n+1)/(12m*n))
+    zvalue = (phat - 1/2)/sehat
+    correction = correct/(2m*n*sehat)
+    pvalue = 2ccdf(Normal(), max(0, abs(zvalue) - correction))
+    (; phat, sehat, zvalue, pvalue)
+end
+
+pvalue_mann_whitney_u_test(X, Y; correct=true) = mann_whitney_u_test(X, Y; correct).pvalue
+
+using HypothesisTests
+X = randn(100)
+Y = randn(100)
+pvalue_mann_whitney_u_test(X, Y), pvalue(ApproximateMannWhitneyUTest(X, Y))
+
+# %%
 x ⪅ y = x < y || x ≈ y
 
 function pvalue_permutation_test(f, X, Y, XY=[X; Y]; Nshuffles=10^4)
@@ -228,6 +382,8 @@ Y = [1, 7, 8, 9, 7, 8]
 @show pvalue_permutation_welch_t_test(X, Y; Nshuffles=10^7)
 @show pvalue_welch_t_test(X, Y)
 @show pvalue_bootstrap_diffmean(X, Y; Nsamples=10^7)
+@show pvalue_mann_whitney_u_test(X, Y)
+@show pvalue_brunner_munzel_test(X, Y)
 ;
 
 # %%
@@ -239,6 +395,8 @@ Y = [1, 7, 8, 9]
 @show pvalue_permutation_welch_t_test(X, Y; Nshuffles=10^6)
 @show pvalue_welch_t_test(X, Y)
 @show pvalue_bootstrap_diffmean(X, Y; Nsamples=10^7)
+@show pvalue_mann_whitney_u_test(X, Y)
+@show pvalue_brunner_munzel_test(X, Y)
 ;
 
 # %%
@@ -262,6 +420,8 @@ println()
 @show pvalue_welch_t_test(X, Y)
 @show true_pvalue_welch_t_test(distx, disty, X, Y; Nsamples=10^6)
 @show pvalue_bootstrap_diffmean(X, Y; Nsamples=10^7)
+@show pvalue_mann_whitney_u_test(X, Y)
+@show pvalue_brunner_munzel_test(X, Y)
 ;
 
 # %%
@@ -285,6 +445,8 @@ println()
 @show pvalue_welch_t_test(X, Y)
 @show true_pvalue_welch_t_test(distx, disty, X, Y; Nsamples=10^6)
 @show pvalue_bootstrap_diffmean(X, Y; Nsamples=10^7)
+@show pvalue_mann_whitney_u_test(X, Y)
+@show pvalue_brunner_munzel_test(X, Y)
 ;
 
 # %%
@@ -420,7 +582,7 @@ m, n = 100, 25
 L = Nshuffles = 5000
 @time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
 
-# %%
+# %% tags=[]
 distx, disty = Normal(0, 2), Normal(0, 1)
 m, n = 70, 100
 L = Nshuffles = 5000
@@ -482,6 +644,214 @@ L = Nshuffles = 5000
 distx, disty = inversegammadist(2, 3), inversegammadist(1, 3)
 distx = distx + mean(disty) - mean(distx)
 m, n = 100, 25
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 30, 60
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 50, 100
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 100, 200
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 200, 400
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 20, 40
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 10, 20
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 10)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-0.1, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 5, 10
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = Normal(0, 4)
+disty = Normal(0, 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-15, 15))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 10, 20
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = Normal(0, 4)
+disty = Normal(0, 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(-15, 15))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 5, 10
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 2)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(0, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 50, 100
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 2)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(0, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 20, 40
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 2)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(0, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 10, 20
+
+L = Nshuffles = 5000
+@time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
+
+# %%
+distx = inversegammadist(4, 2)
+disty = Normal(mean(distx), 1)
+plot(1distx; label="distx")
+plot!(1disty; label="disty", ls=:dash)
+plot!(xlim=(0, 20))
+plot!(size=(300, 180)) |> display
+
+@show mean(distx) ≈ mean(disty)
+@show std(distx), std(disty)
+println()
+m, n = 5, 10
+
 L = Nshuffles = 5000
 @time plot_sim_diff_shuffle(; distx, m, disty, n, L, Nshuffles)
 
