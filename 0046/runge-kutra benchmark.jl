451bed7135b30f313fbd6e80e629224d5e3ca38d
Gen Kuroki <genkuroki@gmail.com>
2023-12-27 23:34:26 +0900

    updated

--- 0046/runge-kutra benchmark.jl
+++ 0046/runge-kutra benchmark.jl
@@ -438,3 +438,58 @@ print("main_inline_fastmath_inbounds_simd(nt):"); @btime main_inline_fastmath_in
 versioninfo()
 
 # %%
+function main_inline_fastmath_inbounds_simd2(nt = 100000000)
+    mass = 1.0
+    k = 1.0
+    dt = 1e-2
+
+    xt = zeros(Float64, nt+1)
+    vt = zeros(Float64, nt+1)
+
+    x = 0.0
+    v = 1.0
+
+    @inline @inbounds @simd for it in 1:nt+1
+        xt[it] = x
+        vt[it] = v
+        x, v = Runge_Kutta_4th_fastmath(x, v, dt, mass, k)
+    end
+
+    open("result_julia.out", "w") do file
+        for it = nt-999:nt
+            println(file, "$(it*dt) $(xt[it]) $(vt[it])")
+        end
+    end
+end
+
+@fastmath function Runge_Kutta_4th_fastmath(x, v, dt, mass, k)
+    x1 = v
+    v1 = force(x, mass, k)
+
+    x2 = v + 0.5 * dt * v1
+    v2 = force(x + 0.5 * x1 * dt, mass, k)
+
+    x3 = v + 0.5 * dt * v2
+    v3 = force(x + 0.5 * x2 * dt, mass, k)
+
+    x4 = v + dt * v3
+    v4 = force(x + x3 * dt, mass, k)
+
+    x += (x1 + 2 * x2 + 2 * x3 + x4) * dt / 6
+    v += (v1 + 2 * v2 + 2 * v3 + v4) * dt / 6
+
+    return x, v
+end
+
+function force(x, mass, k)
+    return -x * k / mass
+end
+
+@time main_inline_fastmath_inbounds_simd2()
+@time main_inline_fastmath_inbounds_simd2()
+@time main_inline_fastmath_inbounds_simd2()
+
+# %%
+print("main_inline_fastmath_inbounds_simd2(nt):"); @btime main_inline_fastmath_inbounds_simd2(nt)
+
+# %%
