9fb4889395505cfc36f7071eef81f3805eeb38df
Gen Kuroki <genkuroki@gmail.com>
2024-01-05 11:43:14 +0900

    created

---
+++ 0046/1 + 2 sum of sin(k) over k.jl
@@ -0,0 +1,78 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,jl:hydrogen
+#     text_representation:
+#       extension: .jl
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Julia 1.10.0
+#     language: julia
+#     name: julia-1.10
+# ---
+
+# %%
+using BenchmarkTools
+versioninfo()
+
+# %%
+function f(L=10^8)
+    s = 0.0
+    for k in 1:L
+        s += sin(k)/k
+    end
+    1 + 2s
+end
+
+@btime f()
+
+# %%
+@code_native debuginfo=:none f()
+
+# %%
+using LoopVectorization
+
+"single-thread optimization"
+function f_turbo(L=10^8)
+    s = 0.0
+    @turbo for k in 1:L
+        s += sin(k)/k
+    end
+    1 + 2s
+end
+
+@btime f_turbo()
+
+# %%
+using LoopVectorization
+
+"multi-thread optimization"
+function f_tturbo(L=10^8)
+    s = 0.0
+    @tturbo for k in 1:L
+        s += sin(k)/k
+    end
+    1 + 2s
+end
+
+@show Threads.nthreads()
+@btime f_tturbo()
+
+# %%
+@code_native debuginfo=:none f_turbo(10^8)
+
+# %%
+@macroexpand function f_turbo(L=10^8)
+    s = 0.0
+    @turbo for k in 1:L
+        s += sin(k)/k
+    end
+    1 + 2s
+end
+
+# %%
+@time f_tturbo(10^9)
+
+# %%
---
+++ 0046/sumofsinc/sumofsinc.f90
@@ -0,0 +1,25 @@
+program avx
+implicit none
+integer, parameter :: dp = kind(0.d0)
+real(dp) :: t1, t2, r
+
+call cpu_time(t1)
+r = f(100000000)
+call cpu_time(t2)
+
+print *, "Time", t2-t1
+print *, r
+
+contains
+
+    real(dp) function f(N) result(r)
+    integer, intent(in) :: N
+    integer :: i
+    r = 0
+    do i = 1, N
+        r = r + sin(real(i,dp)) / real(i,dp)
+    end do
+    r = 1.0d0 + 2.0d0 * r
+    end function
+
+end program
