51e7051309d822ac8e3775e1badcfdec15fbd64d
Gen Kuroki <genkuroki@gmail.com>
2024-08-03 16:37:32 +0900

    created

--- 0049/round df or not.jl
+++ 0049/round df or not.jl
@@ -16,23 +16,29 @@
 
 # %%
 using Distributions
-using StatsPlots
-default(fmt=:png)
+#using StatsPlots
+#default(fmt=:png)
 
-pvalue_welch_t(X, Y; Δμ=0.0, roundfunc=identity) =
-    pvalue_welch_t(length(X), mean(X), var(X), length(Y), mean(Y), var(Y); Δμ, roundfunc)
-
-function pvalue_welch_t(m, X̄, U, n, Ȳ, V; Δμ=0.0, roundfunc=round)
-    t = (X̄ - Ȳ - Δμ) / √(U/m + V/n)
-    df = degree_of_freedom(m, U, n, V)
-    2ccdf(TDist(roundfunc(df)), abs(t))
+function pvalue_welch_t(m, X̄, U, n, Ȳ, V; Δμ=0.0, roundfunc=identity, dffunc=degree_of_freedom)
+    t = tvalue_welch(m, X̄, U, n, Ȳ, V; Δμ)
+    dfhat = dffunc(m, U, n, V)
+    2ccdf(TDist(roundfunc(dfhat)), abs(t))
 end
 
-function degree_of_freedom(m, U, n, V)
-    (U/m + V/n)^2 / ((U/m)^2/(m-1) + (V/n)^2/(n-1))
-end
+pvalue_welch_t(X, Y; Δμ=0.0, roundfunc=identity, dffunc=degree_of_freedom) =
+    pvalue_welch_t(length(X), mean(X), var(X), length(Y), mean(Y), var(Y); Δμ, roundfunc, dffunc)
 
+tvalue_welch(m, X̄, U, n, Ȳ, V; Δμ=0.0) = (X̄ - Ȳ - Δμ) / √(U/m + V/n)
+tvalue_welch(X, Y; Δμ=0.0) =
+    tvalue_welch(length(X), mean(X), var(X), length(Y), mean(Y), var(Y); Δμ)
+
+degree_of_freedom(m, U, n, V) = (U/m + V/n)^2 / ((U/m)^2/(m-1) + (V/n)^2/(n-1))
 degree_of_freedom(X, Y) = degree_of_freedom(length(X), var(X), length(Y), var(Y))
+
+degree_of_freedom_not_Satterthwaite(m, U, n, V) =
+    (U/m + V/n)^2 / ((U/m)^2/(m+1) + (V/n)^2/(n+1)) - 2
+degree_of_freedom_not_Satterthwaite(X, Y) =
+    degree_of_freedom_not_Satterthwaite(length(X), var(X), length(Y), var(Y))
 
 X = [80, 87, 78, 72]
 Y = [119, 78, 95, 124, 85, 92]
@@ -88,5 +94,109 @@ end
 @show pvalue_welch_t(X, Y);
 @show pvalue_welch_t(X, Y; roundfunc=round);
 # UnequalVarianceTTest(X, Y)
+
+# %% [markdown]
+# ## R
+
+# %%
+using RCall
+X = [80, 87, 78, 72]
+Y = [119, 78, 95, 124, 85, 92]
+@rput X Y
+R"""t.test(X, Y)"""
+
+# %%
+@show tvalue_welch(X, Y);
+@show degree_of_freedom(X, Y);
+@show pvalue_welch_t(X, Y);
+
+# %% [markdown]
+# ## STATA
+
+# %% [markdown]
+# https://www.statology.org/welchs-t-test-stata/
+#
+# <img src="IMG_5204.png">
+
+# %%
+m, X̄, U = 12, 21.00, 2.730301^2
+n, Ȳ, V = 12, 22.75, 3.250874^2
+@show (; N=m, Mean=X̄, Std=√U, SE=√(U/m));
+@show (; N=n, Mean=Ȳ, Std=√V, SE=√(V/n));
+@show tvalue_welch(m, X̄, U, n, Ȳ, V);
+println()
+@show degree_of_freedom(m, U, n, V);
+@show pvalue_welch_t(m, X̄, U, n, Ȳ, V);
+println()
+@show degree_of_freedom_not_Satterthwaite(m, U, n, V);
+@show pvalue_welch_t(m, X̄, U, n, Ȳ, V; dffunc=degree_of_freedom_not_Satterthwaite);
+
+# %%
+using SymPy
+@syms U V m n
+expr = m + n - degree_of_freedom_not_Satterthwaite(m, U, n, V)
+factor(expr)
+
+# %%
+using SymPy
+@syms U V m n
+expr = m + n - 2 - degree_of_freedom(m, U, n, V)
+factor(expr)
+
+# %%
+using SymPy
+@syms U V m n
+expr = m + n - (U/m + V/n)^2 / ((U/m)^2/m + (V/n)^2/n)
+factor(expr)
+
+# %% [markdown]
+# https://stats.oarc.ucla.edu/stata/output/t-test/
+#
+# <img src="IMG_5209.jpeg" width=60%>
+
+# %%
+m, X̄, U =  91, 50.12088, 10.30516^2
+n, Ȳ, V = 109, 54.99083, 8.133715^2
+@show (; N=m, Mean=X̄, Std=√U, SE=√(U/m));
+@show (; N=n, Mean=Ȳ, Std=√V, SE=√(V/n));
+@show tvalue_welch(m, X̄, U, n, Ȳ, V);
+@show degree_of_freedom(m, U, n, V);
+@show pvalue_welch_t(m, X̄, U, n, Ȳ, V);
+
+# %% [markdown]
+# ## SAS
+
+# %% [markdown]
+# https://support.sas.com/documentation/onlinedoc/stat/132/ttest.pdf
+#
+# <img src="IMG_5210.jpeg" width=60%>
+# <img src="IMG_5211.jpeg" width=50%>
+
+# %%
+m, X̄, U = 7, 76.8571, 2.5448^2
+n, Ȳ, V = 7, 82.7143, 3.1472^2
+@show (; N=m, Mean=X̄, Std=√U, SE=√(U/m));
+@show (; N=n, Mean=Ȳ, Std=√V, SE=√(V/n));
+@show tvalue_welch(m, X̄, U, n, Ȳ, V);
+println()
+@show degree_of_freedom(m, U, n, V);
+@show pvalue_welch_t(m, X̄, U, n, Ȳ, V);
+
+# %% [markdown]
+# ## SPSS
+
+# %% [markdown]
+# https://www.stats-guild.com/analytics/15678
+#
+# <img src="IMG_5212.jpeg" width=100%>
+
+# %%
+m, X̄, U = 115, 3054.17, 751.684^2
+n, Ȳ, V =  74, 2773.16, 660.340^2
+@show (; N=m, Mean=X̄, Std=√U, SE=√(U/m));
+@show (; N=n, Mean=Ȳ, Std=√V, SE=√(V/n));
+@show tvalue_welch(m, X̄, U, n, Ȳ, V);
+@show degree_of_freedom(m, U, n, V);
+@show pvalue_welch_t(m, X̄, U, n, Ȳ, V);
 
 # %%
