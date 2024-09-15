a4d71c26155a41eb8764e3f59883a8919fb8ef9a
Gen Kuroki <genkuroki@gmail.com>
2024-09-15 11:32:03 +0900

    updated

--- 0050/HypergeometricFunctions.jl and distribution of ratio of Betas.jl
+++ 0050/HypergeometricFunctions.jl and distribution of ratio of Betas.jl
@@ -19,6 +19,7 @@
 
 # %%
 using Distributions
+using ForwardDiff
 using HypergeometricFunctions
 using QuadGK
 using SpecialFunctions
@@ -28,77 +29,159 @@ default(fmt=:png)
 
 function pdf_betaratio(κ, λ, μ, ν, x)
     if x < 0
-        0.0
+        -Inf
     elseif x ≤ 1
-        exp(logbeta(κ+μ, ν) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(κ-1, x) + 
-            log(max(0, _₂F₁(κ+μ, 1-λ, κ+μ+ν, x))))
+        exp(logbeta(κ+μ, ν) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(κ-1, x)) * 
+        _₂F₁(κ+μ, 1-λ, κ+μ+ν, x)
     elseif x < Inf
-        exp(logbeta(κ+μ, λ) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(-μ-1, x) +
-        log(max(0, _₂F₁(κ+μ, 1-ν, κ+μ+λ, 1/x))))
+        exp(logbeta(κ+μ, λ) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(-μ-1, x)) *
+        _₂F₁(κ+μ, 1-ν, κ+μ+λ, 1/x)
     else
-        Inf
-    end
-    
+        -Inf
+    end  
 end
 
-# %%
-κ, λ, μ, ν = 2, 3, 4, 5
-f = x -> pdf_betaratio(κ, λ, μ, ν, x)
-@show quadgk(f, 0, Inf)[1]
-L = 10^6
-X = rand(Beta(κ, λ), L)
-Y = rand(Beta(μ, ν), L)
-R = @. X / Y
-a, b = -0.2, min(5, maximum(R))
-#a, b = 0.9, 1.1
-plot()
-stephist!(R; norm=true, label="")
-plot!(f, extrema(R)...; label="")
-plot!(xlim=(a, b))
+function cdf_betaratio(κ, λ, μ, ν, x)
+    if x < 0
+        0.0
+    elseif x ≤ 1
+        exp(logbeta(κ+μ, ν) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(κ, x) - log(κ)) * 
+        _₃F₂(κ, κ+μ, 1-λ, κ+1, κ+μ+ν, x)
+    elseif x < Inf
+        1 - exp(logbeta(κ+μ, λ) - logbeta(κ, λ) - logbeta(μ, ν) + xlogy(-μ, x) - log(μ)) * 
+        _₃F₂(μ, κ+μ, 1-ν, μ+1, κ+μ+λ, 1/x)
+    else
+        1.0
+    end  
+end
 
 # %%
 κ, λ, μ, ν = 5, 10, 8, 13
 f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
 @show quadgk(f, 0, Inf)[1]
 L = 10^6
 X = rand(Beta(κ, λ), L)
 Y = rand(Beta(μ, ν), L)
 R = @. X / Y
 a, b = -0.2, min(5, maximum(R))
-#a, b = 0.9, 1.1
-plot()
-stephist!(R; norm=true, label="")
-plot!(f, extrema(R)...; label="")
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
 plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
 
 # %%
 κ, λ, μ, ν = 20, 30, 15, 30
 f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
 @show quadgk(f, 0, Inf)[1]
 L = 10^6
 X = rand(Beta(κ, λ), L)
 Y = rand(Beta(μ, ν), L)
 R = @. X / Y
 a, b = -0.2, min(5, maximum(R))
-#a, b = 0.9, 1.1
-plot()
-stephist!(R; norm=true, label="")
-plot!(f, extrema(R)...; label="")
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
 plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
 
 # %%
 κ, λ, μ, ν = 20, 30, 30, 30
 f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
 @show quadgk(f, 0, Inf)[1]
 L = 10^6
 X = rand(Beta(κ, λ), L)
 Y = rand(Beta(μ, ν), L)
 R = @. X / Y
 a, b = -0.2, min(5, maximum(R))
-#a, b = 0.9, 1.1
-plot()
-stephist!(R; norm=true, label="")
-plot!(f, extrema(R)...; label="")
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
 plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
+
+# %%
+κ, λ, μ, ν = 20, 30, 30, 40
+f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
+@show quadgk(f, 0, Inf)[1]
+L = 10^6
+X = rand(Beta(κ, λ), L)
+Y = rand(Beta(μ, ν), L)
+R = @. X / Y
+a, b = -0.2, min(5, maximum(R))
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
+plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
+
+# %%
+κ, λ, μ, ν = 20, 30, 30, 40
+f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
+#@show quadgk(f, 0, Inf)[1]
+L = 10^6
+X = rand(Beta(κ, λ), L)
+Y = rand(Beta(μ, ν), L)
+R = @. X / Y
+a, b = -0.2, min(5, maximum(R))
+a, b = 0.95, 1.2
+P1 = stephist(R; norm=true, label="")
+plot!(x -> ForwardDiff.derivative(f, x), a, b; label="")
+plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
+
+# %%
+κ, λ, μ, ν = 30, 40, 40, 50
+f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
+#@show quadgk(f, 0, Inf)[1]
+L = 10^6
+X = rand(Beta(κ, λ), L)
+Y = rand(Beta(μ, ν), L)
+R = @. X / Y
+a, b = -0.2, min(5, maximum(R))
+#a, b = 0.95, 1.2
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
+plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
+
+# %%
+κ, λ, μ, ν = 30, 40, 40, 50
+f = x -> pdf_betaratio(κ, λ, μ, ν, x)
+F = x -> cdf_betaratio(κ, λ, μ, ν, x)
+#@show quadgk(f, 0, Inf)[1]
+L = 10^6
+X = rand(Beta(κ, λ), L)
+Y = rand(Beta(μ, ν), L)
+R = @. X / Y
+a, b = -0.2, min(5, maximum(R))
+a, b = 0.95, 1.2
+P1 = stephist(R; norm=true, label="")
+plot!(f, a, b; label="")
+plot!(xlim=(a, b))
+P2 = plot(x -> count(≤(x), R)/length(R), a, b; label="")
+plot!(F, a, b; label="")
+plot!(xlim=(a, b))
+plot(P1, P2; size=(800, 300))
 
 # %%
