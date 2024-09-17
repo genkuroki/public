9bf3afab95d2971f94e1568b09808e8e92315116
Gen Kuroki <genkuroki@gmail.com>
2024-09-17 09:30:30 +0900

    revised

--- 0050/comparison between Bayes and P-value.jl
+++ 0050/comparison between Bayes and P-value.jl
@@ -82,7 +82,8 @@ function print_and_show_results_bayes(;
         N = 10^5,
         nchains = min(Threads.nthreads(), 10),
         legend = :outertop,
-        xlim_RR = nothing, 
+        xlim_RR = nothing,
+        sigdigits = 3,
         kwargs...
     )
     chain = sample(model_rr(a, b, c, d; prior_logRR), NUTS(), MCMCThreads(), N, nchains)
@@ -96,12 +97,12 @@ function print_and_show_results_bayes(;
 
     println("prior of log(RR): ", prior_logRR)
     println("new data: ", [a b; c d])
-    println("posterior probabilities of RR ≤ 1, 0.9, 0.8, 0.67 = ", 
-        round.(_ecdf.((logRR,), log.((1.0, 0.9, 0.8, 0.67))); sigdigits=4))
-    println("posterior quantiles of (0.025, 0.25, 0.5, 0.75, 0.975) = ", 
-        round.(exp.(Q_logRR); sigdigits=4))
-    println("posterior $(100(1-α))% HDI = ", round.(exp.(hdi_logRR); sigdigits=4))
-    println("mode of posterior = ", round.(exp.(mode_logRR); sigdigits=4))
+    println("posterior probabilities of RR ≤ 1, 0.9, 0.8, 0.67: ", 
+        round.(_ecdf.((logRR,), log.((1.0, 0.9, 0.8, 0.67))); sigdigits))
+    println("posterior quantiles of (0.025, 0.25, 0.5, 0.75, 0.975): ", 
+        round.(exp.(Q_logRR); sigdigits))
+    println("posterior $(100(1-α))% HDI: ", round.(exp.(hdi_logRR); sigdigits))
+    println("posterior point estimate (MAP): ", round.(exp.(mode_logRR); sigdigits))
     if isnothing(xlim_RR)
         xlim_RR = extrema(exp.(logRR)) .* (0.8, 1.4)
     end
@@ -111,7 +112,7 @@ function print_and_show_results_bayes(;
     plot(f, xlim_logRR...; norm=true, label="posterior density of log(RR)")
     plot!(prior_logRR, xlim_logRR...; label="prior density of log(RR)", ls=:dash)
     plot!(hdi_logRR, f.(hdi_logRR); label="posterior $(100(1-α))% HDI", lw=3)
-    vline!([mode_logRR]; label="mode of posterior density", ls=:dot)
+    vline!([mode_logRR]; label="posterior point estimate (MAP)", ls=:dot)
     vline!([0.0]; label="RR = 1", c=:black, alpha=0.7, lw=0.5)
     plot!(; xguide="RR", xtick=xtick_logRR)
     plot!(; legend, kwargs...)
@@ -173,6 +174,27 @@ function confint_rr_score(a, b, c, d; α=0.05)
     [exp(L), exp(U)]
 end
 
+function logtick(; xlim=(0.03, 500))
+    xmin, xmax = xlim
+    a = floor(Int, log10(xmin))
+    b = ceil(Int, log10(xmax))
+    nums =     [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    mask = Bool[1, 1, 0, 0, 1, 0, 0, 0, 0]
+    
+    logtick = foldl(vcat, ([10.0^k*x for x in nums if xmin ≤ 10.0^k*x ≤ xmax] for k in a:b))
+    nticks = length(logtick)
+    logticklabel_a = foldl(vcat,
+        ([(nticks ≤ 10 || mask[i]) ? string(round(10.0^k*x; digits=-k)) : ""
+                for (i, x) in enumerate(nums) if xmin ≤ 10.0^k*x ≤ xmax]
+            for k in a:-1))
+    logticklabel_b = foldl(vcat,
+        ([(nticks ≤ 10 || mask[i]) ? string(10^k*x) : ""
+                for (i, x) in enumerate(nums) if xmin ≤ 10.0^k*x ≤ xmax]
+            for k in 0:b))
+    logticklabel = vcat(logticklabel_a, logticklabel_b)
+    (logtick, logticklabel)
+end
+
 function print_and_show_results_pvalue(;
         a = 44,
         b = 124-44,
@@ -183,6 +205,7 @@ function print_and_show_results_pvalue(;
         α = 0.05,
         legend = :outertop,
         xlim_RR = nothing,
+        sigdigits = 3,
         kwargs...
     )
     
@@ -191,7 +214,7 @@ function print_and_show_results_pvalue(;
         q̃ = c/(c+d)
         p̃ = min(1, q̃ * RR0)
         ã, b̃, c̃, d̃ = round.(Int, (n0*p̃, n0*(1-p̃), n0*q̃, n0*(1-q̃)))
-        prior_data_setting
+        println("prior data settin: RR = $(RR0),  sample size = $(n0)")
     else
         ã, b̃, c̃, d̃ = prior_data
     end
@@ -202,20 +225,23 @@ function print_and_show_results_pvalue(;
     ci = confint_rr_score(aa, bb, cc, dd; α)
     pe = _riskratiohat(aa, bb, cc, dd)
     g(ρ) = pvalue_rr_score(aa, bb, cc, dd; ρ)
+    gg(ρ) = ρ < pe ? g(ρ)/2 : 1 - g(ρ)/2
     h(ρ) = pvalue_rr_score(ã, b̃, c̃, d̃; ρ)
     
-    println("posterior $(100(1-α))% confidence interval of RR = ", round.(ci; sigdigits=4))
-    println("posterior point estimate (MLE) of RR = ", round.(pe; sigdigits=4))
+    println("posterior P-values of RR ≤ 1, 0.9, 0.8, 0.67: ", round.(gg.((1.0, 0.9, 0.8, 0.67)); sigdigits))
+    println("posterior P-values of RR = 1, 0.9, 0.8, 0.67: ", round.(g.((1.0, 0.9, 0.8, 0.67)); sigdigits))
+    println("posterior $(100(1-α))% confidence interval of RR: ", round.(ci; sigdigits))
+    println("posterior point estimate (MLE) of RR: ", round.(pe; sigdigits))
     if isnothing(xlim_RR)
         xlim_RR = confint_rr_score(aa, bb, cc, dd; α=0.005) .* (0.6, 1.67)
     end
     xtick_RR = logtick(; xlim=xlim_RR)
     plot(g, xlim_RR...; norm=true, label="posterior P-value function")
     n0 ≥ 10 && plot!(h, xlim_RR...; label="prior P-value function", ls=:dash, c=2)
-    plot!(ci, fill(α, 2); label="posterior $(100(1-α))% CI", lw=3, c=3)
+    plot!(ci, fill(α, 2); label="posterior $(100(1-α))% confidence interval", lw=3, c=3)
     vline!([pe]; label="posterior point estimate (MLE)", ls=:dot, c=4)
     vline!([1.0]; label="RR = 1", c=:black, alpha=0.7, lw=0.5)
-    plot!(; xguide="RR", xscale=:log10, xtick=xtick_RR)
+    plot!(; xguide="RR", xscale=:log10, xtick=xtick_RR, ytick=0:0.05:1)
     #plot!(; xguide="RR", xscale=:log10, xtick=xtick_RR)
     plot!(; legend, kwargs...)
 end
@@ -260,6 +286,6 @@ print_and_show_results_pvalue(; prior_data_setting=(RR0=1.5, n0=30), xlim_RR=(0.
 print_and_show_results_bayes(; prior_logRR=Normal(log(1.2), 0.15), xlim_RR=(0.3, 2.1))
 
 # %%
-print_and_show_results_pvalue(; prior_data_setting=(RR0=1.2, n0=110), xlim_RR=(0.3, 2.1))
+print_and_show_results_pvalue(; prior_data_setting=(RR0=1.2, n0=100), xlim_RR=(0.3, 2.1))
 
 # %%
