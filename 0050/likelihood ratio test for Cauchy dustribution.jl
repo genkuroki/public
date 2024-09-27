7af749a567e7833658dc7a02bca128f3e07d2f5d
Gen Kuroki <genkuroki@gmail.com>
2024-09-27 17:48:59 +0900

    add normal cases

--- 0050/likelihood ratio test for Cauchy dustribution.jl
+++ 0050/likelihood ratio test for Cauchy dustribution.jl
@@ -23,7 +23,11 @@ default(fmt=:png)
 
 x ⪅ y = x < y || x ≈ y
 
-likrat(x; μ=1) = exp(logpdf(Cauchy(μ), x) - logpdf(Cauchy(), x))
+loglikrat(x; μ=1) = logpdf(Cauchy(μ), x) - logpdf(Cauchy(), x)
+likrat(x; μ=1) = exp(loglikrat(x; μ))
+
+loglikrat_normal(x; μ=1) = logpdf(Normal(μ), x) - logpdf(Normal(), x)
+likrat_normal(x; μ=1) = exp(loglikrat_normal(x; μ))
 
 function min_likrat_optim(; μ=1)
     f(x) = likrat(x; μ)
@@ -69,33 +73,75 @@ function rejprob_likrat(λ; μ=1, dist=Cauchy())
     end
 end
 
+function rejprob_likrat_normal(λ; μ=1, dist=Normal())
+    @assert μ > 0
+    c = (log(λ) + μ^2/2) / μ
+    ccdf(dist, c)
+end
+
 function lambda_likrat(α; μ=1)
+    @assert μ > 0
     1 ⪅ α && return min_likrat(; μ)
     α ⪅ 0 && return max_likrat(; μ)
     f(λ) = rejprob_likrat(λ; μ) - α
     find_zero(f, (min_likrat(; μ), max_likrat(; μ)))
 end
 
+function lambda_likrat_normal(α; μ=1)
+    @assert μ > 0
+    c = cquantile(Normal(), α)
+    exp(μ*c - μ^2/2)
+end
+
 function power_likrat(α; μ=1)
+    @assert μ > 0
     λ = lambda_likrat(α; μ)
     rejprob_likrat(λ; μ, dist=Cauchy(μ))
 end
 
+function power_likrat_normal(α; μ=1)
+    @assert μ > 0
+    λ = lambda_likrat_normal(α; μ)
+    rejprob_likrat_normal(λ; μ, dist=Normal(μ))
+end
+
 function power_onesided(α; μ=1)
-    x = cquantile(Cauchy(), α)
-    ccdf(Cauchy(μ), x)
+    @assert μ > 0
+    c = cquantile(Cauchy(), α)
+    ccdf(Cauchy(μ), c)
+end
+
+function power_onesided_normal(α; μ=1)
+    @assert μ > 0
+    c = cquantile(Normal(), α)
+    ccdf(Normal(μ), c)
 end
 
 function pvalue_likrat(x; μ=1)
+    @assert μ > 0
     λ = likrat(x; μ)
     rejprob_likrat(λ; μ)
 end
 
+function pvalue_likrat_normal(x; μ=1)
+    @assert μ > 0
+    λ = likrat_normal(x; μ)
+    rejprob_likrat_normal(λ; μ)
+end
+
 function pvalue_onesided(x; μ=1)
+    @assert μ > 0
     ccdf(Cauchy(), x)
 end
 
+function pvalue_onesided_normal(x; μ=1)
+    @assert μ > 0
+    ccdf(Normal(), x)
+end
+
 function plot_likrattest(; μ=1)
+    @assert μ > 0
+    
     a = argmin_likrat(; μ)
     b = argmax_likrat(; μ)
     f(x) = likrat(x; μ) - 0.7
@@ -107,7 +153,8 @@ function plot_likrattest(; μ=1)
     #    label="", ylim=(0, 1.05max_likrat(; μ)))
     #plot!(xguide="x", yguide="likelihood ratio")
     
-    P1 = plot(x -> -log(likrat(x; μ)), c, d; label="")
+    P1 = plot(x -> -loglikrat(x; μ), c, d; label="")
+    #plot!(x -> -loglikrat_normal(x; μ), c, d; label="normal case", ls=:dash, c=4)
     plot!(xguide="x", yguide="−log likelihood ratio")
     
     #P2 = plot(λ -> rejprob_likrat(λ; μ), min_likrat(; μ), max_likrat(; μ); label="")
@@ -130,9 +177,35 @@ function plot_likrattest(; μ=1)
     plot!(xguide="x", yguide="P-value")
     
     plot(P1, P2, P3, P4; size=(600, 620), layout=(2, 2))
-    plot!(plot_title="μ = $μ", plot_titlefontsize=12)
+    plot!(plot_title="Cauchy distribution case: μ = $μ", plot_titlefontsize=12)
 end
 
+function plot_likrattest_normal(; μ=1)
+    @assert μ > 0
+    
+    c, d = -4, μ+4
+    
+    P1 = plot(x -> -loglikrat_normal(x; μ), c, d; label="")
+    plot!(xguide="x", yguide="−log likelihood ratio")
+    
+    P4 = plot(α -> power_likrat_normal(α; μ), 0, 1; label="likrat")
+    plot!(α -> power_onesided_normal(α; μ); label="onesided", ls=:dash)
+    plot!(xguide="alpha", yguide="power")
+    plot!(legend=:bottomright)
+    
+    P3 = plot(α -> power_likrat_normal(α; μ), 0, 0.1; label="likrat")
+    plot!(α -> power_onesided_normal(α; μ); label="onesided", ls=:dash)
+    plot!(xguide="alpha", yguide="power")
+    
+    P2 = plot(x -> pvalue_likrat_normal(x; μ), c, d; label="likrat")
+    plot!(x -> pvalue_onesided_normal(x; μ), c, d; label="onesided", ls=:dash)
+    plot!(xguide="x", yguide="P-value")
+    
+    plot(P1, P2, P3, P4; size=(600, 620), layout=(2, 2))
+    plot!(plot_title="normal distribution case: μ = $μ", plot_titlefontsize=12)
+end
+
+# %%
 plot_likrattest(; μ=1)
 
 # %%
@@ -147,7 +220,13 @@ plot_likrattest(; μ=4)
 # %%
 plot_likrattest(; μ=5)
 
-# %%
+# %% tags=[]
 plot_likrattest(; μ=6)
+
+# %% tags=[]
+plot_likrattest_normal(; μ=1)
+
+# %%
+plot_likrattest_normal(; μ=2)
 
 # %%
