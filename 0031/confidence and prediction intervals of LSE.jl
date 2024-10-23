6edbf98858439d8349e1a151ce447043a6088189
Gen Kuroki <genkuroki@gmail.com>
2024-10-23 21:08:01 +0900

    Julia v1.11.1

--- 0031/confidence and prediction intervals of LSE.jl
+++ 0031/confidence and prediction intervals of LSE.jl
@@ -9,9 +9,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.10.3
 #   kernelspec:
-#     display_name: Julia 1.7.3
+#     display_name: Julia 1.11.1
 #     language: julia
-#     name: julia-1.7
+#     name: julia-1.11
 # ---
 
 # %%
