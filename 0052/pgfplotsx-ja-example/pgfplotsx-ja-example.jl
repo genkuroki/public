cc352b506efec9bf7f804b409e1b246b7b51569c
Gen Kuroki <genkuroki@gmail.com>
2025-02-12 13:41:50 +0900

    updated

--- 0052/pgfplotsx-ja-example/pgfplotsx-ja-example.jl
+++ 0052/pgfplotsx-ja-example/pgfplotsx-ja-example.jl
@@ -15,6 +15,34 @@
 # ---
 
 # %%
+# `@eval Plots pgfx_sanitize_string(s::AbstractString) = s` としていない場合には
+# 「じ」「で」などの濁点の位置がおかしくなる。TeXのコードでは以下のようになってしまう。
+# 
+# title={$こ$$ん$$な$$感$$じ$$に$$日$$本$$語$$を$pgfplotsx()$で$$も$$使$$え$$ま$$す$!}
+# \addlegendentry {$正$$弦$$関$$数$ y = sin\theta}
+
+using LaTeXStrings
+using Plots
+default(fmt=:png)
+
+pgfplotsx()
+PGFPlotsX.CUSTOM_PREAMBLE=[raw"\usepackage{luatexja}"]
+
+plot(sin; label=raw"正弦関数 y = sin\theta", legendfontsize=15)
+plot!(xguide=raw"\theta", yguide=raw"y", guidefontsize=14)
+plot!(xtick=(-2π:π/2:2π, [L"-2\pi", L"-3\pi/2", L"-\pi", L"-\pi/2", L"0", L"\pi/2", L"\pi", L"3\pi/2", L"2\pi"]),
+    ytick=-1:0.2:1, tickfontsize=14)
+plot!(ylim=(-1.2, 1.4), legend=:topleft)
+annotate!(0, -0.3, text(raw"正弦関数 y = sin\theta", :left, 14, :red, rotation=45))
+title!(raw"こんな感じに日本語をpgfplotsx()でも使えます!", titlefontsize=17)
+plot!(tex_output_standalone=true)
+savefig("pgfplotsx-ja-standalone-inappropriate.tex")
+plot!()
+
+# %%
+; cat pgfplotsx-ja-standalone-inappropriate.tex
+
+# %%
 using LaTeXStrings
 using Plots
 default(fmt=:png)
@@ -41,7 +69,7 @@ plot!(xguide=raw"横軸 $\theta$", yguide=raw"縦軸 $y$", guidefontsize=14)
 plot!(xtick=(-2π:π/2:2π, [L"-2\pi", L"-3\pi/2", L"-\pi", L"-\pi/2", L"0", L"\pi/2", L"\pi", L"3\pi/2", L"2\pi"]),
     ytick=-1:0.2:1, tickfontsize=14)
 plot!(ylim=(-1.2, 1.4), legend=:topleft)
-annotate!(0, -0.3, text(raw"正弦関数 $y=\sin\theta$", :left, 14, :red, rotation=45))
+annotate!(0, -0.3, text(raw"正弦関数 $y = \sin\theta$", :left, 14, :red, rotation=45))
 title!(raw"\textbf{こんな感じに日本語をpgfplotsx()でも使えます!}", titlefontsize=17)
 savefig("pgfplotsx-ja-fig.tex")
 savefig("pgfplotsx-ja-fig.pdf")
@@ -77,7 +105,7 @@ plot!(xguide=raw"横軸 $\theta$", yguide=raw"縦軸 $y$", guidefontsize=14)
 plot!(xtick=(-2π:π/2:2π, [L"-2\pi", L"-3\pi/2", L"-\pi", L"-\pi/2", L"0", L"\pi/2", L"\pi", L"3\pi/2", L"2\pi"]),
     ytick=-1:0.2:1, tickfontsize=14)
 plot!(ylim=(-1.2, 1.4), legend=:topleft)
-annotate!(0, -0.3, text(raw"正弦関数 $y=\sin\theta$", :left, 14, :red, rotation=45))
+annotate!(0, -0.3, text(raw"正弦関数 $y = \sin\theta$", :left, 14, :red, rotation=45))
 title!(raw"\textbf{こんな感じに日本語をpgfplotsx()でも使えます!}", titlefontsize=17)
 plot!(tex_output_standalone=true)
 savefig("pgfplotsx-ja-standalone.tex")
