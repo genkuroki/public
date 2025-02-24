d1b9342e929eb1b94e3dfa40c382885032a938ab
Gen Kuroki <genkuroki@gmail.com>
2025-02-24 22:10:05 +0900

    updated

--- 0053/Bayesian method for diversity index.jl
+++ 0053/Bayesian method for diversity index.jl
@@ -27,6 +27,8 @@ default(fmt=:png)
 
 simpson_diversity_index(x) = 1 - dot(x, x) / sum(x)^2
 
+sid_bayes(x) = 1 - (dot(x, x) + sum(x)) / (sum(x) * (sum(x) + 1))
+
 function sehat_sdi(x)
     n = sum(x)
     √((4/n) * (sum(x->(x/n)^3, x) - sum(x -> (x/n)^2, x)^2))
@@ -93,29 +95,93 @@ table3 = [30; 13; 9; 8; fill(7, 3); fill(6, 2); 5; fill(2, 3); fill(1, 13)]
 plot_posterior(data=table3)
 
 # %%
-function _eq_of_lambda(λ, x, d)
-    r = length(x)
-    n = sum(x)
-    ν = n - λ*(1 - d)
-    2λ - r*ν - sum(√(ν^2 + 4λ*x[i]) for i in eachindex(x))
+dist = Multinomial(sum(table1), table1/sum(table1))
+μ = mean(dist)
+k = params(Dirichlet(μ))[1]
+sid_bayes(k)
+
+# %%
+function pvalues_bayes(data, sid₀; κ=eps(), L=10^4)
+    m = length(data)
+    prior_param = fill(κ, m)
+    posterior = Dirichlet(data .+ prior_param)
+    Xtmp = rand(posterior)
+    c = zeros(Int, length(sid₀))
+    d = zeros(Int, length(sid₀))
+    for i in 1:L
+        X = rand!(posterior, Xtmp)
+        sid = 1 - dot(X, X)
+        @. c += sid ≤ sid₀
+        @. d += sid ≥ sid₀
+    end
+    @. min(1, 2c/L, 2d/L)
 end
 
-@show x = table1
-@show d = 0.9
-f(λ) = _eq_of_lambda(λ, x, d)
-@show λ = find_zero(f, 0.0)
+function pvalue_bayes(data, sid₀; κ=eps(), L=10^4)
+    m = length(data)
+    prior_param = fill(κ, m)
+    posterior = Dirichlet(data .+ prior_param)
+    Xtmp = rand(posterior)
+    c = 0
+    d = 0
+    for i in 1:L
+        X = rand!(posterior, Xtmp)
+        sid = 1 - dot(X, X)
+        c += sid ≤ sid₀
+        d += sid ≥ sid₀
+    end
+    min(1, 2c/L, 2d/L)
+end
 
-n = sum(x)
-phat = x/n
-ν = n - λ*(1 - d)
-ptilde = [2x/(-ν+√(ν^2 + 4λ*x)) for x in x]
-@show ptilde .|> r
-@show all(ptilde .> 0)
-@show sum(ptilde)
-@show 1 - dot(ptilde, ptilde)
-@show G = 2sum(x[i]*log(phat[i]/ptilde[i]) for i in eachindex(x))
-@show pval = ccdf(Chisq(1), G)
+function sim_pval(dist::Multinomial; niters=10^4, κ=eps(), L=10^4)
+    μ = mean(dist)
+    sdi₀ = sid_bayes(μ)
+    pval = zeros(niters)
+    datatmp = [rand(dist) for _ in 1:Threads.nthreads()]
+    Threads.@threads :static for i in 1:niters
+        tid = Threads.threadid()
+        data = rand!(dist, datatmp[tid])
+        pval[i] = pvalue_bayes(data, sdi₀; κ, L)
+    end
+    pval
+end
 
-plot(f, λ-1, λ+1)
+_ecdf(A, x) = count(≤(x), A) / length(A)
+
+# %%
+data = table1
+
+@show sidhat = simpson_diversity_index(data)
+@show sidcheck = sid_bayes(data)
+sid₀ = range(sidcheck-0.02, sidcheck+0.02, 100)
+@show length(sid₀)
+@time pval = pvalues_bayes(data, sid₀; κ=eps(), L=10^6)
+plot(sid₀, pval)
+#vline!([sidhat])
+vline!([sidcheck])
+
+# %%
+data = table1
+dist = Multinomial(sum(data), data/sum(data))
+@time pval = sim_pval(dist; κ=0.23, niters=3000)
+plot(α -> _ecdf(pval, α), 0, 0.1; label="")
+plot!(identity; label="", ls=:dot)
+plot!(size=(400, 400))
+
+# %%
+data = table2
+dist = Multinomial(sum(data), data/sum(data))
+@time pval = sim_pval(dist; κ=0.05, niters=3000)
+plot(α -> _ecdf(pval, α), 0, 0.1; label="")
+plot!(identity; label="", ls=:dot)
+plot!(size=(400, 400))
+
+# %%
+data = table3
+dist = Multinomial(sum(data), data/sum(data))
+@time pval = sim_pval(dist; κ=0.1, niters=3000)
+plot(α -> _ecdf(pval, α), 0, 0.1; label="")
+plot!(identity; label="", ls=:dot)
+plot!(size=(400, 400))
 
 # %%
