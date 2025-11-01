7d98622ef27d3999c915e912a26d21209e34ec54
Gen Kuroki <genkuroki@gmail.com>
2025-11-01 23:00:46 +0900

    updated

--- 0055/dash_plot2.pdf.jl
+++ 0055/dash_plot2.pdf.jl
@@ -18,32 +18,6 @@
 using Makie, CairoMakie
 
 # データの準備
-x = 0:0.1:10
-y1 = sin.(x)
-y2 = sin.(x .+ 0.2)
-y3 = sin.(x .+ 0.4)
-
-# 図の作成
-fig = Figure(size=(600, 400))
-
-# サブプロット2: 複数の破線パターン
-ax2 = Axis(fig[1,1], title="Multiple Dash Patterns")
-lines!(ax2, x, y1, label="dash", linewidth=2, linestyle=:dash)
-lines!(ax2, x, y2, label="dash", linewidth=2, linestyle=:dash)
-lines!(ax2, x, y3, label="dash", linewidth=2, linestyle=:dash)
-axislegend(ax2)
-xlims!(ax2, 0, 10)
-
-# PDF形式で保存（この際にApple製品での表示問題が発生する可能性あり）
-save("dash_plot.pdf", fig)
-
-# PNG形式でも保存（こちらは問題なく表示される）
-save("dash_plot.png", fig)
-
-# %%
-using Makie, CairoMakie
-
-# データの準備
 x = 0:0.0001:10
 y1 = sin.(x)
 y2 = sin.(x .+ 0.2)
@@ -65,5 +39,24 @@ save("dash_plot2.pdf", fig)
 
 # PNG形式でも保存（こちらは問題なく表示される）
 save("dash_plot2.png", fig)
+
+fig
+
+# %%
+using Plots: Plots
+Plots.gr()
+
+x = 0:0.0001:10
+y1 = sin.(x)
+y2 = sin.(x .+ 0.2)
+y3 = sin.(x .+ 0.4)
+
+P = Plots.plot(title="Multiple Dash Patterns by Plots gr()")
+Plots.plot!(x, y1; ls=:dash, label="dash")
+Plots.plot!(x, y2; ls=:dash, label="dash")
+Plots.plot!(x, y3; ls=:dash, label="dash")
+Plots.savefig("dash_plot2_Plots_gr.png")
+Plots.savefig("dash_plot2_Plots_gr.pdf")
+P
 
 # %%
