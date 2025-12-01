2d8e323c653f561351959590a06c74d41fbb2c91
Gen Kuroki <genkuroki@gmail.com>
2025-12-01 13:28:45 +0900

    updated

--- 0055/t-test and Mann-Whitney U-test.jl
+++ 0055/t-test and Mann-Whitney U-test.jl
@@ -16,6 +16,8 @@
 
 # %% [markdown]
 # https://okumuralab.org/~okumura/python/t_u_test.html はヒストグラムを使っている点が非常によろしくない。
+#
+# 関連ノートブック: https://github.com/genkuroki/public/blob/main/0051/Student%20vs.%20Welch.ipynb
 
 # %%
 using Random
@@ -27,8 +29,9 @@ using Roots
 
 distname(dist) = replace(string(dist), r"{[^}]*}"=>"")
 distname(dist::InverseGamma) = "InverseGamma(α=$(shape(dist)), θ=$(scale(dist)))"
+safe_pdf(dist, x) = minimum(dist) < x < maximum(dist) ? pdf(dist, x) : zero(eltype(dist))
 
-default(fmt=:png, legend=false, titlefontsize=10, plot_titlefontsize=10, size=(400, 200))
+default(fmt=:png, legend=false, titlefontsize=10, plot_titlefontsize=10, size=(320, 200))
 
 # approximate Mann-Whitney U-testではデフォルトでは連続性補正がかかっている。
 # https://github.com/JuliaStats/HypothesisTests.jl/blob/master/src/mann_whitney.jl#L236-L249
@@ -49,9 +52,11 @@ function p_values(distx, m, disty, n; niters=10^6, continuity_correction=true,
         handicap_mean = mean(disty) - mean(distx),
         handicap_win_rate = equalizing_handicap(distx, disty),
     )
+    exact_mwu = m + n ≤ 66 ? true : false
+    name_mwu = exact_mwu ? "exact Mann-Whitney U-test" : "approximate Mann-Whitney U-test"
     pvalue_student_t = zeros(niters)
     pvalue_welch_t = zeros(niters)
-    pvalue_mann_whitney_u_exact = zeros(niters)
+    pvalue_mann_whitney_u = zeros(niters)
     pvalue_mann_whitney_u_approximate = zeros(niters)
     nth = Threads.nthreads(:interactive) + Threads.nthreads(:default)
     tmpx = [zeros(m) for _ in 1:nth]
@@ -65,21 +70,25 @@ function p_values(distx, m, disty, n; niters=10^6, continuity_correction=true,
         pvalue_welch_t[i] = pvalue(UnequalVarianceTTest(datax, datay))
         datax .-= handicap_mean
         datax .+= handicap_win_rate
-        pvalue_mann_whitney_u_exact[i] = pvalue(ExactMannWhitneyUTest(datax, datay))
+        if exact_mwu
+            pvalue_mann_whitney_u[i] = pvalue(ExactMannWhitneyUTest(datax, datay))
+        else
+            pvalue_mann_whitney_u[i] = pvalue(ApproximateMannWhitneyUTest(datax, datay))
+        end
         pvalue_mann_whitney_u_approximate[i] = my_pvalue(ApproximateMannWhitneyUTest(datax, datay); continuity_correction)
     end
-    (; pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u_exact, pvalue_mann_whitney_u_approximate)
+    (; name_mwu, pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u, pvalue_mann_whitney_u_approximate)
 end
     
 function hist_p_values(distx, m, disty, n; niters=10^6, bin=0:0.05:1.05, continuity_correction=true,
         handicap_mean = mean(disty) - mean(distx),
         handicap_win_rate = equalizing_handicap(distx, disty),
     )
-    @time (; pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u_exact, pvalue_mann_whitney_u_approximate) =
+    @time (;name_mwu,  pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u, pvalue_mann_whitney_u_approximate) =
         p_values(distx, m, disty, n; niters, continuity_correction, handicap_mean, handicap_win_rate)
     P1 = histogram(pvalue_student_t; norm=true, c=1, alpha=0.5, title="Student t-test", bin)
     P2 = histogram(pvalue_welch_t; norm=true, c=2, alpha=0.5, title="Welch t-test", bin)
-    P3 = histogram(pvalue_mann_whitney_u_exact; norm=true, c=3, alpha=0.5, title="exact Mann-Whitney U-test", bin)
+    P3 = histogram(pvalue_mann_whitney_u; norm=true, c=3, alpha=0.5, title=name_mwu, bin)
     P4 = histogram(pvalue_mann_whitney_u_approximate; norm=true, c=4, alpha=0.5, title="approximate Mann-Whitney U-test", bin)
     plot(P1, P2, P3, P4; size=(640, 400), layout=(2, 2))
     plot!(plot_title="$(distname(distx)), m=$m vs. $(distname(disty)), n=$n")
@@ -91,13 +100,13 @@ function ecdf_p_values(distx, m, disty, n; niters=10^6, maxalpha=1, continuity_c
         handicap_mean = mean(disty) - mean(distx),
         handicap_win_rate = equalizing_handicap(distx, disty),
     )
-    @time (; pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u_exact, pvalue_mann_whitney_u_approximate) =
+    @time (;name_mwu,  pvalue_student_t, pvalue_welch_t, pvalue_mann_whitney_u, pvalue_mann_whitney_u_approximate) =
         p_values(distx, m, disty, n; niters, continuity_correction, handicap_mean, handicap_win_rate)
     P1 = plot(x -> _ecdf(pvalue_student_t, x), 0, maxalpha; c=1, title="Student t-test")
     plot!(identity; ls=:dot, c=:gray, alpha=0.8)
     P2 = plot(x -> _ecdf(pvalue_welch_t, x), 0, maxalpha; c=2, title="Welch t-test")
     plot!(identity; ls=:dot, c=:gray, alpha=0.8)
-    P3 = plot(x -> _ecdf(pvalue_mann_whitney_u_exact, x), 0, maxalpha; c=3, title="exact Mann-Whitney U-test")
+    P3 = plot(x -> _ecdf(pvalue_mann_whitney_u, x), 0, maxalpha; c=3, title=name_mwu)
     plot!(identity; ls=:dot, c=:gray, alpha=0.8)
     P4 = plot(x -> _ecdf(pvalue_mann_whitney_u_approximate, x), 0, maxalpha; c=4, title="approximate Mann-Whitney U-test")
     plot!(identity; ls=:dot, c=:gray, alpha=0.8)
@@ -152,6 +161,9 @@ hist_p_values(InverseGamma(3.1, 1), 10, InverseGamma(3.1, 1), 10; bin=0:0.02:1, 
 # %% [markdown]
 # ## P値の経験累積分布関数のグラフ
 
+# %% [markdown]
+# ### 等分布の場合
+
 # %%
 ecdf_p_values(Normal(), 10, Normal(), 10)
 
@@ -198,5 +210,98 @@ ecdf_p_values(InverseGamma(3.1, 1), 5, InverseGamma(3.1, 1), 20; maxalpha=0.1)
 
 # %%
 ecdf_p_values(InverseGamma(3.1, 1), 5, InverseGamma(3.1, 1), 20; maxalpha=0.1, continuity_correction=false)
+
+# %% [markdown]
+# ### 非等分布の場合
+
+# %% [markdown]
+# #### 分散が異なる2つの正規分布の場合
+
+# %%
+ecdf_p_values(Normal(0, 1), 10, Normal(0, 3), 10; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(Normal(0, 1), 30, Normal(0, 3), 10; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(Normal(0, 1), 10, Normal(0, 3), 30; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(Normal(0, 1), 100, Normal(0, 3), 100; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(Normal(0, 1), 300, Normal(0, 3), 100; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(Normal(0, 1), 100, Normal(0, 3), 300; maxalpha=0.1, continuity_correction=false)
+
+# %% [markdown]
+# #### 分散が異なる2つの逆ガンマ分布の場合
+
+# %%
+distx = InverseGamma(3.1, 1)
+disty = InverseGamma(3.1, 3)
+igx, igy = distx, disty
+
+@show distname(distx) distname(disty)
+@show std(disty)/std(distx)
+@show skewness(distx) skewness(disty)
+@show handicap_mean = mean(disty) - mean(distx)
+@show handicap_win_rate = equalizing_handicap(distx, disty)
+xmin = min(0, handicap_mean, handicap_win_rate)
+xmax = max(5std(distx) + handicap_win_rate, 5std(disty))
+xs = range(xmin, xmax, 1000)
+P1 = plot(xs, x -> safe_pdf(distx+handicap_mean, x); label="distx + handicap")
+plot!(xs, x -> pdf(disty, x); label="disty")
+title!("fair for t-tests")
+P2 = plot(xs, x -> safe_pdf(distx+handicap_win_rate, x); label="distx + handicap")
+plot!(xs, x -> pdf(disty, x); label="disty")
+title!("fair for Mann-Whitney U-tests")
+plot(P1, P2; size=(640, 200), legend=true)
+
+# %%
+ecdf_p_values(igx, 10, igx, 10; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 30, igx, 30; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 100, igx, 100; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 10, igy, 10; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 30, igy, 10; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 10, igy, 30; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 100, igy, 100; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 300, igy, 100; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 100, igy, 300; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 300, igy, 300; maxalpha=0.1, continuity_correction=false)
+
+# %%
+ecdf_p_values(igx, 900, igy, 300; maxalpha=0.1, continuity_correction=false, niters=10^5)
+
+# %%
+ecdf_p_values(igx, 300, igy, 900; maxalpha=0.1, continuity_correction=false, niters=10^5)
+
+# %%
+ecdf_p_values(igx, 900, igy, 900; maxalpha=0.1, continuity_correction=false, niters=10^5)
+
+# %%
+ecdf_p_values(igx, 2700, igy, 900; maxalpha=0.1, continuity_correction=false, niters=10^5)
+
+# %%
+ecdf_p_values(igx, 2700, igy, 2700; maxalpha=0.1, continuity_correction=false, niters=10^5)
 
 # %%
